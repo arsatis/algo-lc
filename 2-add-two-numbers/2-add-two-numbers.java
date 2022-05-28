68fa8c39a23aa2a1b95bed23fbb54ea4c476a2ae
Zheng Yong <angzhengyong@gmail.com>
2022-05-28 13:38:01 +0800

    Time: 2 ms (93.80%), Space: 48.1 MB (32.06%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -8,7 +8,7 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
+class Solution { 
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy = new ListNode();
         ListNode curr = dummy;
