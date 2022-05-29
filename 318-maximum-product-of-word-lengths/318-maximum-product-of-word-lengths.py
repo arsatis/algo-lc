9d04dabe0d8afd7a553b431c740e7ed88b965a27
Zheng Yong <angzhengyong@gmail.com>
2022-05-29 13:18:18 +0800

    Time: 486 ms (83.07%), Space: 14.5 MB (42.09%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -4,7 +4,7 @@ class Solution:
         d = {}
         for word in words:
             mask = 0
-            for c in set(word):
-                mask |= (1 << (ord(c) - ord('a')))
+            for c in word:
+                mask |= 1 << (ord(c) - ord('a'))
             d[mask] = max(d.get(mask, 0), len(word))
         return max([d[x] * d[y] for x in d for y in d if not x & y] or [0])
