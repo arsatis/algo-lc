a889f5ae9539569cad6a1656a8f7f41d5d4a372b
Zheng Yong <angzhengyong@gmail.com>
2022-05-29 12:30:57 +0800

    Time: 168 ms (99.53%), Space: 14.4 MB (42.09%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.py
@@ -1,15 +1,11 @@
 class Solution:
     def maxProduct(self, words: List[str]) -> int:
-        # dumb O(n^2) solution
-        output = 0
-        for i in range(len(words)):
-            for j in range(i + 1, len(words)):
-                no_common = True
-                for c in words[i]:
-                    no_common = no_common and not (c in words[j])
-                    if not no_common:
-                        break
-                if no_common:
-                    output = max(output, len(words[i]) * len(words[j]))
-        return output
+        # better O(n * c) solution
+        d = {}
+        for word in words:
+            mask = 0
+            for c in set(word):
+                mask |= (1 << (ord(c) - 97))
+            d[mask] = max(d.get(mask, 0), len(word))
+        return max([d[x] * d[y] for x in d for y in d if not x & y] or [0])
             
