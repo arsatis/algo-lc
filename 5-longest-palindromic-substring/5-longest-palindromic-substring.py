32ec46c5ae2c1f1cf057160759a8afa9003ebb5e
Zheng Yong <angzhengyong@gmail.com>
2022-05-29 19:04:49 +0800

    Time: 204 ms (96.16%), Space: 14 MB (30.52%) - LeetHub

--- 5-longest-palindromic-substring/5-longest-palindromic-substring.py
+++ 5-longest-palindromic-substring/5-longest-palindromic-substring.py
@@ -3,8 +3,7 @@ class Solution:
         max_len = 1
         start = 0
         for i in range(len(s)):
-        	if i - max_len >= 1 and\
-                s[i - max_len - 1:i + 1] == s[i - max_len - 1:i + 1][::-1]:
+        	if i - max_len >= 1 and s[i - max_len - 1:i + 1] == s[i - max_len - 1:i + 1][::-1]:
         		start = i - max_len - 1
         		max_len += 2
         		continue
