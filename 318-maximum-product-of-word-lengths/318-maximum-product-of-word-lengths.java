18907d4d56a652614f9285d5b2d0d4441d26302e
Zheng Yong <angzhengyong@gmail.com>
2022-05-29 13:11:36 +0800

    Time: 11 ms (71.82%), Space: 46.2 MB (62.25%) - LeetHub

--- 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
+++ 318-maximum-product-of-word-lengths/318-maximum-product-of-word-lengths.java
@@ -1,21 +1,19 @@
-import java.util.HashMap;
-
 class Solution {
     public int maxProduct(String[] words) {
-        HashMap<Integer, Integer> hm =  new HashMap<>();
-        for (String word : words) {
-            int mask = 0;
-            for (int i = 0; i < word.length(); ++i)
-                mask |= 1 << (word.charAt(i) - 'a');
-            hm.put(mask, Math.max(hm.getOrDefault(mask, 0), word.length()));
-        }
-        int output = 0;
-        for (Integer i : hm.keySet()) {
-            for (Integer j : hm.keySet()) {
-                if ((i & j) == 0) {
-                    output = Math.max(output, hm.get(i) * hm.get(j));
-                }
+        int max = (1 << 26) - 1;
+        int mask[] = new int[words.length];
+        for (int i = 0; i < words.length; ++i)
+            for (int j = 0; j < words[i].length(); ++j) {
+                mask[i] |= 1 << (words[i].charAt(j) - 'a');
+                if (mask[i] == max)
+                    break;
             }
+        int output = 0;
+        for (int i = 0; i < words.length - 1; ++i) {
+            if (mask[i] == max) continue;
+            for (int j = i + 1; j < words.length; j++)
+                if ((mask[i] & mask[j]) == 0)
+                    output = Math.max(output, words[i].length() * words[j].length());
         }
         return output;
     }
