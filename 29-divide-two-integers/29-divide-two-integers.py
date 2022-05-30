d291aa4bcf96beaea853e4fa5285a4b128766556
Zheng Yong <angzhengyong@gmail.com>
2022-05-30 20:56:22 +0800

    Time: 47 ms (48.57%), Space: 14 MB (25.68%) - LeetHub

--- 29-divide-two-integers/29-divide-two-integers.py
+++ 29-divide-two-integers/29-divide-two-integers.py
@@ -1,26 +1,18 @@
 class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-        # idea: find largest x = (divisor) * 2^n <= dividend
-        #       then iteratively add to quotient when dividend is divisible by x
-        # time: O(n), where n is defined in the first line
-        # space: O(1)
-        neg = (dividend < 0) != (divisor < 0)
+        is_negative = (dividend < 0) != (divisor < 0)
         divisor, dividend = abs(divisor), abs(dividend)
 
-        # find largest (divisor) * 2^n <= dividend
-        curr, n = divisor, 1
-        while dividend >= (curr << 1):
-            curr <<= 1
-            n <<= 1
-
-        # repeatedly add current_quotient
         quotient = 0
-        while dividend >= divisor:
-            if dividend >= curr:
-                dividend -= curr
-                quotient += n
-            curr >>= 1
-            n >>= 1
+        the_sum = divisor
 
-        if neg: return max(-quotient, -2147483648)
-        else: return min(quotient, 2147483647)
+        while the_sum <= dividend:
+            current_quotient = 1
+            while (the_sum << 1) <= dividend:
+                the_sum <<= 1
+                current_quotient <<= 1            
+            dividend -= the_sum
+            the_sum = divisor
+            quotient += current_quotient
+
+        return min(2147483647, max(-quotient if is_negative else quotient, -2147483648))
