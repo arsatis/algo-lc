0ef5ba1deff960531a71ee92d5db8e8d55f09c80
Zheng Yong <angzhengyong@gmail.com>
2022-05-31 21:42:45 +0800

    Time: 385 ms (62.89%), Space: 40 MB (77.04%) - LeetHub

---
+++ 1461-check-if-a-string-contains-all-binary-codes-of-size-k/1461-check-if-a-string-contains-all-binary-codes-of-size-k.cpp
@@ -0,0 +1,18 @@
+#include <unordered_set>
+
+class Solution {
+public:
+    bool hasAllCodes(string s, int k) {
+        // rolling hash
+        int d = 0;
+        unordered_set<int> hset;
+        for (int i = 0; i < s.length(); i++) {
+            d = (d << 1) | (s[i] - '0');
+            if (i >= k - 1) {
+                hset.insert(d);
+                d -= (s[i - (k - 1)] - '0') << (k - 1);
+            }
+        }
+        return hset.size() == (1 << k);
+    }
+};
