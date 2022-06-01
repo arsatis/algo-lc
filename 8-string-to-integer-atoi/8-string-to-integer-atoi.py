dfc2a1c4f112c429cf0dd5c77ce047c0194b9ea2
Zheng Yong <angzhengyong@gmail.com>
2022-06-01 21:41:40 +0800

    Time: 50 ms (46.82%), Space: 13.8 MB (79.49%) - LeetHub

--- 8-string-to-integer-atoi/8-string-to-integer-atoi.py
+++ 8-string-to-integer-atoi/8-string-to-integer-atoi.py
@@ -1,7 +1,8 @@
+import re
+
 class Solution:
     def myAtoi(self, s: str) -> int:
         r = re.match(r'\s*[+-]?\d+', s)
-        if not r:
-            return 0
+        if not r: return 0
         r = int(r.group(0))
         return -2147483648 if r < -2147483648 else (r if r < 2147483647 else 2147483647)
