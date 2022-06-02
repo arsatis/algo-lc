92b191a18fb968e443079e1a7b9e90eeac6098f4
Zheng Yong <angzhengyong@gmail.com>
2022-06-02 23:48:16 +0800

    Time: 103 ms (44.92%), Space: 14.7 MB (55.33%) - LeetHub

--- 867-transpose-matrix/867-transpose-matrix.py
+++ 867-transpose-matrix/867-transpose-matrix.py
@@ -1,5 +1,3 @@
-import numpy as np
-
 class Solution:
     def transpose(self, matrix: List[List[int]]) -> List[List[int]]:
-        return list(np.array(matrix).T)
+        return list(zip(*matrix))
