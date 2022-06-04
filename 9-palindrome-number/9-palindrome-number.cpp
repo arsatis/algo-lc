a2012ae485491bb4b1d3d9f001be32d720d3197b
Zheng Yong <angzhengyong@gmail.com>
2022-06-04 17:45:46 +0800

    Time: 22 ms (44.27%), Space: 5.7 MB (99.24%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        string s = to_string(x);
-        for (int i = 0; i < s.size() / 2; ++i)
-            if (s[i] != s[s.size() - i - 1])
-                return false;
-        return true;
+        if (x < 0 || (x != 0 && x % 10 ==0 )) return false;
+        int sum = 0;
+        while (x > sum) {
+            sum = sum * 10 + x % 10;
+            x = x / 10;
+        }
+        return (x == sum) || (x == sum / 10);
     }
 };
