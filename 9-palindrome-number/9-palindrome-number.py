47deae34f31b180c461eea720d57629c73460261
Zheng Yong <angzhengyong@gmail.com>
2022-06-04 17:41:39 +0800

    Time: 63 ms (87.36%), Space: 14 MB (15.94%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.py
+++ 9-palindrome-number/9-palindrome-number.py
@@ -2,4 +2,4 @@ class Solution:
     def isPalindrome(self, x: int) -> bool:
         # should be fastest because string comp in py is amortized O(1)
         s = str(x)
-        return s[:1 + len(s) // 2] == s[::-1][:1 + len(s) // 2]
+        return s == s[::-1]
