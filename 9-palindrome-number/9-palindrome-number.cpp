c342df9d60f9a9702349947babcae1b3b74d761f
Zheng Yong <angzhengyong@gmail.com>
2022-06-04 17:53:27 +0800

    Time: 18 ms (59.90%), Space: 5.7 MB (99.24%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -6,7 +6,7 @@ public:
         int leading, trailing;
         while (x / divisor >= 10) divisor *= 10;
         while (x != 0) {
-          leading = x / divisor;
+            leading = x / divisor;
             trailing = x % 10;
             if (leading != trailing) return false;
             x = (x % divisor) / 10;
