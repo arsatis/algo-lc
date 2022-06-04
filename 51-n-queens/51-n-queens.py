356745facef7fba1eb313e8168af063688884fb6
Zheng Yong <angzhengyong@gmail.com>
2022-06-04 09:11:06 +0800

    Time: 58 ms (88.80%), Space: 14.4 MB (45.52%) - LeetHub

--- 51-n-queens/51-n-queens.py
+++ 51-n-queens/51-n-queens.py
@@ -2,12 +2,12 @@ class Solution:
     def solveNQueens(self, n: int) -> List[List[str]]:
         def DFS(queens, left_diag, right_diag):
             p = len(queens)
-            if p==n:
+            if p == n:
                 result.append(queens)
                 return
             for q in range(n):
-                if q not in queens and p-q not in left_diag and p+q not in right_diag: 
-                    DFS(queens+[q], left_diag+[p-q], right_diag+[p+q])  
+                if q not in queens and p - q not in left_diag and p + q not in right_diag: 
+                    DFS(queens + [q], left_diag + [p - q], right_diag + [p + q])  
         result = []
-        DFS([],[],[])
-        return [ ["." * i + "Q" + "."*(n-i-1) for i in sol] for sol in result]
+        DFS([], [], [])
+        return [["." * i + "Q" + "." * (n - i - 1) for i in sol] for sol in result]
