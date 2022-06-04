a31e1ea43bcb0de9ea893c85eb3c1e8559362192
Zheng Yong <angzhengyong@gmail.com>
2022-06-04 17:53:46 +0800

    Time: 8 ms (92.86%), Space: 5.9 MB (32.92%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.cpp
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -1,17 +1,15 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0) return false;
-        int divisor = 1;
-        int leading, trailing;
-        while (x / divisor >= 10) divisor *= 10;
-        while (x != 0) {
-            leading = x / divisor;
-            trailing = x % 10;
-            if (leading != trailing) return false;
-            x = (x % divisor) / 10;
-            divisor = divisor / 100;
+        if (x < 0) return false;        
+        long copy = 0;
+        long temp = x;
+        while (temp != 0) {
+            int l = temp % 10;
+            copy = copy * 10 + l;
+            temp = temp / 10;
         }
-        return true;
+        
+        return copy == x;
     }
 };
