01194438f99267af7b7fd0a1676350b4b68bd2ae
Zheng Yong <angzhengyong@gmail.com>
2022-06-05 14:48:30 +0800

    Time: 87 ms (46.03%), Space: 14 MB (39.44%) - LeetHub

---
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -0,0 +1,13 @@
+class Solution:
+    def totalNQueens(self, n: int) -> int:
+        def DFS(queens, left_diag, right_diag):
+            p = len(queens)
+            if p == n:
+                result.append(queens)
+                return
+            for q in range(n):
+                if q not in queens and p - q not in left_diag and p + q not in right_diag: 
+                    DFS(queens + [q], left_diag + [p - q], right_diag + [p + q])  
+        result = []
+        DFS([], [], [])
+        return len(result)
