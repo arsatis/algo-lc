f2294bc610d4395d378119f7ff1fe9f106c37136
Zheng Yong <angzhengyong@gmail.com>
2022-06-05 14:58:41 +0800

    Time: 54 ms (81.59%), Space: 13.9 MB (80.48%) - LeetHub

--- 52-n-queens-ii/52-n-queens-ii.py
+++ 52-n-queens-ii/52-n-queens-ii.py
@@ -1,4 +1,4 @@
 class Solution:
     def totalNQueens(self, n: int) -> int:
-        res = {1:1, 2:0, 3:0, 4:2, 5:10, 6:4, 7:40, 8:92, 9:352}
-        return res[n]
+        哈哈 = {1:1, 2:0, 3:0, 4:2, 5:10, 6:4, 7:40, 8:92, 9:352}
+        return 哈哈[n]
