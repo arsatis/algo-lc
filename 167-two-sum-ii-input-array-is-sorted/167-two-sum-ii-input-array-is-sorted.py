4bb14fe27498e71eac6938ae7978cf13b39075ca
Zheng Yong <angzhengyong@gmail.com>
2022-06-09 20:28:09 +0800

    Time: 201 ms (36.78%), Space: 14.9 MB (89.05%) - LeetHub

--- 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
+++ 167-two-sum-ii-input-array-is-sorted/167-two-sum-ii-input-array-is-sorted.py
@@ -2,6 +2,6 @@ class Solution:
     def twoSum(self, numbers: List[int], target: int) -> List[int]:
         d = {}
         for i, num in enumerate(numbers):
-            if target - num in d:
+            if target - num in d.keys():
                 return [d[target - num] + 1, i + 1]
             d[num] = i
