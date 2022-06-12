83f1cd6d48b8f0b5f049a625e3b827afb01fcdb3
Zheng Yong <angzhengyong@gmail.com>
2022-06-12 11:15:58 +0800

    Time: 186 ms (97.93%), Space: 89.1 MB (99.27%) - LeetHub

--- 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
+++ 1695-maximum-erasure-value/1695-maximum-erasure-value.cpp
@@ -1,7 +1,7 @@
 class Solution {
 public:
     int maximumUniqueSubarray(vector<int>& nums) {
-        bool arr[10001] = {false};
+        bool arr[10001] = { false };
         int l = 0, r = 0, output = 0, currSum = 0;
         while (r < nums.size()) {
             while (arr[nums[r]]) {
