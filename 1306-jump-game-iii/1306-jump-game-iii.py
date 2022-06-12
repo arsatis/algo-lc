df18066e904d65290e52d5c644bac0553fbe2134
Zheng Yong <angzhengyong@gmail.com>
2022-06-12 15:59:32 +0800

    Time: 384 ms (55.50%), Space: 21 MB (53.56%) - LeetHub

---
+++ 1306-jump-game-iii/1306-jump-game-iii.py
@@ -0,0 +1,18 @@
+class Solution:
+    def canReach(self, arr: List[int], start: int) -> bool:
+        visited = set([start])
+        stack = [start]
+        while stack:
+            idx = stack.pop()
+            if arr[idx] == 0: return True
+            
+            s = idx + arr[idx]
+            if s < len(arr) and s not in visited:
+                stack.append(s)
+                visited.add(s)
+                
+            d = idx - arr[idx]
+            if d >= 0 and d not in visited:
+                stack.append(d)
+                visited.add(d)
+        return False
