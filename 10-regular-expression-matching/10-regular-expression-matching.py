015915540aac18c9b5c35a526afe264448ff2859
Zheng Yong <angzhengyong@gmail.com>
2022-06-12 18:11:37 +0800

    Time: 66 ms (62.48%), Space: 14.2 MB (25.90%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.py
+++ 10-regular-expression-matching/10-regular-expression-matching.py
@@ -1,3 +1,15 @@
 class Solution:
-    def isMatch(self, s: str, p: str) -> bool:
-        return re.fullmatch(p, s)
+    def isMatch(self, s: str, p: str) -> bool:        
+        def dp(si, pi):
+            if pi >= len(p): return si == len(s)
+            if si >= len(s): return pi + 1 < len(p) and p[pi + 1] == '*' and dp(si, pi + 2)
+            if (si, pi) not in memo:
+                matched = p[pi] == '.' or p[pi] == s[si]
+                if pi + 1 < len(p) and p[pi + 1] == '*':
+                    memo[(si, pi)] = dp(si, pi + 2) or (matched and dp(si + 1, pi))
+                else:
+                    memo[(si, pi)] = matched and dp(si + 1, pi + 1)
+            return memo[(si, pi)]
+        
+        memo = {}
+        return dp(0, 0)
