951b40d91abd83051e4c9e982a714d0dca7c045d
Zheng Yong <angzhengyong@gmail.com>
2022-06-13 23:26:27 +0800

    Time: 71 ms (87.14%), Space: 14.9 MB (60.15%) - LeetHub

--- 120-triangle/120-triangle.py
+++ 120-triangle/120-triangle.py
@@ -1,5 +1,23 @@
 class Solution:
     def minimumTotal(self, triangle: List[List[int]]) -> int:
+        # top-down
+#         def dfs(i, j):
+#             if i == len(triangle) - 1: return triangle[i][j]
+#             if memo[i][j] == None:
+#                 memo[i][j] = triangle[i][j] + min(dfs(i + 1, j), dfs(i + 1, j + 1))
+#             return memo[i][j]
+#         memo = [[None] * len(triangle) for _ in range(len(triangle))]
+#         return dfs(0, 0)
+
+        # bottom-up, recursive
+#         def fill_all(i):
+#             if i == -1: return
+#             for j in range(i + 1):
+#                 triangle[i][j] += min(triangle[i + 1][j], triangle[i + 1][j + 1])
+#             fill_all(i - 1)
+#         fill_all(len(triangle) - 2)
+#         return triangle[0][0]
+    
         # bottom-up, iterative
         for i in range(len(triangle) - 2, -1, -1):
             for j in range(i + 1):
