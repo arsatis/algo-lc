e6e73640cb7c5acd7866e485b9c50c879d32de25
Zheng Yong <angzhengyong@gmail.com>
2022-07-06 23:00:59 +0800

    Time: 5 ms (45.27%), Space: 5.9 MB (80.05%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,8 +1,13 @@
-class Solution:
-    def fib(self, n: int) -> int:
-        if n == 0: return 0
-        arr = [0] * (n + 1)
-        arr[1] = 1
-        for i in range(2, n + 1):
-            arr[i] = arr[i - 1] + arr[i - 2]
-        return arr[n]
+class Solution {
+public:
+    int fib(int n) {
+        if (!n) return 0;
+        
+        int a[n + 1];
+        a[0] = 0;
+        a[1] = 1;
+        for (int i = 2; i <= n; ++i)
+            a[i] = a[i - 1] + a[i - 2];
+        return a[n];
+    }
+};
