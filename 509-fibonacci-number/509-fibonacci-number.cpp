e01486949f4c8f435cdcd338f9cea1f01e36bf24
Zheng Yong <angzhengyong@gmail.com>
2022-07-06 22:50:49 +0800

    Time: 2 ms (64.32%), Space: 5.8 MB (80.05%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int fib(int n) {
+        if (n == 0) return 0;
+        long x = 0, y = 1;
+        for (; n > 1; --n) {
+            y += x;
+            x = y - x;
+        }
+        return y;
+    }
+};
