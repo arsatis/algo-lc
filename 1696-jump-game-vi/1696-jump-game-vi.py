533b0aa95615c558c05043115ad9e6ae04c0a377
Zheng Yong <angzhengyong@gmail.com>
2022-07-09 13:30:44 +0800

    Time: 1744 ms (35.54%), Space: 28.1 MB (47.64%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -1,12 +1,11 @@
 class Solution:
     def maxResult(self, nums: List[int], k: int) -> int:
-        n = len(nums)
         dq = deque([0])
-        for i in range(1, n):
+        for i in range(1, len(nums)):
             nums[i] = nums[dq[0]] + nums[i]
             while dq and nums[dq[-1]] <= nums[i]:
                 dq.pop()
             dq.append(i)
             if i - dq[0] >= k:
                 dq.popleft()
-        return nums[n - 1]
+        return nums[-1]
