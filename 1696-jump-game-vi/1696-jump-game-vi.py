d9b906ed0000d481256af3d21422ff529ec08620
Zheng Yong <angzhengyong@gmail.com>
2022-07-09 13:52:06 +0800

    Time: 979 ms (98.74%), Space: 28.1 MB (38.99%) - LeetHub

--- 1696-jump-game-vi/1696-jump-game-vi.py
+++ 1696-jump-game-vi/1696-jump-game-vi.py
@@ -3,9 +3,7 @@ class Solution:
         dq = deque([0])
         for i in range(1, len(nums)):
             nums[i] = nums[dq[0]] + nums[i]
-            while dq and nums[dq[-1]] <= nums[i]:
-                dq.pop()
+            while dq and nums[dq[-1]] <= nums[i]: dq.pop()
             dq.append(i)
-            if i - dq[0] >= k:
-                dq.popleft()
+            if i - dq[0] >= k: dq.popleft()
         return nums[-1]
