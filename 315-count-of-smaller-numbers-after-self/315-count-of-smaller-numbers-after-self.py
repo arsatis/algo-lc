d73fb6c6c1d2be5073af71101031a26529385e4e
Zheng Yong <angzhengyong@gmail.com>
2022-07-23 14:16:18 +0800

    Time: 1987 ms (98.52%), Space: 31.6 MB (93.57%) - LeetHub

--- 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
+++ 315-count-of-smaller-numbers-after-self/315-count-of-smaller-numbers-after-self.py
@@ -1,10 +1,25 @@
 class Solution:
     def countSmaller(self, nums: List[int]) -> List[int]:
-        n = len(nums)
-        ans = [0] * n
-        sorted_list = [nums[-1]]
-        for i in range(n - 2, -1, -1):
-            idx = bisect.bisect_left(sorted_list, nums[i])
-            ans[i] = idx
-            bisect.insort_left(sorted_list, nums[i])
-        return ans
+        # fenwick tree O(nlogn)
+        def update(index, value, tree, size):
+            index += 1 # index in BIT is 1 more than the original index
+            while index < size:
+                tree[index] += value
+                index += index & -index
+
+        def query(index, tree):
+            # return sum of [0, index)
+            result = 0
+            while index >= 1:
+                result += tree[index]
+                index -= index & -index
+            return result
+        offset = 10 ** 4
+        size = 2 * 10** 4 + 2
+        tree = [0] * size
+        res = []
+        for num in reversed(nums):
+            smaller_count = query(num + offset, tree)
+            res.append(smaller_count)
+            update(num + offset, 1, tree, size)
+        return reversed(res)
