169f1ed0fc942f5ac8ebbd417ca35eb5d8f087ea
Zheng Yong <angzhengyong@gmail.com>
2022-07-31 00:04:43 +0800

    Time: 1924 ms (16.01%), Space: 18.9 MB (23.33%) - LeetHub

--- 916-word-subsets/916-word-subsets.py
+++ 916-word-subsets/916-word-subsets.py
@@ -1,5 +1,4 @@
 class Solution:
     def wordSubsets(self, words1: List[str], words2: List[str]) -> List[str]:
-        count = Counter()
-        for b in words2: count |= Counter(b)
-        return [a for a in words1 if not count - Counter(a)]
+        c_b = reduce(operator.or_, (Counter(w) for w in words2))
+        return [a for a in words1 if c_b & Counter(a) == c_b]
