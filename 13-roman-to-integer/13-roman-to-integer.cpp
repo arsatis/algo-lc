62a448603cc7370d1995bbb7a4a13c90aef0b8fe
Zheng Yong <angzhengyong@gmail.com>
2022-07-31 23:34:43 +0800

    Time: 9 ms (80.66%), Space: 6 MB (64.43%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -3,24 +3,24 @@ public:
     int romanToInt(string s) {
         char prev;
         int output = 0;
-        for (auto it = s.end() - 1; it != s.begin() - 1; --it) {
-            if (*it == 'M') output += 1000;
-            else if (*it == 'D') output += 500;
-            else if (*it == 'C') {
+        for (int i = s.size() - 1; i >= 0; --i) {
+            if (s[i] == 'M') output += 1000;
+            else if (s[i] == 'D') output += 500;
+            else if (s[i] == 'C') {
                 if (prev == 'M' || prev == 'D') output -= 100;
                 else output += 100;
             }
-            else if (*it == 'L') output += 50;
-            else if (*it == 'X') {
+            else if (s[i] == 'L') output += 50;
+            else if (s[i] == 'X') {
                 if (prev == 'C' || prev == 'L') output -= 10;
                 else output += 10;
             }
-            else if (*it == 'V') output += 5;
-            else if (*it == 'I') {
+            else if (s[i] == 'V') output += 5;
+            else if (s[i] == 'I') {
                 if (prev == 'X' || prev == 'V') output -= 1;
                 else output += 1;
             }
-            prev = *it;
+            prev = s[i];
         }
         return output;
     }
