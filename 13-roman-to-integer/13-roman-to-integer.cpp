7f67901a10a6cc726f67bd322efa0c477dde02f3
Zheng Yong <angzhengyong@gmail.com>
2022-07-31 23:30:53 +0800

    Time: 16 ms (58.16%), Space: 5.9 MB (86.62%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        char prev = '\0';
+        int output = 0;
+        for (auto it = s.end() - 1; it != s.begin() - 1; --it) {
+            if (*it == 'M') output += 1000;
+            else if (*it == 'D') output += 500;
+            else if (*it == 'C') {
+                if (prev == 'M' || prev == 'D') output -= 100;
+                else output += 100;
+            }
+            else if (*it == 'L') output += 50;
+            else if (*it == 'X') {
+                if (prev == 'C' || prev == 'L') output -= 10;
+                else output += 10;
+            }
+            else if (*it == 'V') output += 5;
+            else if (*it == 'I') {
+                if (prev == 'X' || prev == 'V') output -= 1;
+                else output += 1;
+            }
+            prev = *it;
+        }
+        return output;
+    }
+};
