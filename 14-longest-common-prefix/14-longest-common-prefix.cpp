d7da641fdd61afaa37d80466b7785dd22ff6f581
Zheng Yong <angzhengyong@gmail.com>
2022-08-06 12:07:44 +0800

    Time: 3 ms (92.59%), Space: 9.1 MB (94.94%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
+        ios_base::sync_with_stdio(0);
+
         string::size_type idx = 0;
         while (idx < strs[0].size()) {
             for (int i = 1; i < strs.size(); ++i)
