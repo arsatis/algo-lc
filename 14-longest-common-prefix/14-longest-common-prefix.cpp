3a1fef2e49c770f3cb091d2daee0bdaa6f3d5a8f
Zheng Yong <angzhengyong@gmail.com>
2022-08-06 12:00:00 +0800

    Time: 11 ms (24.75%), Space: 9.2 MB (82.59%) - LeetHub

--- 14-longest-common-prefix/14-longest-common-prefix.cpp
+++ 14-longest-common-prefix/14-longest-common-prefix.cpp
@@ -4,14 +4,12 @@ public:
         ios_base::sync_with_stdio(false);
 
         int idx = 0;
-        string s = "";
         while (idx < strs[0].size()) {
             for (int i = 1; i < strs.size(); ++i)
                 if (idx >= strs[i].size() || strs[i][idx] != strs[0][idx])
-                    return s;
-            s.push_back(strs[0][idx]);
+                    return strs[0].substr(0, idx);
             ++idx;
         }
-        return s;
+        return strs[0].substr(0, idx);
     }
 };
