0ad8a1a0f9bad10d37da46b5404bb82342d78372
Zheng Yong <angzhengyong@gmail.com>
2022-08-07 21:03:55 +0800

    Time: 0 ms (100.00%), Space: 6 MB (80.73%) - LeetHub

--- 458-poor-pigs/458-poor-pigs.cpp
+++ 458-poor-pigs/458-poor-pigs.cpp
@@ -1,7 +1,8 @@
 class Solution {
 public:
     int poorPigs(int buckets, int minutesToDie, int minutesToTest) {
-        int days = minutesToTest / minutesToDie, x = days + 1;
+        int days = minutesToTest / minutesToDie;
+        int x = days + 1;
         double a = log10(buckets), b = log10(x);
         return ceil(a / b);
     }
