5862da32e010c9a7c8e8d8f986656ad26c9201c9
Zheng Yong <angzhengyong@gmail.com>
2022-08-07 09:59:08 +0800

    Time: 3 ms (95.02%), Space: 6 MB (90.50%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,17 +1,20 @@
 class Solution {
 public:
     int countVowelPermutation(int n) {
-        long a = 1, e = 1, i = 1, o = 1, u = 1, mod = 1e9 + 7;
+        if (n == 1) return 5;
+        int mod = 1e9 + 7, i = 0;
+        long dp[2][5];
         
+        for (int j = 0; j < 5; ++j) dp[i][j] = 1;
         for (int j = 0; j < n - 1; ++j) {
-            long za = a, ze = e, zi = i, zo = o, zu = u;
-            a = (ze + zi + zu) % mod;
-            e = (za + zi) % mod;
-            i = (ze + zo) % mod;
-            o = zi % mod;
-            u = (zi + zo) % mod;
+            dp[1 - i][0] = (dp[i][1] + dp[i][2] + dp[i][4]) % mod;
+            dp[1 - i][1] = (dp[i][0] + dp[i][2]) % mod;
+            dp[1 - i][2] = (dp[i][1] + dp[i][3]) % mod;
+            dp[1 - i][3] = dp[i][2] % mod;
+            dp[1 - i][4] = (dp[i][2] + dp[i][3]) % mod;
+            i = 1 - i;
         }
-        
-        return (a + e + i + o + u) % mod;
+        for (int j = 1; j < 5; ++j) dp[i][0] += dp[i][j];
+        return dp[i][0] % mod;
     }
 };
