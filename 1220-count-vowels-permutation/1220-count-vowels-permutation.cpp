1e5018daafac1f617ee1b6360242befec2c5fb4a
Zheng Yong <angzhengyong@gmail.com>
2022-08-07 09:44:54 +0800

    Time: 3 ms (95.02%), Space: 5.9 MB (95.33%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -1,19 +1,17 @@
 class Solution {
 public:
     int countVowelPermutation(int n) {
-        vector<vector<long>> vvl(n, vector<long>(5, 0));
-        int mod = 1e9 + 7;
+        long a = 1, e = 1, i = 1, o = 1, u = 1, mod = 1e9 + 7;
         
-        for (int i = 0; i < 5; ++i) vvl[0][i] = 1;
-        for (int i = 0; i < n - 1; ++i) {
-            vvl[i + 1][0] = (vvl[i][1] + vvl[i][2] + vvl[i][4]) % mod;
-            vvl[i + 1][1] = (vvl[i][0] + vvl[i][2]) % mod;
-            vvl[i + 1][2] = (vvl[i][1] + vvl[i][3]) % mod;
-            vvl[i + 1][3] = vvl[i][2] % mod;
-            vvl[i + 1][4] = (vvl[i][2] + vvl[i][3]) % mod;
+        for (int j = 0; j < n - 1; ++j) {
+            long za = a, ze = e, zi = i, zo = o, zu = u;
+            a = (ze + zi + zu) % mod;
+            e = (za + zi) % mod;
+            i = (ze + zo) % mod;
+            o = zi % mod;
+            u = (zi + zo) % mod;
         }
         
-        for (int i = 1; i < 5; ++i) vvl[n - 1][0] += vvl[n - 1][i];
-        return vvl[n - 1][0] % mod;
+        return (a + e + i + o + u) % mod;
     }
 };
