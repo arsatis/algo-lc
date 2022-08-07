2f70dd58efa4d25f459638e239a023d8f37d55cf
Zheng Yong <angzhengyong@gmail.com>
2022-08-07 11:10:48 +0800

    Time: 112 ms (94.12%), Space: 33.8 MB (98.08%) - LeetHub

--- 731-my-calendar-ii/731-my-calendar-ii.cpp
+++ 731-my-calendar-ii/731-my-calendar-ii.cpp
@@ -1,14 +1,20 @@
 class MyCalendarTwo {
-    vector<pair<int, int>> overlap, calendar;
+    vector<pair<int, int>> o, c;
 public:
     MyCalendarTwo() {
         ios_base::sync_with_stdio(false);
     }
     
     bool book(int start, int end) {
-        for (auto &date : overlap) if (start < date.second && end > date.first) return false;
-        for (auto &date : calendar) if (start < date.second && end > date.first) overlap.emplace_back(max(start, date.first), min(end, date.second));
-        calendar.emplace_back(start, end);
+        for (auto &date : o)
+            if (start < date.second && end > date.first)
+                return false;
+        
+        for (auto &date : c)
+            if (start < date.second && end > date.first)
+                o.emplace_back(max(start, date.first), min(end, date.second));
+        
+        c.emplace_back(start, end);
         return true;
     }
 };
