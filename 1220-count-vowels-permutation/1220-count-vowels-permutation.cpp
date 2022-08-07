a00b077d5fb0f28513838bf7f303f91b9ae5b04b
Zheng Yong <angzhengyong@gmail.com>
2022-08-07 10:07:02 +0800

    Time: 3 ms (95.02%), Space: 5.8 MB (99.22%) - LeetHub

--- 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
+++ 1220-count-vowels-permutation/1220-count-vowels-permutation.cpp
@@ -4,6 +4,7 @@ public:
         if (n == 1) return 5;
         int mod = 1e9 + 7, i = 0;
         long dp[2][5];
+        
         for (int j = 0; j < 5; ++j) dp[i][j] = 1;
         for (int j = 0; j < n - 1; ++j) {
             dp[1 - i][0] = (dp[i][1] + dp[i][2] + dp[i][4]) % mod;
@@ -13,6 +14,7 @@ public:
             dp[1 - i][4] = (dp[i][2] + dp[i][3]) % mod;
             i = 1 - i;
         }
+        
         for (int j = 1; j < 5; ++j) dp[i][0] += dp[i][j];
         return dp[i][0] % mod;
     }
