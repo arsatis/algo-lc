6d2b776550f42f92cae3f67e6efde383adda6c7f
Zheng Yong <angzhengyong@gmail.com>
2022-08-11 23:48:10 +0800

    Time: 248 ms (35.25%), Space: 71.9 MB (18.74%) - LeetHub

--- 687-longest-univalue-path/687-longest-univalue-path.cpp
+++ 687-longest-univalue-path/687-longest-univalue-path.cpp
@@ -12,14 +12,19 @@
 class Solution {
 public:
     int longestUnivaluePath(TreeNode* root) {
-        if (!root) return 0;
-        int lh = helper(root->left, root->val);
-        int rh = helper(root->right, root->val);
-        return max(lh + rh, max(longestUnivaluePath(root->left), longestUnivaluePath(root->right)));
+        ios::sync_with_stdio(0);
+        int mx = 0;
+        helper(root, mx);
+        return mx;
     }
     
-    int helper(TreeNode* node, int val) {
-        if (!node || node->val != val) return 0;
-        return 1 + max(helper(node->left, val), helper(node->right, val));
+    int helper(TreeNode* node, int& mx) {
+        if (!node) return 0;
+        int leftPath = helper(node->left, mx),
+            rightPath = helper(node->right, mx);
+        int left = node->left && node->left->val == node->val ? leftPath + 1 : 0,
+            right = node->right && node->right->val == node->val ? rightPath + 1 : 0;
+        mx = max(mx, left + right);
+        return max(left, right);
     }
 };
