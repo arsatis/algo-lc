9d39245cad30e942d088e16b6a6f5f43d6957779
Zheng Yong <angzhengyong@gmail.com>
2022-08-13 23:04:00 +0800

    Time: 7 ms (87.59%), Space: 7.2 MB (94.25%) - LeetHub

--- 242-valid-anagram/242-valid-anagram.cpp
+++ 242-valid-anagram/242-valid-anagram.cpp
@@ -1,9 +1,9 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        vector<int> m1(256, 0), m2(256, 0);
-        for (int i = 0; i < s.size(); ++i) ++m1[s[i]];
-        for (int i = 0; i < t.size(); ++i) ++m2[t[i]];
+        vector<int> m1(29, 0), m2(29, 0);
+        for (int i = 0; i < s.size(); ++i) ++m1[s[i] - 'a'];
+        for (int i = 0; i < t.size(); ++i) ++m2[t[i] - 'a'];
         return m1 == m2;
     }
 };
