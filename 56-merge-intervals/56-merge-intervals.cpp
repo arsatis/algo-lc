f1fa5993b8d97358bc18737be86adb58adf3f441
Zheng Yong <angzhengyong@gmail.com>
2022-08-14 11:05:23 +0800

    Time: 59 ms (55.91%), Space: 18.9 MB (84.66%) - LeetHub

---
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        sort(intervals.begin(), intervals.end(), [](auto& i, auto& j) {
+            return i[1] < j[1];
+        });
+        vector<vector<int>> output;
+        for (int i = intervals.size() - 1; i >= 0; --i) {
+            if (output.size() == 0 || output.back()[0] > intervals[i][1])
+                output.push_back(intervals[i]);
+            else
+                output.back()[0] = min(output.back()[0], intervals[i][0]);
+        }
+        return output;
+    }
+};
