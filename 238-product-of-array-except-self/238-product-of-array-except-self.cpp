21f5504bea661b18477008942f1bfccd9ed78aaa
Zheng Yong <angzhengyong@gmail.com>
2022-08-14 10:41:35 +0800

    Time: 38 ms (56.30%), Space: 24.1 MB (61.12%) - LeetHub

--- 238-product-of-array-except-self/238-product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/238-product-of-array-except-self.cpp
@@ -6,8 +6,9 @@ public:
         cout.tie(0);
         
         vector<int> ans(nums.size(), 1);
-        int temp = 1;
         for (int i = 1; i < nums.size(); ++i) ans[i] = ans[i - 1] * nums[i - 1];
+        
+        int temp = 1;
         for (int i = nums.size() - 2; i >= 0; --i) {
             temp *= nums[i + 1];
             ans[i] *= temp;
