01cdcd5e02aef0e88c2a338f436c03579209da54
Zheng Yong <angzhengyong@gmail.com>
2022-08-16 16:42:00 +0800

    Time: 41 ms (71.29%), Space: 10.8 MB (44.39%) - LeetHub

--- 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
+++ 387-first-unique-character-in-a-string/387-first-unique-character-in-a-string.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         unordered_map<int, int> charCount;
         for (auto& c : s) ++charCount[c];
         for (int i = 0; i < s.size(); ++i)
