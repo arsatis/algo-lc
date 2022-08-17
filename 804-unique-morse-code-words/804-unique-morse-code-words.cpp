76125fa84dbf5303adc868201b45bf1f59117dcd
Zheng Yong <angzhengyong@gmail.com>
2022-08-17 20:53:16 +0800

    Time: 0 ms (100.00%), Space: 8.5 MB (61.19%) - LeetHub

--- 804-unique-morse-code-words/804-unique-morse-code-words.cpp
+++ 804-unique-morse-code-words/804-unique-morse-code-words.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int uniqueMorseRepresentations(vector<string>& words) {
-        ios_base::sync_with_stdio(0);
-        
         string mappings[26] = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
         unordered_set<string> unique;
         for (string& w : words) {
