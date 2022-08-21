e95e0f7e7458f104d40eb80b8ab1be63f4a0662c
Zheng Yong <angzhengyong@gmail.com>
2022-08-21 17:46:54 +0800

    Time: 44 ms (76.06%), Space: 12.2 MB (83.02%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int numIslands(vector<vector<char>>& grid) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         int num = 0;
         for (int i = 0; i < grid.size(); ++i)
             for (int j = 0; j < grid[0].size(); ++j)
