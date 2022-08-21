e9d8f24cafa0c17aabbd2a3b19b765b6529b4c6e
Zheng Yong <angzhengyong@gmail.com>
2022-08-21 11:12:13 +0800

    Time: 16 ms (14.83%), Space: 8.6 MB (10.31%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -12,23 +12,13 @@ class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
         ios_base::sync_with_stdio(0);
-        
-        if (!head) return nullptr;
-        
-        stack<ListNode*> s;
-        while (head) {
-            s.push(head);
-            head = head->next;
-        }
-        
-        ListNode* dummy = new ListNode();
-        ListNode* curr = dummy;
-        while (s.size() > 0) {
-            curr->next = s.top();
-            s.pop();
-            curr = curr->next;
-        }
-        curr->next = nullptr;
-        return dummy->next;
+        return helper(nullptr, head);
+    }
+    
+    ListNode* helper(ListNode* prev, ListNode* curr) {
+        if (!curr) return prev;
+        ListNode* temp = curr->next;
+        curr->next = prev;
+        return helper(curr, temp);
     }
 };
