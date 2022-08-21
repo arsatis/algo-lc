8e00f5ced79b9d26cf39f0d724d3c97faaa280b7
Zheng Yong <angzhengyong@gmail.com>
2022-08-21 14:26:07 +0800

    Time: 149 ms (47.34%), Space: 59.4 MB (37.78%) - LeetHub

--- 11-container-with-most-water/11-container-with-most-water.cpp
+++ 11-container-with-most-water/11-container-with-most-water.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int maxArea(vector<int>& height) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         int left = 0, right = height.size() - 1, mx = 0;
         while (left < right) {
