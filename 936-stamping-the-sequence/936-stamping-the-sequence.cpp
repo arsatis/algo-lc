4c8bb08dcd0367d643e202b836a49190f05f8a2c
Zheng Yong <angzhengyong@gmail.com>
2022-08-21 10:47:02 +0800

    Time: 11 ms (87.70%), Space: 7.6 MB (62.30%) - LeetHub

--- 936-stamping-the-sequence/936-stamping-the-sequence.cpp
+++ 936-stamping-the-sequence/936-stamping-the-sequence.cpp
@@ -1,24 +1,42 @@
 class Solution {
 public:
-    vector<int> movesToStamp(string stamp, string target) {
-        vector<int> res;
-        auto total_stamp = 0, turn_stamp = -1;
-        while (turn_stamp) {
-            turn_stamp = 0;
-            for (int sz = stamp.size(); sz > 0; --sz) 
-                for (auto i = 0; i <= stamp.size() - sz; ++i) {
-                    auto new_stamp = string(i, '*') + stamp.substr(i, sz) + string(stamp.size() - sz - i, '*');
-                    auto pos = target.find(new_stamp);
-                    while (pos != string::npos) {
-                        res.push_back(pos);
-                        turn_stamp += sz;
-                        fill(begin(target) + pos, begin(target) + pos + stamp.size(), '*');
-                        pos = target.find(new_stamp);
-                    }
-                }
-            total_stamp += turn_stamp;
-        }
-        reverse(begin(res), end(res));
-        return total_stamp == target.size() ? res : vector<int>();
-    }
+	bool replaceable(int i, string &t, string &s) {
+		for (int j = 0; j < s.size(); ++j, ++i) {
+			if (t[i] == '*' || s[j] == t[i]) continue;
+			return false;
+		}
+		return true;
+	}
+    
+	int replace(int i, string &t, string &s) {
+		int cnt = 0;
+		for (int j = 0; j < s.size(); ++j, ++i) {
+			if (t[i] == '*') continue;
+			t[i] = '*';
+			++cnt;
+		}
+		return cnt;
+	}
+    
+	vector<int> movesToStamp(string &s, string &t) {
+		int st = 0;
+		vector<bool> visited(t.size(), false);
+		vector<int> res;
+        
+		while (st < t.size()) {
+			bool flag = false;
+			for (int i = 0; i <= t.size() - s.size() && st < t.size(); ++i) {
+				if (visited[i]) continue;
+				if (replaceable(i, t, s)) {
+					st += replace(i, t, s);
+					visited[i] = true;
+					flag = true;
+					res.push_back(i);
+				}
+			}
+			if (!flag) return *new vector<int>();
+		}
+		reverse(res.begin(), res.end());
+		return res;
+	}
 };
