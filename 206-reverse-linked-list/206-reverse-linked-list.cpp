3a129ed7955e79de54df7acbb963b20e967f2af9
Zheng Yong <angzhengyong@gmail.com>
2022-08-21 11:12:55 +0800

    Time: 11 ms (47.30%), Space: 8.4 MB (42.11%) - LeetHub

--- 206-reverse-linked-list/206-reverse-linked-list.cpp
+++ 206-reverse-linked-list/206-reverse-linked-list.cpp
@@ -11,6 +11,9 @@
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         return helper(nullptr, head);
     }
     
