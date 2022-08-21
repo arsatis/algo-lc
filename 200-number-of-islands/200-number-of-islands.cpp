8dfd1664ce8427b488665aa22f624d78fbd4e359
Zheng Yong <angzhengyong@gmail.com>
2022-08-21 17:46:23 +0800

    Time: 50 ms (62.46%), Space: 12.4 MB (55.20%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int numIslands(vector<vector<char>>& grid) {
+        int num = 0;
+        for (int i = 0; i < grid.size(); ++i)
+            for (int j = 0; j < grid[0].size(); ++j)
+                num += bfs(grid, i, j);
+        return num;
+    }
+    
+    int bfs(vector<vector<char>>& grid, int i, int j) {
+        if (grid[i][j] == '0') return 0;
+        grid[i][j] = '0';
+        if (i > 0) bfs(grid, i - 1, j);
+        if (i < grid.size() - 1) bfs(grid, i + 1, j);
+        if (j > 0) bfs(grid, i, j - 1);
+        if (j < grid[0].size() - 1) bfs(grid, i, j + 1);
+        return 1;
+    }
+};
