4c8b20834a3754a55d95e9b8c7d10711daa0442b
Zheng Yong <angzhengyong@gmail.com>
2022-08-21 17:07:56 +0800

    Time: 9 ms (83.56%), Space: 7 MB (51.25%) - LeetHub

--- 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
+++ 424-longest-repeating-character-replacement/424-longest-repeating-character-replacement.cpp
@@ -4,15 +4,14 @@ public:
         int count[26];
         fill(count, count + 26, 0);
         
-        int start = 0, max_count = 0, max_length = 0;
+        int start = 0, max_count = 0;
         for (int end = 0; end < s.size(); ++end) {
             max_count = max(max_count, ++count[s[end] - 'A']);
             if (end - start + 1 - max_count > k) {
                 --count[s[start] - 'A'];
                 ++start;
             }
-            max_length = max(max_length, end - start + 1);
         }
-        return max_length;
+        return s.size() - start;
     }
 };
