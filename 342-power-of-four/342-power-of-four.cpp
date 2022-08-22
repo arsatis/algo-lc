c2a514f4e48fc248eec34cda20f2f11ab7f72767
Zheng Yong <angzhengyong@gmail.com>
2022-08-22 13:34:08 +0800

    Time: 0 ms (100.00%), Space: 6 MB (24.97%) - LeetHub

--- 342-power-of-four/342-power-of-four.cpp
+++ 342-power-of-four/342-power-of-four.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     bool isPowerOfFour(int n) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
         if (n < 1) return false;
         while (n != 1) {
             if (n % 4 != 0) return false;
