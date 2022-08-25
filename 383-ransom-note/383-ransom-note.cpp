730138b5ff131c7555ab7d030bdaadfa80724a5b
Zheng Yong <angzhengyong@gmail.com>
2022-08-25 20:03:51 +0800

    Time: 3 ms (99.98%), Space: 8.7 MB (72.69%) - LeetHub

--- 383-ransom-note/383-ransom-note.cpp
+++ 383-ransom-note/383-ransom-note.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool canConstruct(string ransomNote, string magazine) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         int chars[26] = { 0 };
         for (char& c : magazine) ++chars[c - 'a'];
