3e431482adb40668cf9a7b7447a0fcace6d6c8cf
Zheng Yong <angzhengyong@gmail.com>
2022-08-25 22:58:51 +0800

    Time: 0 ms (100.00%), Space: 6.3 MB (84.63%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.cpp
+++ 647-palindromic-substrings/647-palindromic-substrings.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int countSubstrings(string s) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         int num = 0;
         for (int i = 0; i < (s.size() << 1); ++i) {
             int left = i >> 1, right = (i + 1) >> 1, size = 0;
