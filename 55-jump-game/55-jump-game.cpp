fa91a21310a6557f4d02149fa72b5ddfe5619b54
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 16:04:57 +0800

    Time: 114 ms (51.05%), Space: 48.5 MB (45.07%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        
         int n = nums.size() - 1;
         for (int i = n - 1; i >= 0; --i)
             if (nums[i] + i >= n) n = i;
