b809900ab59c873cda2f480da7d1f4982026e04a
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 16:23:41 +0800

    Time: 3 ms (89.47%), Space: 8.6 MB (47.12%) - LeetHub

--- 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         vector<string> ans;
         
