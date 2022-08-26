6d560ccdaa7aebdfedacf399a8167dbf47c4136a
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 14:54:21 +0800

    Time: 7 ms (90.55%), Space: 6 MB (66.08%) - LeetHub

--- 13-roman-to-integer/13-roman-to-integer.cpp
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -12,8 +12,6 @@ class Solution {
 public:
     int romanToInt(string s)  {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         int output = 0;
         for (int i = 0; i < s.size(); ++i) {
