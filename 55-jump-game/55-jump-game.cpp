e350b2a242df812c49789fb6cd4d30865abfc6a3
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 16:04:41 +0800

    Time: 101 ms (62.79%), Space: 48.5 MB (45.07%) - LeetHub

--- 55-jump-game/55-jump-game.cpp
+++ 55-jump-game/55-jump-game.cpp
@@ -1,8 +1,9 @@
 class Solution {
 public:
     bool canJump(vector<int>& nums) {
-        int i = 0;
-        for (int reach = 0; i < nums.size() && i <= reach; ++i) reach = max(i + nums[i], reach);
-        return i == nums.size();
+        int n = nums.size() - 1;
+        for (int i = n - 1; i >= 0; --i)
+            if (nums[i] + i >= n) n = i;
+        return n <= 0;
     }
 };
