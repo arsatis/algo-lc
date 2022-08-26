9a9215a3899940184a112fa8cd751bbeae55853b
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 15:48:55 +0800

    Time: 14 ms (40.89%), Space: 13.8 MB (25.90%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.cpp
+++ 152-maximum-product-subarray/152-maximum-product-subarray.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int maxProduct(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         int ans = nums[0], maxProd = ans, minProd = ans;
 
