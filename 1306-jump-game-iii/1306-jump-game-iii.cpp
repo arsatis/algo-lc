a40a42fddcdc43f40df5eff9798bbc7a3dcc81aa
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 14:59:55 +0800

    Time: 48 ms (91.67%), Space: 30.8 MB (95.75%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool canReach(vector<int>& arr, int start) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         if (start < 0 || start >= arr.size() || arr[start] < 0) return false;
         arr[start] = -arr[start];
         return !arr[start] || canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]);
