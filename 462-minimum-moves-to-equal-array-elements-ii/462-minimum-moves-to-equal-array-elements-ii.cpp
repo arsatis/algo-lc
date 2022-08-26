190bc5d5a3495a721ffa6dcd053f249c22337dcc
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 15:29:08 +0800

    Time: 12 ms (83.34%), Space: 11.9 MB (6.54%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int minMoves2(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        
         int n = nums.size(), steps = 0;
         nth_element(nums.begin(), nums.begin() + (n / 2), nums.end());
         int median = nums[n / 2];
