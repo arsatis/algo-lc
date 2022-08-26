a06d59697bc434cdc0764b3f08fde27f20e8e66c
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 16:23:05 +0800

    Time: 8 ms (43.28%), Space: 8.4 MB (58.17%) - LeetHub

---
+++ 890-find-and-replace-pattern/890-find-and-replace-pattern.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<string> findAndReplacePattern(vector<string>& words, string pattern) {
+        vector<string> ans;
+        
+        for (string& i : words) {
+            map<char, int> mp1, mp2;
+            int cn = 0;
+            bool temp = true;
+            for (char& j : i) {
+                if (mp1.find(pattern[cn]) == mp1.end() && mp2.find(j) == mp2.end()) {
+                    mp1[pattern[cn]] = j;
+                    mp2[j] = pattern[cn];
+                } else if (mp1[pattern[cn]] != j) {
+                    temp = false;
+                    break;
+                }
+                ++cn;
+            }
+            if (temp) ans.push_back(i);
+        }
+        return ans;
+    }
+};
