9004d0929639a3cf7604e78d7ec78b58b8fbf97f
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 14:57:20 +0800

    Time: 103 ms (16.74%), Space: 30.6 MB (99.98%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    bool canReach(vector<int>& A, int i) {
-        return 0 <= i && i < A.size() && A[i] >= 0 && (!(A[i] = -A[i]) || canReach(A, i + A[i]) || canReach(A, i - A[i]));
+    bool canReach(vector<int>& arr, int start) {
+        return (0 <= start && start < arr.size() && arr[start] >= 0 && (!(arr[start] =- arr[start]) || canReach(arr, start + arr[start]) || canReach(arr, start - arr[start])));
     }
 };
