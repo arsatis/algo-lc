e9acf93bdef9a4279c95b3820f3c282fdb305236
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 14:48:08 +0800

    Time: 0 ms (100.00%), Space: 7.2 MB (44.46%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.cpp
+++ 10-regular-expression-matching/10-regular-expression-matching.cpp
@@ -1,39 +1,23 @@
 class Solution {
 public:
-    bool check(int i, int j, const string &text, const string &pattern, vector<vector<int>> &lookupTable){
-        if (i >= text.size() && j >= pattern.size()) return true;
-        if (i >= text.size()) {
-            if (pattern[j] == '*') return check(i, j + 1, text, pattern, lookupTable);
-            else if (j + 1 < pattern.size() && pattern[j + 1] == '*') return check(i, j + 2, text, pattern, lookupTable);
-            else return false;
-        }
-        if (j >= pattern.size()) return false;
-        if (lookupTable[i][j] != -1) return lookupTable[i][j] == 1 ? true : false;
-        
-        bool match = false;
-        if (j + 1 < pattern.size() && pattern[j + 1]=='*') {
-            if (text[i] == pattern[j] || pattern[j] == '.') {
-                match |= check(i + 1, j + 2, text, pattern, lookupTable);
-                match |= check(i + 1, j, text, pattern, lookupTable);
-            }
-            match |= check(i, j + 2, text, pattern, lookupTable);
-        } else if (pattern[j] == '.' || text[i] == pattern[j]) {
-            match |= check(i + 1, j + 1, text, pattern, lookupTable);
-        } else {
-            lookupTable[i][j] = match ? 1 : 0; 
-            return lookupTable[i][j] == 1 ? true: false;
-        }
-        
-        lookupTable[i][j] = match ? 1 : 0; 
-        return lookupTable[i][j] == 1 ? true : false;
+    bool isMatch(string s, string p) {
+        vector<vector<int>> memo(s.size()+ 1, vector<int>(p.size() + 1));
+        return dp(s, p, 0, 0, memo);
     }
     
-    bool isMatch(string &s, string &p) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
+    bool dp(const string& s, const string& p, int i, int j, vector<vector<int>>& memo) {
+        if (j >= p.size()) return i == s.size();
+        bool is_match = i < s.size() && (s[i] == p[j] || p[j] == '.');
+        bool res;
         
-        vector<vector<int>> lookupTable(s.size(), vector<int>(p.size(), -1));
-        return check(0, 0, s, p, lookupTable);   
+        if (memo[i][j] != 0) return memo[i][j] == 1 ? true : false;
+        
+        if (j + 1 < p.size() && p[j + 1] == '*') res = dp(s, p, i, j + 2, memo) || (is_match && dp(s, p, i + 1, j, memo));
+        else res = is_match && dp(s, p, i + 1, j + 1, memo);
+        
+        if (res) memo[i][j] = 1;
+        else memo[i][j] = 2;
+        
+        return res;
     }
 };
