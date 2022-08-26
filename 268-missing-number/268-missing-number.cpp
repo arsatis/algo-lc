c49e6b3bc9fbf995e98209bd01ef42c5c548f71d
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 15:26:03 +0800

    Time: 14 ms (98.48%), Space: 18.2 MB (8.20%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     int missingNumber(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
         int ans = nums.size();
         for (int i = 0; i < nums.size(); ++i) ans ^= (nums[i] ^ i);
         return ans;
