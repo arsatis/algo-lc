685dc9c74dccc89462fda6724f4223cf06641a27
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 14:26:55 +0800

    Time: 43 ms (98.66%), Space: 30.2 MB (80.79%) - LeetHub

--- 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
+++ 974-subarray-sums-divisible-by-k/974-subarray-sums-divisible-by-k.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int subarraysDivByK(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         vector<int> count(k, 0);
         int sum = 0;
         for (auto& x : nums) {
