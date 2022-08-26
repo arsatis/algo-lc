31e7650e4bb69818912a71bdbf9e9dcef06d5d59
Zheng Yong <angzhengyong@gmail.com>
2022-08-26 14:59:36 +0800

    Time: 68 ms (61.35%), Space: 30.6 MB (99.58%) - LeetHub

--- 1306-jump-game-iii/1306-jump-game-iii.cpp
+++ 1306-jump-game-iii/1306-jump-game-iii.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
-    bool f(int i, vector<int>& arr, vector<int>& vis){
-        if (i >= arr.size() || i < 0) return false;
-        if (arr[i] == 0) return true;
-        if (vis[i]) return false;
-        
-        vis[i] = true;
-        if (f(i + arr[i], arr, vis)) return true;
-        if (f(i - arr[i], arr, vis)) return true;
-        return false;
-    }
     bool canReach(vector<int>& arr, int start) {
-        vector<int> vis(arr.size(), 0);
-        return f(start, arr, vis);
+        if (start < 0 || start >= arr.size() || arr[start] < 0) return false;
+        arr[start] = -arr[start];
+        return !arr[start] || canReach(arr, start + arr[start]) || canReach(arr, start - arr[start]);
     }
 };
