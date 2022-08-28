aed19848287b7b744e5ca150a5a43022a1ec646a
Zheng Yong <angzhengyong@gmail.com>
2022-08-28 19:47:42 +0800

    Time: 23 ms (83.11%), Space: 5.7 MB (88.84%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -2,6 +2,7 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         if (n < 1) return false;
+        else if (n > 100000) return 1162261467 % n == 0;
         while (n != 1) {
             if (n % 3 != 0) return false;
             n /= 3;
