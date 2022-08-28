1acaff054c1def4a4ef1e9ac9fe6345e2436320e
Zheng Yong <angzhengyong@gmail.com>
2022-08-28 14:24:45 +0800

    Time: 17 ms (57.89%), Space: 9.4 MB (52.67%) - LeetHub

---
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    void f(int m, int n, int i, int j, vector<vector<int>>& mat) {
+        int x = i, y = j;
+        vector<int> v;
+        while (x < n && y < m) {
+            v.emplace_back(mat[x][y]);
+            ++x, ++y;
+        }
+        sort(v.begin(), v.end());
+
+        x = i, y = j;
+        int cnt = 0;
+        while (x < n && y < m) {
+            mat[x][y] = v[cnt++];
+            ++x, ++y;
+        }
+    }
+    
+    vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
+        ios_base::sync_with_stdio(0);
+        
+        for (int i = 0, j = 0; j < mat[0].size(); ++j)
+            f(mat[0].size(), mat.size(), i, j, mat);
+        for (int i = 0, j = 0; i < mat.size(); ++i)
+            f(mat[0].size(), mat.size(), i, j, mat);
+        return mat;
+    }
+};
