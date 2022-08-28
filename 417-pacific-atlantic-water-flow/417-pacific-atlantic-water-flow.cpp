73c0166dbc15619219fbbd179f64c9540316cd37
Zheng Yong <angzhengyong@gmail.com>
2022-08-28 15:55:00 +0800

    Time: 55 ms (77.49%), Space: 17.9 MB (68.77%) - LeetHub

--- 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
+++ 417-pacific-atlantic-water-flow/417-pacific-atlantic-water-flow.cpp
@@ -3,19 +3,28 @@ class Solution {
         if ((matrix[i][j] & ocean) == ocean) return;
         matrix[i][j] += ocean;
         
-        if (i > 0 && (matrix[i - 1][j] & ocean) != ocean && heights[i - 1][j] >= heights[i][j])
+        // left
+        if (i > 0 && (matrix[i - 1][j] & ocean) != ocean
+                  && heights[i - 1][j] >= heights[i][j])
             bfs(heights, matrix, ocean, i - 1, j);
-        if (i < matrix.size() - 1 && (matrix[i + 1][j] & ocean) != ocean && heights[i + 1][j] >= heights[i][j])
+        // right
+        if (i < matrix.size() - 1 && (matrix[i + 1][j] & ocean) != ocean
+                                  && heights[i + 1][j] >= heights[i][j])
             bfs(heights, matrix, ocean, i + 1, j);
-        if (j > 0 && (matrix[i][j - 1] & ocean) != ocean && heights[i][j - 1] >= heights[i][j])
+        // up
+        if (j > 0 && (matrix[i][j - 1] & ocean) != ocean
+                  && heights[i][j - 1] >= heights[i][j])
             bfs(heights, matrix, ocean, i, j - 1);
-        if (j < matrix[0].size() - 1 && (matrix[i][j + 1] & ocean) != ocean && heights[i][j + 1] >= heights[i][j])
+        // down
+        if (j < matrix[0].size() - 1 && (matrix[i][j + 1] & ocean) != ocean
+                                     && heights[i][j + 1] >= heights[i][j])
             bfs(heights, matrix, ocean, i, j + 1);
     }
 public:
     vector<vector<int>> pacificAtlantic(vector<vector<int>>& heights) {
-        vector<vector<int>> matrix(heights.size(), vector<int>(heights[0].size()));
+        ios_base::sync_with_stdio(0);
         
+        vector<vector<int>> matrix(heights.size(), vector<int>(heights[0].size()));
         for (int i = 0; i < matrix.size(); ++i) {
             bfs(heights, matrix, 1, i, 0);
             bfs(heights, matrix, 2, i, matrix[0].size() - 1);
