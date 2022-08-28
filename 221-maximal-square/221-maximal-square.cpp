9ae897fb1b2c02e088a9cf5d32f8c7b8d896ce5a
Zheng Yong <angzhengyong@gmail.com>
2022-08-28 20:21:06 +0800

    Time: 84 ms (72.82%), Space: 19 MB (7.42%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,19 +1,25 @@
 class Solution {
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int dp[matrix.size() + 1][matrix[0].size() + 1];
+        int dp[matrix.size()][matrix[0].size()];
         int maxSide = 0;
         
-        for (int i = 0; i < matrix.size(); ++i) dp[i][0] = 0;
-        for (int i = 1; i < matrix[0].size(); ++i) dp[0][i] = 0;
-        
         for (int i = 0; i < matrix.size(); ++i) {
-            for (int j = 0; j < matrix[0].size(); ++j) {
+            dp[i][0] = matrix[i][0] - '0';
+            if (dp[i][0]) maxSide = 1;
+        }
+        for (int i = 1; i < matrix[0].size(); ++i) {
+            dp[0][i] = matrix[0][i] - '0';
+            if (dp[0][i]) maxSide = 1;
+        }
+        
+        for (int i = 1; i < matrix.size(); ++i) {
+            for (int j = 1; j < matrix[0].size(); ++j) {
                 if (matrix[i][j] == '1') {
-                    dp[i + 1][j + 1] = 1 + min({dp[i][j], dp[i + 1][j], dp[i][j + 1]});
-                    maxSide = max(maxSide, dp[i + 1][j + 1]);
+                    dp[i][j] = 1 + min({dp[i - 1][j - 1], dp[i - 1][j], dp[i][j - 1]});
+                    maxSide = max(maxSide, dp[i][j]);
                 } else {
-                    dp[i + 1][j + 1] = 0;
+                    dp[i][j] = 0;
                 }
             }
         }
