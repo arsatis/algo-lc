045bb08d0f31d960f745ea6b65d21062b4983e9e
Zheng Yong <angzhengyong@gmail.com>
2022-08-28 22:07:08 +0800

    Time: 0 ms (100.00%), Space: 7 MB (26.90%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.cpp
+++ 228-summary-ranges/228-summary-ranges.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     vector<string> summaryRanges(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         vector<string> result;
         int n = nums.size();
