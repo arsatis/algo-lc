8ac044233138aaa998722aa220a1b9973137661a
Zheng Yong <angzhengyong@gmail.com>
2022-08-28 19:48:05 +0800

    Time: 18 ms (91.71%), Space: 6.1 MB (15.14%) - LeetHub

--- 326-power-of-three/326-power-of-three.cpp
+++ 326-power-of-three/326-power-of-three.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     bool isPowerOfThree(int n) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         if (n < 1) return false;
         else if (n > 100000) return 1162261467 % n == 0;
