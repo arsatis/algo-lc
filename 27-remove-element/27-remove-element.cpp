8efe63a14d9725b7d4d4156c87ecb1325420e456
Zheng Yong <angzhengyong@gmail.com>
2022-08-28 21:56:49 +0800

    Time: 0 ms (100.00%), Space: 8.9 MB (35.53%) - LeetHub

--- 27-remove-element/27-remove-element.cpp
+++ 27-remove-element/27-remove-element.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int removeElement(vector<int>& nums, int val) {
-        ios_base::sync_with_stdio(0);
-        
 		int length = nums.size(), index = 0;
 		while (index < length) {
             if (nums[index] != val) ++index;
