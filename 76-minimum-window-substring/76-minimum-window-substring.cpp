4282773c731db864042d2764db1a3036ee23bd9a
Zheng Yong <angzhengyong@gmail.com>
2022-08-28 17:55:45 +0800

    Time: 7 ms (96.32%), Space: 7.5 MB (98.34%) - LeetHub

---
+++ 76-minimum-window-substring/76-minimum-window-substring.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        int left = 0, right = 0, minLeft = 0, minRight = 0, count = t.size();
+        int arr[64] = { 0 };
+        
+        for (char& c : t) ++arr[c - 'A'];
+        while (right < s.size()) {
+            count -= (arr[s[right] - 'A'] > 0);
+            --arr[s[right] - 'A'];
+            ++right;
+            if (count == 0) {
+                while (left < right && arr[s[left] - 'A'] < 0) {
+                    ++arr[s[left] - 'A'];
+                    ++left;
+                }
+                if (!minRight || minRight - minLeft > right - left) {
+                    minLeft = left;
+                    minRight = right;
+                }
+            }
+        }
+        return s.substr(minLeft, minRight - minLeft);
+    }
+};
