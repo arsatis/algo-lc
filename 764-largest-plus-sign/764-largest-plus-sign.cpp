91ff8dfc4c7baa4b5862df423b150a17fee1731f
Zheng Yong <angzhengyong@gmail.com>
2022-08-28 21:50:04 +0800

    Time: 112 ms (98.30%), Space: 22.8 MB (93.02%) - LeetHub

--- 764-largest-plus-sign/764-largest-plus-sign.cpp
+++ 764-largest-plus-sign/764-largest-plus-sign.cpp
@@ -1,22 +1,37 @@
 class Solution {
 public:
     int orderOfLargestPlusSign(int n, vector<vector<int>>& mines) {
-        ios_base::sync_with_stdio(0);
+        if (mines.size() == n * n) return 0;
+        if (mines.size() > n * n - 5) return 1;
         
-        vector<vector<int>> grid(n, vector<int>(n, n));
-        for (auto& mine : mines) grid[mine[0]][mine[1]] = 0;
+        int grid[n][n], arm[n][n][4];
+        memset(grid, -1, sizeof(grid));
+        memset(arm, 0, sizeof(arm));
+        
+        for (auto& v : mines) grid[v[0]][v[1]] = 0;
         for (int i = 0; i < n; ++i) {
-            int l = 0, r = 0, u = 0, d = 0;
-            for (int j = 0, k = n - 1; j < n; ++j, --k) {
-                grid[i][j] = min(grid[i][j], l = (grid[i][j] == 0 ? 0 : l + 1));
-                grid[i][k] = min(grid[i][k], r = (grid[i][k] == 0 ? 0 : r + 1));
-                grid[j][i] = min(grid[j][i], u = (grid[j][i] == 0 ? 0 : u + 1));
-                grid[k][i] = min(grid[k][i], d = (grid[k][i] == 0 ? 0 : d + 1));
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0) continue;
+                arm[i][j][0] = 1 + ((i > 0) ? arm[i - 1][j][0] : 0);
+                arm[i][j][1] = 1 + ((j > 0) ? arm[i][j - 1][1] : 0);
+            }
+        }
+        for (int i = n - 1; i >= 0; --i) {
+            for (int j = n - 1; j >= 0; --j) {
+                if (grid[i][j] == 0) continue;
+                arm[i][j][2] = 1 + ((i < n - 1) ? arm[i + 1][j][2] : 0);
+                arm[i][j][3] = 1 + ((j < n - 1) ? arm[i][j + 1][3] : 0);
             }
         }
         
-        int maxSize = 0;
-        for (int i = 0; i < n; ++i) for (int j = 0; j < n; ++j) maxSize = max(maxSize, grid[i][j]);
-        return maxSize;
+        int res = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 0) continue;
+                int s = min(min(arm[i][j][0], arm[i][j][1]), min(arm[i][j][2], arm[i][j][3]));
+                res = max(res, s);
+            }
+        }
+        return res;
     }
 };
