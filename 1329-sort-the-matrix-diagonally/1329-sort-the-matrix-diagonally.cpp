409eacd9695e9fb5263a3d56b65e5734686774a3
Zheng Yong <angzhengyong@gmail.com>
2022-08-28 11:52:27 +0800

    Time: 18 ms (54.08%), Space: 9.4 MB (52.67%) - LeetHub

--- 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
+++ 1329-sort-the-matrix-diagonally/1329-sort-the-matrix-diagonally.cpp
@@ -1,23 +1,41 @@
 class Solution {
 public:
     vector<vector<int>> diagonalSort(vector<vector<int>>& mat) {
-        unordered_map<int, vector<int>> mp;
+        int n = mat.size(), m = mat[0].size();
+        
+        for (int i = 0, j = 0; j < m; ++j) {
+            int x = i, y = j;
+            vector<int> v;
+            while (x < n && y < m) {
+                v.emplace_back(mat[x][y]);
+                ++x, ++y;
+            }
+            sort(v.begin(), v.end());
 
-        for (int i = 0; i < mat.size(); ++i)
-            for (int j = 0; j < mat[0].size(); ++j)
-                mp[i - j].push_back(mat[i][j]);
-
-        int m = mat.size();
-        for (int k = -(mat[0].size() - 1); k < m; ++k)
-            sort(mp[k].begin(), mp[k].end());
-
-        for (int i = mat.size() - 1; i >= 0; --i) {
-            for (int j = mat[0].size() - 1; j >= 0; --j) {
-                mat[i][j] = mp[i - j].back();
-                mp[i - j].pop_back();
+            x = i, y = j;
+            int cnt = 0;
+            while (x < n && y < m) {
+                mat[x][y] = v[cnt++];
+                ++x, ++y;
             }
         }
         
+        for (int i = 0, j = 0; i < n; ++i) {
+            int x = i, y = j;
+            vector<int> v;
+            while (x < n && y < m) {
+                v.emplace_back(mat[x][y]);
+                ++x, ++y;
+            }
+            sort(v.begin(), v.end());
+
+            x = i, y = j;
+            int cnt = 0;
+            while (x < n && y < m) {
+                mat[x][y] = v[cnt++];
+                ++x, ++y;
+            }
+        }
         return mat;
     }
 };
