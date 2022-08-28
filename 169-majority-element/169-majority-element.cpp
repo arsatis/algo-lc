ec46274a5f9b4621b36ec6bc4bc1230472e11a96
Zheng Yong <angzhengyong@gmail.com>
2022-08-28 22:03:58 +0800

    Time: 22 ms (84.52%), Space: 19.7 MB (13.83%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int majorityElement(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         int major = nums[0], count = 1;
         for (int i = 1; i < nums.size(); ++i) {
