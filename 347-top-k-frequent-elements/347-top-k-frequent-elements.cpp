490f421afe4ff81042ba27032612cea8c2f4a2f6
Zheng Yong <angzhengyong@gmail.com>
2022-08-29 22:00:12 +0800

    Time: 18 ms (84.45%), Space: 15.1 MB (13.42%) - LeetHub

--- 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         unordered_map<int, int> counts;
         for (int& i : nums) ++counts[i];
