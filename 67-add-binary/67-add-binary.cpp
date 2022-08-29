0bc1e79876dc7edea73ffa76370cde4e4306cfc6
Zheng Yong <angzhengyong@gmail.com>
2022-08-29 09:36:18 +0800

    Time: 8 ms (25.34%), Space: 6.5 MB (37.58%) - LeetHub

--- 67-add-binary/67-add-binary.cpp
+++ 67-add-binary/67-add-binary.cpp
@@ -2,20 +2,17 @@ class Solution {
 public:
     string addBinary(string a, string b) {
         ios_base::sync_with_stdio(0);
-        int i = a.size() - 1, j = b.size() - 1, carry = 0;
+        
         string res;
-        while(i >= 0 || j >= 0 || carry) {
-            if(i >= 0) {
-                carry += a[i] - '0';
-                i--;
-            }
-            if(j >= 0) {
-                carry += b[j] - '0';
-                j--;
-            }
-            res += (carry % 2 + '0');
-            carry = carry / 2;
+        int i = a.length() - 1, j = b.length() - 1, carry = 0;
+        while (i >= 0 || j >= 0) {
+            int sum = carry;
+            if (i >= 0) sum += a[i--] - '0';
+            if (j >= 0) sum += b[j--] - '0';
+            carry = sum > 1 ? 1 : 0;
+            res += to_string(sum % 2);
         }
+        if (carry) res += to_string(carry);
         reverse(res.begin(), res.end());
         return res;
     }
