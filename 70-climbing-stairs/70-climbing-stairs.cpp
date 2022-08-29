c994801195779646a0ea3983b513f225d360a8f0
Zheng Yong <angzhengyong@gmail.com>
2022-08-29 09:42:31 +0800

    Time: 3 ms (36.11%), Space: 5.9 MB (83.50%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        ios_base::sync_with_stdio(0);
-        
         if (n <= 2) return n;
         int prev = 2, prev2 = 1, res;
         for (int i = 3; i <= n; i++) {
