c57cf156169575d02b1792e61adc5c1b060de191
Zheng Yong <angzhengyong@gmail.com>
2022-08-29 21:59:58 +0800

    Time: 19 ms (81.24%), Space: 14.9 MB (13.95%) - LeetHub

---
+++ 347-top-k-frequent-elements/347-top-k-frequent-elements.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> topKFrequent(vector<int>& nums, int k) {
+        ios_base::sync_with_stdio(0);
+        
+        unordered_map<int, int> counts;
+        for (int& i : nums) ++counts[i];
+        
+        vector<int> buckets[nums.size() + 1];
+        for (auto& pair : counts) buckets[pair.second].push_back(pair.first);
+        
+        vector<int> mostFreq(k);
+        int outerIdx = nums.size(), innerIdx = 0;
+        for (int i = 0; i < k; ++i) {
+            while (buckets[outerIdx].size() == 0) --outerIdx;
+            mostFreq[i] = buckets[outerIdx][innerIdx];
+            if (buckets[outerIdx].size() <= ++innerIdx) {
+                --outerIdx;
+                innerIdx = 0;
+            }
+        }
+        return mostFreq;
+    }
+};
