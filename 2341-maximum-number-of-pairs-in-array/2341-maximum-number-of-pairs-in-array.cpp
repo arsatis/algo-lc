ca5318aaa55c917bb0a483f6cad42637613002be
Zheng Yong <angzhengyong@gmail.com>
2022-08-29 22:40:11 +0800

    Time: 4 ms (62.60%), Space: 9.2 MB (62.87%) - LeetHub

--- 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
+++ 2341-maximum-number-of-pairs-in-array/2341-maximum-number-of-pairs-in-array.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> numberOfPairs(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        
         int pairs = 0, leftovers = 0, counts[101] = { 0 };
         for (int& i : nums) {
             if (!counts[i]) {
