799c0946d1dbf03d1b4079e82d9ab8c80641e837
Zheng Yong <angzhengyong@gmail.com>
2022-08-29 09:39:21 +0800

    Time: 7 ms (34.70%), Space: 6 MB (25.28%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -1,6 +1,15 @@
 class Solution {
 public:
     int mySqrt(int x) {
-        return (int) pow(x, 0.5);
+        int low = 1, high = x, mid, ans;
+        while (low <= high) {
+            mid = low + (high - low) / 2;
+            if ((long long) mid * mid <= x) {
+                ans = mid;
+                low = mid + 1;
+            }
+            else high = mid - 1;
+        }
+        return ans;
     }
 };
