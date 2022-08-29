4927feef85ad4cbc72d3e999a3b10a8426265ceb
Zheng Yong <angzhengyong@gmail.com>
2022-08-29 09:53:03 +0800

    Time: 15 ms (97.45%), Space: 17.1 MB (24.05%) - LeetHub

--- 136-single-number/136-single-number.cpp
+++ 136-single-number/136-single-number.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int singleNumber(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         int ans = 0;
         for (auto& x : nums) ans ^= x;
