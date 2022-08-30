ba614ddcf20a3f6faad8c6459317b610343d3e4a
Zheng Yong <angzhengyong@gmail.com>
2022-08-31 00:24:27 +0800

    Time: 12 ms (84.77%), Space: 8.5 MB (38.95%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -4,14 +4,15 @@ public:
         ios_base::sync_with_stdio(0);
         
         vector<int> freq(128, 0);
-        for (char c : s) freq[c]++;
+        for (char c : s) ++freq[c];
+        
         string res;
         int sz = 0;
-        while(sz < s.size()) {
+        while (sz < s.size()) {
             auto it = max_element(freq.begin(), freq.end());
             sz += *it;
             int ch = it - freq.begin();
-            while(freq[ch]-- > 0) res.push_back(ch);
+            while (freq[ch]-- > 0) res.push_back(ch);
         }
         return res;
     }
