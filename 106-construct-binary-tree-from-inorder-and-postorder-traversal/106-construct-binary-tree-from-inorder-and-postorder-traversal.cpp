7ed1ce580e8b76a40c15604c751d4d0cbdedcda1
Zheng Yong <angzhengyong@gmail.com>
2022-08-30 20:50:18 +0800

    Time: 30 ms (57.34%), Space: 26.5 MB (20.56%) - LeetHub

--- 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 106-construct-binary-tree-from-inorder-and-postorder-traversal/106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -21,10 +21,6 @@ class Solution {
     }
 public:
     TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
         int pIdx = postorder.size() - 1;
         unordered_map<int, int> idx;
         for (int i = 0; i < inorder.size(); ++i) idx[inorder[i]] = i;
