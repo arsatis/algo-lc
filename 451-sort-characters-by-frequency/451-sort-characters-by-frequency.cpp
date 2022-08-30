11bf331247f5de5a82f752574f23f0f72524820c
Zheng Yong <angzhengyong@gmail.com>
2022-08-31 00:22:45 +0800

    Time: 18 ms (53.33%), Space: 8.7 MB (23.68%) - LeetHub

--- 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     string frequencySort(string s) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         unordered_map<char, int> counts;
         for (char c : s) ++counts[c];
