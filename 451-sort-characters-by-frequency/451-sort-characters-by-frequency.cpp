82cef1f4fb7d91a9f0e8b24e06069da855211d7c
Zheng Yong <angzhengyong@gmail.com>
2022-08-31 00:22:16 +0800

    Time: 12 ms (84.77%), Space: 8.6 MB (23.68%) - LeetHub

---
+++ 451-sort-characters-by-frequency/451-sort-characters-by-frequency.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    string frequencySort(string s) {
+        ios_base::sync_with_stdio(0);
+        
+        unordered_map<char, int> counts;
+        for (char c : s) ++counts[c];
+        
+        int n = counts.size(), i = 0;
+        string ss = "";
+        vector<pair<int, char>> arr(n);
+        for (auto& p : counts) {
+            arr[i] = { p.second, p.first };
+            ++i;
+        }
+        sort(arr.begin(), arr.end(), [](auto& x, auto& y) {
+            return x.first > y.first;
+        });
+        for (i = 0; i < n; ++i)
+            for (int j = 0; j < arr[i].first; ++j)
+                ss += arr[i].second;
+        return ss;
+    }
+};
