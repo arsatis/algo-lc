757a54ce8fb074287d47b4bb113ff7fa843f380e
Zheng Yong <angzhengyong@gmail.com>
2022-08-30 19:14:29 +0800

    Time: 401 ms (99.18%), Space: 88.9 MB (99.68%) - LeetHub

--- 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
+++ 435-non-overlapping-intervals/435-non-overlapping-intervals.cpp
@@ -5,14 +5,13 @@ public:
         cin.tie(0);
         cout.tie(0);
         
-        int num = 0;
-        vector<int> prev = { 0, INT_MIN };
+        int num = 0, currEnd = INT_MIN;
         
         sort(intervals.begin(), intervals.end(), [](auto& x, auto& y) {
             return x[1] < y[1];
         });
         for (auto& i : intervals) {
-            if (prev[1] <= i[0]) prev = i;
+            if (currEnd <= i[0]) currEnd = i[1];
             else ++num;
         }
         
