79a6014aad11e5870c9d454eecf1b69e03730339
Zheng Yong <angzhengyong@gmail.com>
2022-09-01 13:29:59 +0800

    Time: 40 ms (49.70%), Space: 31.2 MB (52.40%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
+        
         int leftSum = 0, sum = accumulate(nums.begin(), nums.end(), 0);
         for (int i = 0; i < nums.size(); ++i) {
             if (leftSum == sum - leftSum - nums[i]) return i;
