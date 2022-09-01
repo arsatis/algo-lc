fe9e4df7c269540a8b5207f3105cd4b750559d4e
Zheng Yong <angzhengyong@gmail.com>
2022-09-01 14:35:10 +0800

    Time: 18 ms (99.88%), Space: 27.5 MB (92.16%) - LeetHub

---
+++ 704-binary-search/704-binary-search.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        ios_base::sync_with_stdio(0);
+        
+        int left = 0, right = nums.size() - 1;
+        while (left <= right) {
+            int mid = (left + right) >> 1;
+            if (nums[mid] == target) return mid;
+            else if (nums[mid] > target) right = mid - 1;
+            else left = mid + 1;
+        }
+        return -1;
+    }
+};
