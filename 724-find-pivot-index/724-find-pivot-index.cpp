86faa5bf221553d506be27169291c94318129373
Zheng Yong <angzhengyong@gmail.com>
2022-09-01 13:29:41 +0800

    Time: 43 ms (42.17%), Space: 31.1 MB (87.50%) - LeetHub

--- 724-find-pivot-index/724-find-pivot-index.cpp
+++ 724-find-pivot-index/724-find-pivot-index.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int pivotIndex(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         int leftSum = 0, sum = accumulate(nums.begin(), nums.end(), 0);
         for (int i = 0; i < nums.size(); ++i) {
