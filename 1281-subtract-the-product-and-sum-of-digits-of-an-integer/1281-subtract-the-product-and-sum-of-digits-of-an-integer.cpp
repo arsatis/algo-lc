0a149a3ebf7a3050850246389ede109ecf685856
Zheng Yong <angzhengyong@gmail.com>
2022-09-02 13:51:17 +0800

    Time: 1 ms (43.73%), Space: 5.8 MB (71.36%) - LeetHub

--- 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
+++ 1281-subtract-the-product-and-sum-of-digits-of-an-integer/1281-subtract-the-product-and-sum-of-digits-of-an-integer.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int subtractProductAndSum(int n) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         int sum = 0, product = 1;
         for (char c : to_string(n)) {
