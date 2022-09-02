f265312b3bb4b20f2f42c581c260e628cc146cf9
Zheng Yong <angzhengyong@gmail.com>
2022-09-02 14:24:47 +0800

    Time: 7 ms (78.72%), Space: 6.9 MB (74.25%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.cpp
+++ 205-isomorphic-strings/205-isomorphic-strings.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     bool isIsomorphic(string s, string t) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         int sMap[128] = { 0 }, tMap[128] = { 0 };
         for (int i = 0; i < s.size(); ++i) {
             if (!sMap[s[i]] && !tMap[t[i]]) {
