dbe930127389a2f6b45c09618b9e642955f96a36
Zheng Yong <angzhengyong@gmail.com>
2022-09-02 13:45:26 +0800

    Time: 0 ms (100.00%), Space: 6 MB (47.64%) - LeetHub

--- 191-number-of-1-bits/191-number-of-1-bits.cpp
+++ 191-number-of-1-bits/191-number-of-1-bits.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     int hammingWeight(uint32_t n) {
+        ios_base::sync_with_stdio(0);
+
         int weight = 0;
         while (n > 0) {
             weight += 1 & n;
