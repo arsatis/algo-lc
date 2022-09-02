34988767be1c0b00e8f236ef3874acbad1adf55b
Zheng Yong <angzhengyong@gmail.com>
2022-09-02 15:37:37 +0800

    Time: 20 ms (99.82%), Space: 24.9 MB (93.26%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,13 +1,7 @@
 class Solution {
 public:
-    void rotate(vector<int>& nums, int k) {
+    void rotate(vector<int>& v, int k) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
-        k %= nums.size();
-        reverse(nums.begin(), nums.end());
-        reverse(nums.begin(), nums.begin() + k);
-        reverse(nums.begin() + k, nums.end());
+        std::rotate(v.rbegin(), v.rbegin() + k % v.size(), v.rend());
     }
 };
