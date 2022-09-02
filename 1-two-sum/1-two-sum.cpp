0410e47a3f3167919aa1010e9a8fc64fb74ab390
Zheng Yong <angzhengyong@gmail.com>
2022-09-02 16:28:08 +0800

    Time: 19 ms (68.64%), Space: 10.8 MB (47.38%) - LeetHub

--- 1-two-sum/1-two-sum.cpp
+++ 1-two-sum/1-two-sum.cpp
@@ -2,19 +2,13 @@ class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         unordered_map<int, int> comp;
-        vector<int> vi;
         for (int i = 0; i < nums.size(); ++i) {
-            if (comp.find(target - nums[i]) != comp.end()) {
-                vi.push_back(i);
-                vi.push_back(comp[target - nums[i]]);
-                return vi;
-            }
+            if (comp.find(target - nums[i]) != comp.end())
+                return { i, comp[target - nums[i]] };
             comp[nums[i]] = i;
         }
-        return vi;
+        return {};
     }
 };
