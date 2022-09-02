4992aee4d2024b0aa24036d365d5b77a38be87a2
Zheng Yong <angzhengyong@gmail.com>
2022-09-02 13:09:58 +0800

    Time: 29 ms (36.06%), Space: 22.8 MB (13.23%) - LeetHub

--- 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
+++ 637-average-of-levels-in-binary-tree/637-average-of-levels-in-binary-tree.cpp
@@ -13,8 +13,6 @@ class Solution {
 public:
     vector<double> averageOfLevels(TreeNode* root) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         int nodesInLevel = 0;
         vector<double> averages;
