76a37d93f5f2b201d4abc9621c4bdb05e874f015
Zheng Yong <angzhengyong@gmail.com>
2022-09-03 15:24:32 +0800

    Time: 11 ms (99.97%), Space: 19.4 MB (13.69%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         int slowIndex = 0;
         for (int fastIndex = 0; fastIndex < nums.size(); ++fastIndex)
