d1b731386b65f9517c8393bb91b64d386326ff62
Zheng Yong <angzhengyong@gmail.com>
2022-09-03 20:30:16 +0800

    Time: 17 ms (74.52%), Space: 10.5 MB (67.42%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -4,13 +4,9 @@ public:
         ios_base::sync_with_stdio(0);
         
         vector<int> increase = { nums[0] };
-        
         for (int num : nums) {
             if (num > increase.back()) increase.push_back(num);
-            else {
-                auto it = lower_bound(increase.begin(), increase.end(), num);
-                *it = num;
-            }
+            else *lower_bound(increase.begin(), increase.end(), num) = num;
         }
         return increase.size();
     }
