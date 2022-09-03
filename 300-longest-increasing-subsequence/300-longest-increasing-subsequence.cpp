ec7c59f23b3a26f387f1beaff503274719c44e21
Zheng Yong <angzhengyong@gmail.com>
2022-09-03 20:22:51 +0800

    Time: 345 ms (55.19%), Space: 10.5 MB (67.42%) - LeetHub

--- 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
+++ 300-longest-increasing-subsequence/300-longest-increasing-subsequence.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     int lengthOfLIS(vector<int>& nums) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
         vector<int> dp(nums.size(), 1);
         for (int i = 1; i < nums.size(); ++i)
             for (int j = 0; j < i; ++j)
