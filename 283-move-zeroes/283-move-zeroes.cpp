8198c6ae91afd563dce517e3174cdcae75d620ec
Zheng Yong <angzhengyong@gmail.com>
2022-09-03 15:22:32 +0800

    Time: 64 ms (14.01%), Space: 19.3 MB (13.69%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
-        stable_partition(rbegin(nums), rend(nums), logical_not<int>());
+        int slowIndex = 0;
+        for (int fastIndex = 0; fastIndex < nums.size(); ++fastIndex)
+            if (nums[fastIndex] != 0)
+                nums[slowIndex++] = nums[fastIndex];
+        for (int i = slowIndex; i < nums.size(); ++i) nums[i] = 0;
     }
 };
