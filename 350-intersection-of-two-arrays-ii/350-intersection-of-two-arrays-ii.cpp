e6fe7ce386e19ec8da27c28a4f7cf2f86301203f
Zheng Yong <angzhengyong@gmail.com>
2022-09-03 15:45:04 +0800

    Time: 9 ms (56.17%), Space: 9.9 MB (93.03%) - LeetHub

--- 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
+++ 350-intersection-of-two-arrays-ii/350-intersection-of-two-arrays-ii.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     vector<int> intersect(vector<int>& nums1, vector<int>& nums2) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         vector<int> output;
         int overlap[1001] = { 0 };
