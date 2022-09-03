cf01f68ba36a7c4fb36d8356c1742cf9974b6cbc
Zheng Yong <angzhengyong@gmail.com>
2022-09-03 20:04:52 +0800

    Time: 71 ms (94.19%), Space: 14.6 MB (55.49%) - LeetHub

---
+++ 322-coin-change/322-coin-change.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        ios_base::sync_with_stdio(0);
+        
+        vector<int> dp(amount + 1, -1);
+        dp[0] = 0;
+        
+        for (int i = 1; i <= amount; ++i) {
+            int val = INT_MAX;
+            for (int c : coins)
+                if (c <= i && dp[i - c] != -1)
+                    val = min(val, 1 + dp[i - c]);
+            if (val != INT_MAX) dp[i] = val;
+        }
+        return dp[amount];
+    }
+};
