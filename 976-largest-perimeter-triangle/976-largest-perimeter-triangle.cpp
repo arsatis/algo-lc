2cacab2a495016782f763446a8db49c0c04882c3
Zheng Yong <angzhengyong@gmail.com>
2022-09-03 15:16:01 +0800

    Time: 57 ms (52.74%), Space: 21.9 MB (71.26%) - LeetHub

--- 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
+++ 976-largest-perimeter-triangle/976-largest-perimeter-triangle.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     int largestPerimeter(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         sort(nums.begin(), nums.end(), greater<int>());
         for (int i = 0; i < nums.size() - 2; ++i)
