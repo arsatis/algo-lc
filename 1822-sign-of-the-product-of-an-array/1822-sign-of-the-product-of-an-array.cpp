f9132ac575578017ca94691380a3d70defbd216e
Zheng Yong <angzhengyong@gmail.com>
2022-09-04 13:47:20 +0800

    Time: 6 ms (79.85%), Space: 10.4 MB (42.43%) - LeetHub

--- 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
+++ 1822-sign-of-the-product-of-an-array/1822-sign-of-the-product-of-an-array.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int arraySign(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         bool isNeg = false;
         for (int i : nums) {
