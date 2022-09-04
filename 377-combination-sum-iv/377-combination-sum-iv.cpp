2e78596d2bc24652b00828b5e203b9685037f910
Zheng Yong <angzhengyong@gmail.com>
2022-09-04 23:41:20 +0800

    Time: 4 ms (43.25%), Space: 6.7 MB (29.77%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int combinationSum4(vector<int>& nums, int target) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         vector<unsigned int> dp(target + 1);
         dp[0] = 1;
