823f2df097cf22fe883e512bd8ba35e696e58012
Zheng Yong <angzhengyong@gmail.com>
2022-09-04 10:19:38 +0800

    Time: 7 ms (70.92%), Space: 13.8 MB (20.64%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -13,41 +13,31 @@ class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         ios_base::sync_with_stdio(0);
-        map<int,map<int,multiset<int>>> mpp;
-        queue<pair<TreeNode*,pair<int,int>>> q;
+        
+        map<int, map<int, multiset<int>>> mpp;
+        queue<pair<TreeNode*, pair<int, int>>> q;
 
-        q.push({root,{0,0}});
-
-        while(!q.empty())
-        {
+        q.push({root, {0, 0}});
+        while (!q.empty()) {
             auto it = q.front();
 
             TreeNode* node = it.first;
-            int x = it.second.first;
-            int y = it.second.second;
+            int x = it.second.first, y = it.second.second;
 
             mpp[x][y].insert(node->val) ;
 
-            if(node->left)
-            {
-                q.push({node->left,{x-1,y+1}});
-            }
-            if(node->right)
-            {
-                q.push({node->right,{x+1,y+1}});
-            }
+            if (node->left) q.push({node->left,{x-1,y+1}});
+            if (node->right) q.push({node->right,{x+1,y+1}});
             q.pop();
-          }
-            vector<vector<int>> ans;
-
-            for(auto p:mpp)
-            {
-                vector<int> col;
-                for (auto q: p.second) {
-          col.insert(col.end(), q.second.begin(), q.second.end());
         }
-        ans.push_back(col);
-      }
-      return ans;
+                                    
+        vector<vector<int>> ans;
+        for (auto p : mpp) {
+            vector<int> col;
+            for (auto q : p.second)
+                col.insert(col.end(), q.second.begin(), q.second.end());
+            ans.push_back(col);
+        }
+        return ans;
     }
 };
