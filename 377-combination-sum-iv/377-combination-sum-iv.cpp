c630772d867e8024486fc0022018c0bf613004fa
Zheng Yong <angzhengyong@gmail.com>
2022-09-04 23:41:49 +0800

    Time: 0 ms (100.00%), Space: 6.8 MB (29.77%) - LeetHub

--- 377-combination-sum-iv/377-combination-sum-iv.cpp
+++ 377-combination-sum-iv/377-combination-sum-iv.cpp
@@ -8,10 +8,12 @@ public:
         vector<unsigned int> dp(target + 1);
         dp[0] = 1;
         
-        for (int i = 1; i <= target; ++i)
-            for (int j : nums)
-                if (i >= j)
-                    dp[i] += dp[i - j];
+        for (int i = 1; i <= target; ++i) {
+            for (int j : nums) {
+                if (i < j) continue;
+                else dp[i] += dp[i - j];
+            }
+        }
         return dp[target];
     }
 };
