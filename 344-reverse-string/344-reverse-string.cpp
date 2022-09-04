1cb1be11bb246122e4af99633809e74b374c299f
Zheng Yong <angzhengyong@gmail.com>
2022-09-04 15:01:23 +0800

    Time: 14 ms (99.20%), Space: 23.3 MB (40.89%) - LeetHub

--- 344-reverse-string/344-reverse-string.cpp
+++ 344-reverse-string/344-reverse-string.cpp
@@ -2,9 +2,6 @@ class Solution {
 public:
     void reverseString(vector<char>& s) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
         reverse(s.begin(), s.end());
     }
 };
