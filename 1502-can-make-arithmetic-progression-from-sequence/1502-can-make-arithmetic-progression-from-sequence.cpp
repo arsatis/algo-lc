9afdcae4968ae545c713a4fb7d499ae22b453759
Zheng Yong <angzhengyong@gmail.com>
2022-09-04 13:55:53 +0800

    Time: 4 ms (74.25%), Space: 9.2 MB (5.19%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -8,7 +8,7 @@ public:
         sort(arr.begin(), arr.end());
         
         for (int i = 2; i < arr.size(); ++i)
-            if (arr[i] + arr[i - 2] != arr[i - 1] * 2)
+            if (arr[i] - arr[i - 1] != arr[i - 1] - arr[i - 2])
                 return false;
         return true;
     }
