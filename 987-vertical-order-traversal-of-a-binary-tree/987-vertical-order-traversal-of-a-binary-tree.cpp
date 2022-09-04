f47409ecda2a125b2a5ea49e112551f927336c64
Zheng Yong <angzhengyong@gmail.com>
2022-09-04 10:16:49 +0800

    Time: 7 ms (70.92%), Space: 14.5 MB (13.71%) - LeetHub

--- 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
+++ 987-vertical-order-traversal-of-a-binary-tree/987-vertical-order-traversal-of-a-binary-tree.cpp
@@ -21,6 +21,8 @@ class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         vector<vector<int>> output;
         bfs(root, 0, 0);
