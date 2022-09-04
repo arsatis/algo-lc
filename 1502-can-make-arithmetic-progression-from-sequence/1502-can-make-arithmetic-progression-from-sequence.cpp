fbaad597d770d4388d0e46e6ff362dd52b3fca89
Zheng Yong <angzhengyong@gmail.com>
2022-09-04 13:53:30 +0800

    Time: 9 ms (26.78%), Space: 9 MB (93.19%) - LeetHub

--- 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
+++ 1502-can-make-arithmetic-progression-from-sequence/1502-can-make-arithmetic-progression-from-sequence.cpp
@@ -1,10 +1,6 @@
 class Solution {
 public:
     bool canMakeArithmeticProgression(vector<int>& arr) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
         sort(arr.begin(), arr.end());
         
         int diff = arr[1] - arr[0];
