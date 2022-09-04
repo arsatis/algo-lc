8401003092f9029176f6c3d1c0eab6788644f39f
Zheng Yong <angzhengyong@gmail.com>
2022-09-04 14:32:28 +0800

    Time: 11 ms (70.90%), Space: 7.6 MB (75.44%) - LeetHub

--- 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.cpp
@@ -10,8 +10,6 @@ class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         ListNode *fast = head, *slow = head, *entry = head;
         while (fast && fast->next) {
