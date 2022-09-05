f4eaba59b7cde280cced2e73a24e3d52fa347e69
Zheng Yong <angzhengyong@gmail.com>
2022-09-05 11:34:28 +0800

    Time: 31 ms (68.85%), Space: 18 MB (89.10%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,26 +1,20 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        int n = board.size();
-        int m = board[0].size();
-        
         int row[9][9] = {0};
         int col[9][9] = {0};
-        int box[9][9] = {0};
-        
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(board[i][j] != '.'){
-                    int b = i/3 * 3 +j/3;
-                    int x = board[i][j]-'0'-1;
-                    if(row[i][x] == 1 or col[j][x] == 1 or box[b][x] == 1){
-                        return false;
-                    }
-                    row[i][x] = 1;
-                    col[j][x] = 1;
-                    box[b][x] = 1;
+        int sub[9][9] = {0};
+        for (int i = 0; i < 9; i++) {
+            for (int j = 0; j < 9; j++) {
+                if (board[i][j] == '.') continue;
+                int num = board[i][j] - '0' - 1; //convert int and remove off by one error
+                int subBoxNum  = (i/3)*3 + j/3;
+                if (row[i][num] || col[j][num] || sub[subBoxNum][num]) {
+                    return false;
+                } else {
+                    row[i][num] = col[j][num] = sub[subBoxNum][num] = 1;
                 }
-            }
+            } 
         }
         return true;
     }
