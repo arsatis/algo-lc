e2429864fe5b10a7671b88f76ea0d62b568e174e
Zheng Yong <angzhengyong@gmail.com>
2022-09-05 11:34:11 +0800

    Time: 40 ms (43.91%), Space: 18.1 MB (73.26%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,31 +1,25 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        // row
-        for (int i = 0; i < 9; ++i) {
-            int arr[9] = { 0 };
-            for (int j = 0; j < 9; ++j)
-                if (board[i][j] != '.' && arr[board[i][j] - '1']++ > 0)
-                    return false;
-        }
+        int n = board.size();
+        int m = board[0].size();
         
-        // col
-        for (int j = 0; j < 9; ++j) {
-            int arr[9] = { 0 };
-            for (int i = 0; i < 9; ++i)
-                if (board[i][j] != '.' && arr[board[i][j] - '1']++ > 0)
-                    return false;
-        }
+        int row[9][9] = {0};
+        int col[9][9] = {0};
+        int box[9][9] = {0};
         
-        // square
-        for (int i = 0; i < 9; i += 3) {
-            for (int j = 0; j < 9; j += 3) {
-                int arr[9] = { 0 };
-                for (int di = 0; di < 3; ++di)
-                    for (int dj = 0; dj < 3; ++dj)
-                        if (board[i + di][j + dj] != '.' &&
-                            arr[board[i + di][j + dj] - '1']++ > 0)
-                            return false;
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                if(board[i][j] != '.'){
+                    int b = i/3 * 3 +j/3;
+                    int x = board[i][j]-'0'-1;
+                    if(row[i][x] == 1 or col[j][x] == 1 or box[b][x] == 1){
+                        return false;
+                    }
+                    row[i][x] = 1;
+                    col[j][x] = 1;
+                    box[b][x] = 1;
+                }
             }
         }
         return true;
