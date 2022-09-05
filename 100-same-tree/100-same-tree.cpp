db8a0aebba250754f84de3715ef12f9417bc2794
Zheng Yong <angzhengyong@gmail.com>
2022-09-05 22:01:54 +0800

    Time: 7 ms (21.67%), Space: 10.1 MB (44.81%) - LeetHub

--- 100-same-tree/100-same-tree.cpp
+++ 100-same-tree/100-same-tree.cpp
@@ -12,6 +12,7 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
+        ios_base::sync_with_stdio(0);
         if (!p && !q) return true;
         if (!p || !q || p->val != q->val) return false;
         return isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
