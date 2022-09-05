9d5aa11cc87913433f14e786cfbed10e2a4bbad3
Zheng Yong <angzhengyong@gmail.com>
2022-09-05 09:15:27 +0800

    Time: 34 ms (58.64%), Space: 11.9 MB (34.37%) - LeetHub

--- 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
+++ 429-n-ary-tree-level-order-traversal/429-n-ary-tree-level-order-traversal.cpp
@@ -21,6 +21,10 @@ public:
 class Solution {
 public:
     vector<vector<int>> levelOrder(Node* root) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         if (!root) return {};
         vector<vector<int>> levels;
         queue<pair<int, Node*>> q;
