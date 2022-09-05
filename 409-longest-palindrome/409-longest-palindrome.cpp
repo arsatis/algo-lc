de1e412edf382de207fa9fc5ae430788ce7abc19
Zheng Yong <angzhengyong@gmail.com>
2022-09-05 11:15:09 +0800

    Time: 3 ms (81.38%), Space: 6.5 MB (81.24%) - LeetHub

---
+++ 409-longest-palindrome/409-longest-palindrome.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int longestPalindrome(string s) {
+        int len = 0, chars[64] = { 0 };
+        for (char c : s) if (++chars[c - 'A'] % 2 == 0) len += 2;
+        for (int i : chars) if (i % 2 == 1) return 1 + len;
+        return len;
+    }
+};
