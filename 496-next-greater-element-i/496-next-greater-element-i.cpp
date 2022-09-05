bd1b66063aa1a5ee78836f50a5b42c21020df58e
Zheng Yong <angzhengyong@gmail.com>
2022-09-05 10:13:56 +0800

    Time: 7 ms (79.67%), Space: 8.9 MB (57.34%) - LeetHub

--- 496-next-greater-element-i/496-next-greater-element-i.cpp
+++ 496-next-greater-element-i/496-next-greater-element-i.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {
-        ios_base::sync_with_stdio(0);
-        
         stack<int> s;
         unordered_map<int, int> m;
         for (int n : nums2) {
@@ -12,8 +10,10 @@ public:
             }
             s.push(n);
         }
-        vector<int> ans;
-        for (int n : nums1) ans.push_back(m.count(n) ? m[n] : -1);
+        
+        vector<int> ans(nums1.size());
+        for (int i = 0; i < nums1.size(); ++i)
+            ans[i] = m.count(nums1[i]) ? m[nums1[i]] : -1;
         return ans;
     }
 };
