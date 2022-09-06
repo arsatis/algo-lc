6bc16c1c6f6d7074862fa22053336b8896808dd1
Zheng Yong <angzhengyong@gmail.com>
2022-09-06 10:02:09 +0800

    Time: 4 ms (87.51%), Space: 7.9 MB (50.93%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     int maximumWealth(vector<vector<int>>& accounts) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         int output = 0;
         for (auto& acc : accounts) output = max(output, accumulate(acc.begin(), acc.end(), 0));
