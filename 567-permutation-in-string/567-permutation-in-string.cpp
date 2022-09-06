10ec982cb362834afc97787215e45138efcad28e
Zheng Yong <angzhengyong@gmail.com>
2022-09-06 10:36:00 +0800

    Time: 16 ms (50.83%), Space: 7.3 MB (45.60%) - LeetHub

---
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    bool checkInclusion(string s1, string s2) {
+        int chars[26] = { 0 };
+        for (char& c : s1) ++chars[c - 'a'];
+        for (int l = 0, r = 0; r < s2.size(); ++r) {
+            while (chars[s2[r] - 'a'] == 0) ++chars[s2[l++] - 'a'];
+            if (r - l + 1 == s1.size()) return true;
+            --chars[s2[r] - 'a'];
+        }
+        return false;
+    }
+};
