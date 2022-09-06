d5c380fd5e4134aab78b85b8aa63be0de4d70121
Zheng Yong <angzhengyong@gmail.com>
2022-09-06 10:36:42 +0800

    Time: 6 ms (89.45%), Space: 7.3 MB (74.38%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        ios_base::sync_with_stdio(0);
-        
         int chars[26] = { 0 };
         for (char& c : s1) ++chars[c - 'a'];
         for (int l = 0, r = 0; r < s2.size(); ++r) {
