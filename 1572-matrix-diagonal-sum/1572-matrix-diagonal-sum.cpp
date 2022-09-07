b8a048bc58966b4c65ec7a594d7ee8b6647cca94
Zheng Yong <angzhengyong@gmail.com>
2022-09-07 19:00:21 +0800

    Time: 29 ms (27.84%), Space: 11.3 MB (10.15%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -4,8 +4,8 @@ public:
         ios_base::sync_with_stdio(0);
         
         int output = 0, n = mat.size();
-        for (int i = 0, j = n - 1; i < n; ++i, --j)
-            output += mat[i][i] + mat[i][j];
+        for (int i = 0; i < n; ++i)
+            output += mat[i][i] + mat[i][n - i - 1];
         if (n % 2) output -= mat[n >> 1][n >> 1];
         return output;
     }
