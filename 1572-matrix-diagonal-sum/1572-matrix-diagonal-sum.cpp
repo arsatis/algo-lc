5ab1a6faa9f155c21ddf62c8a30427c1a3482736
Zheng Yong <angzhengyong@gmail.com>
2022-09-07 19:01:03 +0800

    Time: 26 ms (39.69%), Space: 11.4 MB (10.15%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -1,9 +1,13 @@
 class Solution {
 public:
     int diagonalSum(vector<vector<int>>& mat) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         int output = 0, n = mat.size();
-        for (int i = 0; i < n; ++i)
-            output += mat[i][i] + mat[i][n - i - 1];
+        for (int i = 0, j = n - 1; i < n; ++i, --j)
+            output += mat[i][i] + mat[i][j];
         if (n % 2) output -= mat[n >> 1][n >> 1];
         return output;
     }
