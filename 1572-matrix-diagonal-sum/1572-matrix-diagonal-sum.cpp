aff7f6af8c7f1258ba88f6201d61d198e6d53eea
Zheng Yong <angzhengyong@gmail.com>
2022-09-07 18:58:12 +0800

    Time: 23 ms (52.03%), Space: 11.3 MB (10.15%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.cpp
@@ -5,9 +5,10 @@ public:
         cin.tie(0);
         cout.tie(0);
         
-        int output = 0;
-        for (int i = 0, j = mat.size() - 1; i < mat.size(); ++i, --j)
-            output += mat[i][i] + (i != j ? mat[i][j] : 0);
+        int output = 0, n = mat.size();
+        for (int i = 0, j = n - 1; i < n; ++i, --j)
+            output += mat[i][i] + mat[i][j];
+        if (n % 2) output -= mat[n >> 1][n >> 1];
         return output;
     }
 };
