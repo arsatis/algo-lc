cb2fca229ecec5087a04238ef8e39ecc43f935b9
Zheng Yong <angzhengyong@gmail.com>
2022-09-07 19:17:02 +0800

    Time: 22 ms (26.59%), Space: 10.8 MB (59.45%) - LeetHub

--- 566-reshape-the-matrix/566-reshape-the-matrix.cpp
+++ 566-reshape-the-matrix/566-reshape-the-matrix.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     vector<vector<int>> matrixReshape(vector<vector<int>>& mat, int r, int c) {
-        ios_base::sync_with_stdio(0);
-        
         int n = mat.size() * mat[0].size();
         if (n != r * c) return mat;
         
