fa965cf2b291535711c904ba1ac5c3ddc8b3d4b3
Zheng Yong <angzhengyong@gmail.com>
2022-09-07 20:29:33 +0800

    Time: 7 ms (97.92%), Space: 8.1 MB (58.76%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -13,8 +13,7 @@ public:
         cin.tie(0);
         cout.tie(0);
         
-        ListNode *fast = head, *slow = head;
-        while (fast && fast->next) {
+        for (ListNode *fast = head, *slow = head; fast && fast->next;) {
             fast = fast->next->next;
             slow = slow->next;
             if (fast == slow) return true;
