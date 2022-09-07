261684b279a6e86167799c30c7e5b665325ed7c8
Zheng Yong <angzhengyong@gmail.com>
2022-09-07 20:12:31 +0800

    Time: 16 ms (48.16%), Space: 14.4 MB (15.48%) - LeetHub

---
+++ 733-flood-fill/733-flood-fill.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
+        if (image[sr][sc] == color) return image;
+        int origColor = image[sr][sc];
+        image[sr][sc] = color;
+        if (sr > 0 && origColor == image[sr - 1][sc])
+            floodFill(image, sr - 1, sc, color);
+        if (sc > 0 && origColor == image[sr][sc - 1])
+            floodFill(image, sr, sc - 1, color);
+        if (sr < image.size() - 1 && origColor == image[sr + 1][sc])
+            floodFill(image, sr + 1, sc, color);
+        if (sc < image[0].size() - 1 && origColor == image[sr][sc + 1])
+            floodFill(image, sr, sc + 1, color);
+        return image;
+    }
+};
