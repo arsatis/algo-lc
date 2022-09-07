9751ed5ced74035163b52d32f2cf5c84236631b5
Zheng Yong <angzhengyong@gmail.com>
2022-09-07 20:27:08 +0800

    Time: 15 ms (63.72%), Space: 8.1 MB (58.76%) - LeetHub

--- 141-linked-list-cycle/141-linked-list-cycle.cpp
+++ 141-linked-list-cycle/141-linked-list-cycle.cpp
@@ -9,6 +9,8 @@
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
+        ios_base::sync_with_stdio(0);
+        
         ListNode *fast = head, *slow = head;
         while (fast && fast->next) {
             fast = fast->next->next;
