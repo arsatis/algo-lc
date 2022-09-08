096797254543253272d6ea146c5c8fde00bcb690
Zheng Yong <angzhengyong@gmail.com>
2022-09-08 18:28:02 +0800

    Time: 73 ms (31.61%), Space: 33 MB (15.82%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -13,6 +13,8 @@ class Solution {
 public:
     Solution() { 
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
