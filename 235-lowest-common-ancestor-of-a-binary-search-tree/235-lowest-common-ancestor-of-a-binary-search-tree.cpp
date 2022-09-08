1846420b907e9982169fb329efeabe1ff5185b58
Zheng Yong <angzhengyong@gmail.com>
2022-09-08 18:17:20 +0800

    Time: 46 ms (45.42%), Space: 23.4 MB (14.09%) - LeetHub

--- 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/235-lowest-common-ancestor-of-a-binary-search-tree.cpp
@@ -15,11 +15,10 @@ public:
         cin.tie(0);
         cout.tie(0);
         
-        while (root) {
-            if (root->val < p->val && root->val < q->val) root = root->right;
-            else if (root->val > p->val && root->val > q->val) root = root->left;
-            else return root;
-        }
+        if (root->val < p->val && root->val < q->val)
+            return lowestCommonAncestor(root->right, p, q);
+        if (root->val > p->val && root->val > q->val)
+            return lowestCommonAncestor(root->left, p, q);
         return root;
     }
 };
