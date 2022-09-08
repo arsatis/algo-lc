92fb3a71084d8561fb34764817c4e47a2d4b5d92
Zheng Yong <angzhengyong@gmail.com>
2022-09-08 18:02:26 +0800

    Time: 3 ms (80.32%), Space: 6.6 MB (67.25%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     char findTheDifference(string s, string t) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         int chars[26] = { 0 };
         for (char c : s) ++chars[c - 'a'];
