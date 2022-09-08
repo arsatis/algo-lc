21ba17ac3a88a3bd9820c839e52e6e1ea2893ab5
Zheng Yong <angzhengyong@gmail.com>
2022-09-08 17:59:14 +0800

    Time: 3 ms (45.46%), Space: 6.1 MB (56.67%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    string interpret(string command) {
+        string s = "";
+        auto it = command.begin();
+        
+        while (it != command.end()) {
+            auto it2 = find(it, command.end(), '(');
+            for (int i = 0; i < it2 - it; ++i) s += 'G';
+            if (it2 == command.end()) break;
+            
+            it = find(it2, command.end(), ')');
+            if (it++ - it2 == 1) s += 'o';
+            else s += "al";
+        }
+        return s;
+    }
+};
