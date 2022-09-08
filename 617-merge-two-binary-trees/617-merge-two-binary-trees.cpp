27e4442910852567d157e37099cb69675cf101d6
Zheng Yong <angzhengyong@gmail.com>
2022-09-08 18:28:59 +0800

    Time: 91 ms (8.02%), Space: 33 MB (15.82%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -20,9 +20,11 @@ public:
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
         if (!root1) return root2;
         if (!root2) return root1;
-        TreeNode* node = new TreeNode(root1->val + root2->val);
-        node->left = mergeTrees(root1->left, root2->left);
-        node->right = mergeTrees(root1->right, root2->right);
+        TreeNode* node = new TreeNode(
+            root1->val + root2->val,
+            mergeTrees(root1->left, root2->left),
+            mergeTrees(root1->right, root2->right)
+        );
         return node;
     }
 };
