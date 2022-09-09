76cc2ff7e7ff7af31cca9d6f143177c56efbdae2
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 23:24:16 +0800

    Time: 85 ms (99.95%), Space: 61 MB (99.29%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int findDuplicate(vector<int>& nums) {
+        ios_base::sync_with_stdio(0);
         while (nums[0] != nums[nums[0]]) swap(nums[0], nums[nums[0]]);
         return nums[0];
     }
