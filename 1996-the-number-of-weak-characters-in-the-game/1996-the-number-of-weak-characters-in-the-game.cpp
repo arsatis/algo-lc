76cc240a429b7828095da133bab4cb05702ea2c3
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 13:03:48 +0800

    Time: 726 ms (88.34%), Space: 126.4 MB (37.54%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,13 +1,14 @@
 class Solution {
 public:
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
+        ios_base::sync_with_stdio(0);
+        
         int maxAtk = 0;
         for (auto& p : properties) maxAtk = max(p[0], maxAtk);
         
         vector<int> maxDef(maxAtk + 2);
         for (auto& p : properties)
             maxDef[p.front()] = max(maxDef[p.front()], p.back());
-        
         for (int i = maxAtk - 1; i >= 0; --i)
             maxDef[i] = max(maxDef[i], maxDef[i+1]);
         
