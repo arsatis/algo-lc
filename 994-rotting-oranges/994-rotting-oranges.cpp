80bd5c2cc1e433542c61c2b77a40fcb5b3194340
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 22:51:32 +0800

    Time: 11 ms (62.30%), Space: 13.3 MB (25.52%) - LeetHub

--- 994-rotting-oranges/994-rotting-oranges.cpp
+++ 994-rotting-oranges/994-rotting-oranges.cpp
@@ -1,46 +1,43 @@
 class Solution {
+    int hash(int i, int j) {
+        return (i << 4) + j;
+    }
 public:
     int orangesRotting(vector<vector<int>>& grid) {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
         
-        int m=grid.size();
-        if(m==0) return 0;
-        int n=grid[0].size();
-        int time=0;
-        int fresh=0;
-        queue<pair<int,int>> rotten;
-        for(int i=0;i<m;i++){
-            for(int j=0;j<n;j++){
-                if(grid[i][j]==2){
-                    rotten.push(make_pair(i,j));
-                }
-                else if(grid[i][j]==1){
-                    fresh++;
-                }
+        int m = grid.size(), n = grid[0].size();
+        unordered_set<int> fresh;
+        queue<pair<int, int>> rotten;
+        
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (grid[i][j] == 1) fresh.insert(hash(i, j));
+                if (grid[i][j] == 2) rotten.emplace(i, j);
             }
         }
-        vector<pair<int,int>> v={{1,0},{-1,0},{0,-1},{0,1}};
-        while( fresh!=0 && !rotten.empty()){
-            int size=rotten.size();
-            for(int i=0;i<size;i++){
-                int curx=rotten.front().first;
-                int cury=rotten.front().second;
+        
+        int iter = rotten.empty() ? 0 : -1;
+        while (!rotten.empty()) {
+            int s = rotten.size();
+            while (s--) {
+                int key;
+                auto [i, j] = rotten.front();
                 rotten.pop();
-                for(auto a : v){
-                    int nx=curx+a.first;
-                    int ny=cury+a.second;
-                    if((nx>=0 && nx<m) && (ny>=0 && ny<n) && grid[nx][ny]==1){
-                        grid[nx][ny]=2;
-                        rotten.push({nx,ny});
-                        fresh--;
-                    }
-                }
+                
+                if (i > 0 && fresh.find(key = hash(i - 1, j)) != fresh.end())
+                    fresh.erase(key), rotten.emplace(i - 1, j);
+                if (j > 0 && fresh.find(key = hash(i, j - 1)) != fresh.end())
+                    fresh.erase(key), rotten.emplace(i, j - 1);
+                if (i < m - 1 && fresh.find(key = hash(i + 1, j)) != fresh.end())
+                    fresh.erase(key), rotten.emplace(i + 1, j);
+                if (j < n - 1 && fresh.find(key = hash(i, j + 1)) != fresh.end())
+                    fresh.erase(key), rotten.emplace(i, j + 1);
             }
-            time++;
+            ++iter;
         }
-        if(fresh!=0) return -1;
-        return time;
+        return fresh.empty() ? iter : -1;
     }
 };
