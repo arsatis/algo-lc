0257994b51e5b52a32fceb45bbbb71aba276b787
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 16:41:56 +0800

    Time: 4 ms (83.67%), Space: 9.7 MB (39.40%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
-        ios_base::sync_with_stdio(0);
+        std::unordered_map<char, int> letter_map;
+        auto it = order.begin();
+        while (it != order.end())
+            letter_map[*it] = std::distance(order.begin(), it++);
         
-        unordered_map<char, int> dict;
-        for (int i = 0; i < order.size(); ++i) dict[order[i]] = i;
-        
-        vector<string> words2 = words;
-        sort(words.begin(), words.end(), [&dict](auto& x, auto& y) {
-            int n = max(x.size(), y.size());
-            for (int i = 0; i < n; ++i) {
-                if (i == x.size()) return true;
-                if (i == y.size() || dict[x[i]] > dict[y[i]]) return false;
-                if (dict[x[i]] < dict[y[i]]) return true;
+        auto it1 = words.begin(), it2 = std::next(it1);
+        while (it2 != words.end()) {
+            int size = (*it1).size() > (*it2).size() ? (*it2).size() : (*it1).size();
+            for (int i = 0; i < size; ++i) {
+                cout << letter_map[(*it1)[i]] << " " << letter_map[(*it2)[i]]<<'\n';
+                if (letter_map[(*it1)[i]] != letter_map[(*it2)[i]]) {
+                    if (letter_map[(*it1)[i]] > letter_map[(*it2)[i]]) return false;
+                    break;
+                }
             }
-            return true;
-        });
-        for (int i = 0; i < words.size(); ++i)
-            if (words[i] != words2[i]) return false;
+            if (((*it1).size() > (*it2).size()) && (letter_map[(*it1)[size-1]] == letter_map[(*it2)[size-1]])) return false;
+            it1++;
+            it2++;
+        }
         return true;
     }
 };
