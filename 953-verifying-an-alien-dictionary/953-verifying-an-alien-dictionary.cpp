10fb21c93f661c29c02fd131dd62741645d251fa
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 16:44:02 +0800

    Time: 6 ms (72.26%), Space: 9.8 MB (7.45%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -3,22 +3,23 @@ public:
     bool isAlienSorted(vector<string>& words, string order) {
         ios_base::sync_with_stdio(0);
         
-        unordered_map<char, int> letter_map;
+        unordered_map<char, int> dict;
         auto it = order.begin();
-        while (it != order.end())
-            letter_map[*it] = std::distance(order.begin(), it++);
+        while (it != order.end()) dict[*it] = distance(order.begin(), it++);
         
         auto it1 = words.begin(), it2 = it1 + 1;
         while (it2 != words.end()) {
-            int size = (*it1).size() > (*it2).size() ? (*it2).size() : (*it1).size();
+            int size = (*it1).size() > (*it2).size()
+                ? (*it2).size()
+                : (*it1).size();
             for (int i = 0; i < size; ++i) {
-                if (letter_map[(*it1)[i]] != letter_map[(*it2)[i]]) {
-                    if (letter_map[(*it1)[i]] > letter_map[(*it2)[i]]) return false;
+                if (dict[(*it1)[i]] != dict[(*it2)[i]]) {
+                    if (dict[(*it1)[i]] > dict[(*it2)[i]]) return false;
                     break;
                 }
             }
             if (((*it1).size() > (*it2).size()) &&
-                (letter_map[(*it1)[size-1]] == letter_map[(*it2)[size-1]]))
+                (dict[(*it1)[size-1]] == dict[(*it2)[size-1]]))
                 return false;
             ++it1, ++it2;
         }
