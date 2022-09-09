01faca541729cde56223fdc74abd9ac8d86b6777
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 19:29:05 +0800

    Time: 7 ms (67.70%), Space: 9.8 MB (16.63%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
+        ios_base::sync_with_stdio(0);
+        
         unordered_map<char, int> dict;
         
         for (int i = 0; i < order.size(); ++i) dict[order[i]] = i;
