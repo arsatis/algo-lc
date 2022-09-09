42de559376ea219d874acf692b22e279290897ee
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 19:30:53 +0800

    Time: 11 ms (35.29%), Space: 9.5 MB (55.83%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -1,17 +1,19 @@
 class Solution {
 public:
     bool isAlienSorted(vector<string>& words, string order) {
-        unordered_map<char, int> dict;
+        int dict[26] = { 0 };
         
-        for (int i = 0; i < order.size(); ++i) dict[order[i]] = i;
+        for (int i = 0; i < order.size(); ++i) dict[order[i] - 'a'] = i;
         for (int i = 0; i < words.size() - 1; ++i) {
             int size = min(words[i].size(), words[i + 1].size());
             for (int j = 0; j < size; ++j) {
-                if (dict[words[i][j]] > dict[words[i + 1][j]]) return false;
-                if (dict[words[i][j]] < dict[words[i + 1][j]]) break;
+                if (dict[words[i][j] - 'a'] > dict[words[i + 1][j] - 'a'])
+                    return false;
+                if (dict[words[i][j] - 'a'] < dict[words[i + 1][j] - 'a'])
+                    break;
             }
             if (words[i].size() > words[i + 1].size() &&
-                dict[words[i][size - 1]] == dict[words[i + 1][size - 1]])
+                dict[words[i][size - 1] - 'a'] == dict[words[i + 1][size - 1] - 'a'])
                 return false;
         }
         return true;
