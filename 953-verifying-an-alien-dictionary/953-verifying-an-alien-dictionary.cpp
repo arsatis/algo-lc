ac945743d2517dec33406a6b3ea52103883087e8
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 16:39:30 +0800

    Time: 15 ms (13.69%), Space: 9.8 MB (7.45%) - LeetHub

---
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool isAlienSorted(vector<string>& words, string order) {
+        unordered_map<char, int> dict;
+        for (int i = 0; i < order.size(); ++i) dict[order[i]] = i;
+        
+        vector<string> words2 = words;
+        sort(words.begin(), words.end(), [&dict](auto& x, auto& y) {
+            int n = max(x.size(), y.size());
+            for (int i = 0; i < n; ++i) {
+                if (i == x.size()) return true;
+                if (i == y.size() || dict[x[i]] > dict[y[i]]) return false;
+                if (dict[x[i]] < dict[y[i]]) return true;
+            }
+            return true;
+        });
+        for (int i = 0; i < words.size(); ++i)
+            if (words[i] != words2[i]) return false;
+        return true;
+    }
+};
