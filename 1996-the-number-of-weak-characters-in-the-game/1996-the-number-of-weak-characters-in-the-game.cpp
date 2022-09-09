b322552f6c3498db8f259e84dc646dca05218648
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 12:58:52 +0800

    Time: 1558 ms (9.99%), Space: 125.1 MB (91.78%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        ios_base::sync_with_stdio(0);
-        
         int count = 0, currMax = INT_MIN;
         sort(properties.begin(), properties.end(), [](auto& x, auto& y) {
             return x.back() == y.back() ? x.front() > y.front() : x.back() < y.back();
