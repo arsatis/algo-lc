f2fcfcc7ac13a741571a17a651b370bc123659eb
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 12:59:25 +0800

    Time: 913 ms (68.03%), Space: 126.6 MB (35.16%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.cpp
@@ -1,18 +1,29 @@
 class Solution {
 public:
     int numberOfWeakCharacters(vector<vector<int>>& properties) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
-        int count = 0, currMax = INT_MIN;
-        sort(properties.begin(), properties.end(), [](auto& x, auto& y) {
-            return x.back() == y.back() ? x.front() > y.front() : x.back() < y.back();
-        });
-        for (int i = properties.size() - 1; i >= 0; --i) {
-            if (properties[i].front() < currMax) ++count;
-            currMax = max(currMax, properties[i].front());
+        int maxattack=0;
+        int weak=0;
+        for(auto &property:properties){
+            maxattack=max(property[0],maxattack);
         }
-        return count;
+        vector<int>maxdefense(maxattack+2,0);
+        //storing the max defense for an attack value
+        for(auto &property:properties){
+            int attack=property[0];
+            int defense=property[1];
+            maxdefense[attack]=max(maxdefense[attack],defense);
+        }
+        
+        //storing the max defense for attack greater than or equal to value
+        for(int i=maxattack-1;i>=0;i--){
+            maxdefense[i]=max(maxdefense[i],maxdefense[i+1]);
+        }
+        for(auto &property:properties){
+            int attack=property[0];
+            int defense=property[1];
+            if(defense<maxdefense[attack+1])
+                weak++;
+        }
+        return weak;
     }
 };
