53015f2b11a7a9e466c6eef710c366387aed3246
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 16:42:30 +0800

    Time: 15 ms (13.69%), Space: 9.4 MB (92.04%) - LeetHub

--- 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
+++ 953-verifying-an-alien-dictionary/953-verifying-an-alien-dictionary.cpp
@@ -10,15 +10,15 @@ public:
         while (it2 != words.end()) {
             int size = (*it1).size() > (*it2).size() ? (*it2).size() : (*it1).size();
             for (int i = 0; i < size; ++i) {
-                cout << letter_map[(*it1)[i]] << " " << letter_map[(*it2)[i]]<<'\n';
                 if (letter_map[(*it1)[i]] != letter_map[(*it2)[i]]) {
                     if (letter_map[(*it1)[i]] > letter_map[(*it2)[i]]) return false;
                     break;
                 }
             }
-            if (((*it1).size() > (*it2).size()) && (letter_map[(*it1)[size-1]] == letter_map[(*it2)[size-1]])) return false;
-            it1++;
-            it2++;
+            if (((*it1).size() > (*it2).size()) &&
+                (letter_map[(*it1)[size-1]] == letter_map[(*it2)[size-1]]))
+                return false;
+            ++it1, ++it2;
         }
         return true;
     }
