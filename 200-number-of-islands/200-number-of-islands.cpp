ae7229b7bcf7f332b09d1d19dbf205ed11cff4a6
Zheng Yong <angzhengyong@gmail.com>
2022-09-09 19:33:00 +0800

    Time: 61 ms (42.55%), Space: 12.1 MB (96.70%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.cpp
+++ 200-number-of-islands/200-number-of-islands.cpp
@@ -1,4 +1,13 @@
 class Solution {
+    int dfs(vector<vector<char>>& grid, int i, int j) {
+        if (grid[i][j] == '0') return 0;
+        grid[i][j] = '0';
+        if (i > 0) dfs(grid, i - 1, j);
+        if (j > 0) dfs(grid, i, j - 1);
+        if (i < grid.size() - 1) dfs(grid, i + 1, j);
+        if (j < grid[0].size() - 1) dfs(grid, i, j + 1);
+        return 1;
+    }
 public:
     int numIslands(vector<vector<char>>& grid) {
         ios_base::sync_with_stdio(0);
@@ -6,17 +15,7 @@ public:
         int num = 0;
         for (int i = 0; i < grid.size(); ++i)
             for (int j = 0; j < grid[0].size(); ++j)
-                num += bfs(grid, i, j);
+                num += dfs(grid, i, j);
         return num;
-    }
-    
-    int bfs(vector<vector<char>>& grid, int i, int j) {
-        if (grid[i][j] == '0') return 0;
-        grid[i][j] = '0';
-        if (i > 0) bfs(grid, i - 1, j);
-        if (i < grid.size() - 1) bfs(grid, i + 1, j);
-        if (j > 0) bfs(grid, i, j - 1);
-        if (j < grid[0].size() - 1) bfs(grid, i, j + 1);
-        return 1;
     }
 };
