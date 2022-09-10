812a1e822b3220936ea86710da15839a324b22eb
Zheng Yong <angzhengyong@gmail.com>
2022-09-10 10:15:33 +0800

    Time: 3 ms (60.63%), Space: 8.6 MB (13.03%) - LeetHub

--- 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
+++ 144-binary-tree-preorder-traversal/144-binary-tree-preorder-traversal.cpp
@@ -19,6 +19,8 @@ class Solution {
 public:
     vector<int> preorderTraversal(TreeNode* root) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         if (!root) return {};
         f(root);
