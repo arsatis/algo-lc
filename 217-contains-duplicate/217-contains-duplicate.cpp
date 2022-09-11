45b8c0362b0c16ac7bbfdf7439b92a5cdbccb58d
Zheng Yong <angzhengyong@gmail.com>
2022-09-11 08:35:53 +0800

    Time: 71 ms (99.91%), Space: 51.5 MB (52.17%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.cpp
+++ 217-contains-duplicate/217-contains-duplicate.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     bool containsDuplicate(vector<int>& nums) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         unordered_set<int> seen;
         for (int& i : nums) {
