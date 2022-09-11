3e35138c7eaf2b47ea447424dc641c3d55012065
Zheng Yong <angzhengyong@gmail.com>
2022-09-11 08:52:12 +0800

    Time: 6 ms (96.02%), Space: 10.2 MB (44.09%) - LeetHub

--- 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
+++ 1356-sort-integers-by-the-number-of-1-bits/1356-sort-integers-by-the-number-of-1-bits.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     vector<int> sortByBits(vector<int>& arr) {
+        ios_base::sync_with_stdio(0);
+        
         sort(arr.begin(), arr.end(), [](int i, int j) {
             bitset<14> bi(i), bj(j);
             return bi.count() != bj.count() ? bi.count() < bj.count() : i < j;
