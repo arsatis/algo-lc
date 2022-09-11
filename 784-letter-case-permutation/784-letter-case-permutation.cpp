829d13ea98748c83125407e82f00f5fff7db6eab
Zheng Yong <angzhengyong@gmail.com>
2022-09-11 11:54:35 +0800

    Time: 4 ms (92.14%), Space: 9.6 MB (84.22%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.cpp
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -17,6 +17,8 @@ class Solution {
     }
 public:
     vector<string> letterCasePermutation(string s) {
+        ios_base::sync_with_stdio(0);
+        
         vector<string> perms;
         f(perms, s, 0);
         return move(perms);
