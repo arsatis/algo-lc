431e25bfa7e21c1947fdf04162c9623d6d0cc9c8
Zheng Yong <angzhengyong@gmail.com>
2022-09-11 11:37:58 +0800

    Time: 13 ms (94.70%), Space: 9.2 MB (68.27%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -1,7 +1,7 @@
 class Solution {
     void f(int n, int k, vector<vector<int>>& combs, vector<int>& comb) {
         if (comb.size() == k) combs.push_back(comb);
-        else for (int i = comb.empty() ? 1 : comb.back() + 1; i + k - comb.size() - 1 <= n; ++i) {
+        else for (int i = comb.empty() ? 1 : comb.back() + 1; i + (k - comb.size() - 1) <= n; ++i) {
             comb.emplace_back(i);
             f(n, k, combs, comb);
             comb.pop_back();
@@ -9,6 +9,8 @@ class Solution {
     }
 public:
     vector<vector<int>> combine(int n, int k) {
+        ios_base::sync_with_stdio(0);
+        
         vector<vector<int>> combs;
         vector<int> comb;
         comb.reserve(k);
