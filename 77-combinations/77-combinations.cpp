a6523718198bbfc04d15188dd56fe932334d70f0
Zheng Yong <angzhengyong@gmail.com>
2022-09-11 11:33:08 +0800

    Time: 65 ms (28.14%), Space: 9 MB (81.46%) - LeetHub

---
+++ 77-combinations/77-combinations.cpp
@@ -0,0 +1,17 @@
+class Solution {
+    void f(int n, int k, vector<vector<int>>& combs, vector<int>& comb) {
+        if (comb.size() == k) combs.push_back(comb);
+        else for (int i = comb.empty() ? 1 : comb.back() + 1; i <= n; ++i) {
+            comb.emplace_back(i);
+            f(n, k, combs, comb);
+            comb.pop_back();
+        }
+    }
+public:
+    vector<vector<int>> combine(int n, int k) {
+        vector<vector<int>> combs;
+        vector<int> comb;
+        f(n, k, combs, comb);
+        return combs;
+    }
+};
