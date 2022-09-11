80e6268b630463d3b162be4a08fbfe5a991ae81f
Zheng Yong <angzhengyong@gmail.com>
2022-09-11 08:34:27 +0800

    Time: 73 ms (100.00%), Space: 37.4 MB (30.99%) - LeetHub

--- 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
+++ 1383-maximum-performance-of-a-team/1383-maximum-performance-of-a-team.cpp
@@ -3,21 +3,21 @@ public:
     int maxPerformance(int n, vector<int>& speed, vector<int>& efficiency, int k) {
         ios_base::sync_with_stdio(0);
         
-        vector<pair<int, int>> ess(n);
-        for (int i = 0; i < n; ++i)
-            ess[i] = {efficiency[i], speed[i]};
-        sort(rbegin(ess), rend(ess));
-        long sumS = 0, res = 0;
-        priority_queue <int, vector<int>, greater<int>> pq; //min heap
-        for(auto& [e, s]: ess){
-            pq.emplace(s);
-            sumS += s;
-            if (pq.size() > k) {
-                sumS -= pq.top();
-                pq.pop();
+        vector<pair<int, int>> ens;
+        for(int i=0; i<speed.size(); i++) ens.emplace_back(efficiency[i], speed[i]);
+        sort(ens.begin(), ens.end(), greater<pair<int, int>>());
+        long res = 0;
+        long sum = 0;
+        priority_queue<int, vector<int>, greater<int>> q;
+        for(int i=0; i<ens.size(); i++) {
+            q.push(ens[i].second);
+            sum += ens[i].second;
+            if(q.size() > k) {
+                sum -= q.top();
+                q.pop();
             }
-            res = max(res, sumS * e);
+            res = max(res, sum*ens[i].first);
         }
-        return res % (int)(1e9+7);
+        return res % (long)(1e9+7);
     }
 };
