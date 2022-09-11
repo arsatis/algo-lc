347d524a5495f70037c3ed44b52013731353e29a
Zheng Yong <angzhengyong@gmail.com>
2022-09-11 11:35:05 +0800

    Time: 16 ms (92.67%), Space: 8.7 MB (98.85%) - LeetHub

--- 77-combinations/77-combinations.cpp
+++ 77-combinations/77-combinations.cpp
@@ -9,13 +9,57 @@ class Solution {
     }
 public:
     vector<vector<int>> combine(int n, int k) {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-        
-        vector<vector<int>> combs;
-        vector<int> comb;
-        f(n, k, combs, comb);
-        return move(combs);
+        vector<vector<int>> result;
+        vector<int> tmp;
+        tmp.reserve(k);
+
+        if (k == 1) {
+            for (int i = 1; i <= n; ++i) {
+                tmp.push_back(i);
+                result.push_back(tmp);
+                tmp.pop_back();
+            }
+
+            return result;
+        }
+
+        if (k == n) {
+            for (int i = 1; i <= n; ++i) {
+                tmp.push_back(i);
+            }
+            result.push_back(tmp);
+            return result;
+        }
+
+        for (int i = 1; i < k; ++i) {
+            tmp.push_back(i);
+        }
+
+        while (!tmp.empty()) {
+            int& index = tmp.back();
+
+            for (int i = index + 1; i <= n; ++i) {
+                tmp.push_back(i);
+                result.push_back(tmp);
+                tmp.pop_back();
+            }
+
+            int pos = 0;
+            while (!tmp.empty()) {
+                int& back = tmp.back();
+
+                if (back + 1 >= n - pos++) {
+                    tmp.pop_back();
+                }
+                else {
+                    int i = ++back;
+                    while (tmp.size() < k - 1)tmp.push_back(++i);
+                    break;
+                }
+            }
+
+        }
+
+        return result;
     }
 };
