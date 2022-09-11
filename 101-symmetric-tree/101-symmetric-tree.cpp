2678ec5150f09f63122166a779c20f49b2a50067
Zheng Yong <angzhengyong@gmail.com>
2022-09-11 09:11:20 +0800

    Time: 0 ms (100.00%), Space: 16.4 MB (77.87%) - LeetHub

--- 101-symmetric-tree/101-symmetric-tree.cpp
+++ 101-symmetric-tree/101-symmetric-tree.cpp
@@ -19,6 +19,8 @@ class Solution {
 public:
     bool isSymmetric(TreeNode* root) {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
         
         if (!root) return true;
         return f(root->left, root->right);
