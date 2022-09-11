aca58fae71e1e4552a0ab89b3177185c5655c70b
Zheng Yong <angzhengyong@gmail.com>
2022-09-11 11:50:58 +0800

    Time: 11 ms (58.08%), Space: 10.6 MB (67.45%) - LeetHub

---
+++ 784-letter-case-permutation/784-letter-case-permutation.cpp
@@ -0,0 +1,23 @@
+class Solution {
+    void f(vector<string>& perms, string& s, int begin) {
+        for (int i = begin; i < s.size(); ++i) {
+            if (s[i] > 64 && s[i] < 91) {
+                s[i] += 32;
+                f(perms, s, i + 1);
+                s[i] -= 32;
+            }
+            if (s[i] > 96 && s[i] < 123) {
+                s[i] -= 32;
+                f(perms, s, i + 1);
+                s[i] += 32;
+            }
+        }
+        perms.push_back(s);
+    }
+public:
+    vector<string> letterCasePermutation(string s) {
+        vector<string> perms;
+        f(perms, s, 0);
+        return move(perms);
+    }
+};
