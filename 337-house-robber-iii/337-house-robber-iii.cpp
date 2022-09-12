2616f6f184bbb1c1ecd4e33d4793e394b158e29d
Zheng Yong <angzhengyong@gmail.com>
2022-09-12 20:40:06 +0800

    Time: 15 ms (90.55%), Space: 22.2 MB (76.96%) - LeetHub

--- 337-house-robber-iii/337-house-robber-iii.cpp
+++ 337-house-robber-iii/337-house-robber-iii.cpp
@@ -10,18 +10,18 @@
  * };
  */
 class Solution {
+    int f(TreeNode* root, int &robMax, int &notRobMax) {
+        if (!root) return 0;
+        int leftRobMax = 0, leftNotRobMax = 0, rightRobMax = 0, rightNotRobMax = 0;
+        int leftMax = f(root->left, leftRobMax, leftNotRobMax);
+        int rightMax = f(root->right, rightRobMax, rightNotRobMax);
+        robMax = root->val + leftNotRobMax + rightNotRobMax;
+        notRobMax = leftMax + rightMax;
+        return max(robMax, notRobMax);
+    }
 public:
-    int sum = 0;
     int rob(TreeNode* root) {
-        if (!root) {
-            sum = 0;
-            return 0;
-        }
-        int l = rob(root->left);
-        int preSum = sum;
-        int r = rob(root->right);
-        preSum += sum;
-        sum = l + r;
-        return max(root->val + preSum, sum);
+        int robMax = 0, notRobMax = 0;
+        return f(root, robMax, notRobMax);
     }
 };
