48426278a7f8d1b7b8975cbf9b241dba7c422418
Zheng Yong <angzhengyong@gmail.com>
2022-09-12 13:47:17 +0800

    Time: 4 ms (36.30%), Space: 7.6 MB (77.67%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int rob(vector<int>& nums) {
-        if (nums.size() == 1) return nums[0];
-        if (nums.size() == 2) return max(nums[0], nums[1]);
-        int i = nums[0], j = nums[1], k = nums[0] + nums[2];
-        for (int x = 3; x < nums.size(); ++x) {
-            i = max(i, j) + nums[x];
-            swap(i, k);
-            swap(i, j);
+        int i = 0, j = 0;
+        for (int x : nums) {
+            int tmp = i;
+            i = max(j + x, i);
+            j = tmp;
         }
-        return max(j, k);
+        return i;
     }
 };
