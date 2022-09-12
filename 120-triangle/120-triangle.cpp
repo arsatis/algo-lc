e7718b1ee731118c87921fb37fd592eb5fb05865
Zheng Yong <angzhengyong@gmail.com>
2022-09-12 11:34:15 +0800

    Time: 12 ms (39.69%), Space: 8.6 MB (93.55%) - LeetHub

--- 120-triangle/120-triangle.cpp
+++ 120-triangle/120-triangle.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
         vector<int> minlen(triangle.back());
         for (int layer = triangle.size() - 2; layer >= 0; --layer)
             for (int i = 0; i <= layer; ++i)
