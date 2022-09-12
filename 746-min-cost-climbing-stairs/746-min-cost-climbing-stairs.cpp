00011e37fdbd0b9addba82b46311aae103c8e388
Zheng Yong <angzhengyong@gmail.com>
2022-09-12 11:15:07 +0800

    Time: 7 ms (83.44%), Space: 13.8 MB (53.61%) - LeetHub

--- 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
+++ 746-min-cost-climbing-stairs/746-min-cost-climbing-stairs.cpp
@@ -3,8 +3,8 @@ public:
     int minCostClimbingStairs(vector<int>& cost) {
         ios_base::sync_with_stdio(0);
         
-        int dp[3] = { 0 };
-        for (int i = 2; i < cost.size() + 1; ++i)
+        int n = cost.size() + 1, dp[3] = { 0 };
+        for (int i = 2; i < n; ++i)
             dp[i % 3] = min(dp[(i - 2) % 3] + cost[i - 2],
                             dp[(i - 1) % 3] + cost[i - 1]);
         return dp[cost.size() % 3];
