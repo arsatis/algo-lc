14b5487beaa4f37748ce1f4ff4e3f103ea5266c3
Zheng Yong <angzhengyong@gmail.com>
2022-09-13 21:48:33 +0800

    Time: 19 ms (68.42%), Space: 11.5 MB (93.41%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -5,20 +5,16 @@ public:
         int max_earned = 0, current_sum = nums[0];
         pair<int, int> earned_vm1{0, 0}, earned_vm2{0, 0};
         
-        for (int j = 1; j <= nums.size(); ++j) {
-            const auto& v = nums[j-1];
-            if ((j < nums.size()) && (v == nums[j])) {
-                current_sum += nums[j];
+        for (int i = 1; i <= nums.size(); ++i) {
+            int v = nums[i - 1];
+            if ((i < nums.size()) && (v == nums[i])) {
+                current_sum += nums[i];
                 continue;
             }
-            // decide to take v or v-1
-            // - take v: earned[v-2] + sum(v), as v-1 is cleared
-            // - take v-1: earned[v-1]
-            const auto earned_v = max(current_sum + earned_vm2.second, earned_vm1.second);
-            //cout << v << ":" << earned_v << endl;        
+            int earned_v = max(current_sum + earned_vm2.second, earned_vm1.second);
             if (max_earned < earned_v) max_earned = earned_v;
-            if (j < nums.size()) {
-                int v_new = nums[j];
+            if (i < nums.size()) {
+                int v_new = nums[i];
                 current_sum = v_new;
                 if (v == v_new - 1) earned_vm2 = earned_vm1;
                 else earned_vm2 = { v, earned_v };
