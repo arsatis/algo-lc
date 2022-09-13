b87859daaf4f2947ec90ab4a3873a59f268b5469
Zheng Yong <angzhengyong@gmail.com>
2022-09-13 21:46:44 +0800

    Time: 23 ms (55.19%), Space: 11.3 MB (99.87%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        ios_base::sync_with_stdio(0);
-        
         sort(nums.begin(), nums.end());
         int max_earned = 0, current_sum = nums[0];
         pair<int, int> earned_vm1{0, 0}, earned_vm2{0, 0};
@@ -20,7 +18,7 @@ public:
             //cout << v << ":" << earned_v << endl;        
             if (max_earned < earned_v) max_earned = earned_v;
             if (j < nums.size()) {
-                const auto& v_new = nums[j];
+                auto& v_new = nums[j];
                 current_sum = v_new;
                 if (v == v_new - 1) earned_vm2 = earned_vm1;
                 else earned_vm2 = make_pair(v, earned_v);
