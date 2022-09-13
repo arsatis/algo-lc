f8c3a582d5b902dc54f769a3ccc197978fa27385
Zheng Yong <angzhengyong@gmail.com>
2022-09-13 21:44:10 +0800

    Time: 44 ms (15.62%), Space: 12.9 MB (69.29%) - LeetHub

---
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        map<int, int> counts;
+        int i = 0, j = 0, prev = 10002;
+        
+        for (int x : nums) counts[x] += x;
+        for (auto it = counts.rbegin(); it != counts.rend(); ++it) {
+            int tmp = j;
+            if (prev - it->first > 1) {
+                j = max(i, j) + it->second;
+                prev = it->first;
+            } else if (i + it->second > j) {
+                j = i + it->second;
+                prev = it->first;
+            }
+            i = tmp;
+        }
+        return j;
+    }
+};
