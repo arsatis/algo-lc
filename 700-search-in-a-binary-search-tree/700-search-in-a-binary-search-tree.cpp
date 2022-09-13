2f20f9aab00fc83c786a8050f4a969c21ad5a628
Zheng Yong <angzhengyong@gmail.com>
2022-09-13 20:09:23 +0800

    Time: 52 ms (76.10%), Space: 34.9 MB (22.40%) - LeetHub

--- 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
+++ 700-search-in-a-binary-search-tree/700-search-in-a-binary-search-tree.cpp
@@ -13,8 +13,6 @@ class Solution {
 public:
     TreeNode* searchBST(TreeNode* root, int val) {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         
         if (!root || root->val == val) return root;
         TreeNode* left = root->left ? searchBST(root->left, val) : nullptr;
