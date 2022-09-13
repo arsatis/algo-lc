9e240f4e84132f9a60e1491dbc725d04fcdc9a53
Zheng Yong <angzhengyong@gmail.com>
2022-09-13 22:06:25 +0800

    Time: 19 ms (68.42%), Space: 11.5 MB (97.19%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,29 +1,16 @@
 class Solution {
 public:
     int deleteAndEarn(vector<int>& nums) {
-        ios_base::sync_with_stdio(0);
-        
-        sort(nums.begin(), nums.end());
-        int maxPoints = 0, currPoints = nums[0];
-        pair<int, int> prev1 = {0, 0}, prev2 = {0, 0};
-        
-        for (int i = 1; i <= nums.size(); ++i) {
-            int v = nums[i - 1];
-            if (i < nums.size() && v == nums[i]) {
-                currPoints += nums[i];
-                continue;
-            }
-            
-            int vEarned = max(currPoints + prev2.second, prev1.second);
-            if (maxPoints < vEarned) maxPoints = vEarned;
-            if (i < nums.size()) {
-                int vNew = nums[i];
-                currPoints = vNew;
-                if (v == vNew - 1) prev2 = prev1;
-                else prev2 = { v, vEarned };
-                prev1 = { v, vEarned };
-            }
+        if (nums.size() == 1) return nums[0];
+
+        int dp[10001] = { 0 };
+        for (int n : nums) dp[n] += n;
+     
+        int x = dp[1];
+        for (int i = 2; i <= 10000; ++i) {
+            dp[i] = max(dp[i - 1], dp[i] + dp[i - 2]);
+            if (dp[i] > x) x = dp[i];
         }
-        return maxPoints;
+        return x;
     }
 };
