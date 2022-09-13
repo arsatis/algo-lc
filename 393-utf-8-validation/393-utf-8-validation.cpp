2b276b8e7404e8b31105d693c66c07994ecd6224
Zheng Yong <angzhengyong@gmail.com>
2022-09-13 09:41:37 +0800

    Time: 22 ms (45.39%), Space: 14.1 MB (38.65%) - LeetHub

--- 393-utf-8-validation/393-utf-8-validation.cpp
+++ 393-utf-8-validation/393-utf-8-validation.cpp
@@ -1,6 +1,8 @@
 class Solution {
 public:
     bool validUtf8(vector<int>& data) {
+        ios_base::sync_with_stdio(0);
+        
         int rb = 0;
         for (int val : data) {
             if (!rb) {
