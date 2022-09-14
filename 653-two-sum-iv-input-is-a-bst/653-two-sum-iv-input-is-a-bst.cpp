3b4a021e67dbcb9bf2c7e988c11429fb7c0c9fd9
Zheng Yong <angzhengyong@gmail.com>
2022-09-14 16:18:36 +0800

    Time: 98 ms (14.06%), Space: 36.7 MB (80.01%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -10,19 +10,41 @@
  * };
  */
 class Solution {
-    unordered_set<int> comps;
 public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
+    void inorder(TreeNode*root,vector<int>&in){
+        if(root==NULL){
+            return;
+        }
+        
+        inorder(root->left,in);
+        
+        in.push_back(root->val);
+        
+        inorder(root->right,in);
+        
+        return;
     }
-    
     bool findTarget(TreeNode* root, int k) {
-        if (!root) return false;
-        if (comps.find(root->val) != comps.end()) return true;
-        comps.insert(k - root->val);
-        return (root->left ? findTarget(root->left, k) : false) ||
-               (root->right ? findTarget(root->right, k) : false);
+        vector<int>in;
+        
+        inorder(root,in);
+        
+        int n=in.size();
+        int i=0;
+        int j=n-1;
+        
+        while(i<j){
+            if(in[i]+in[j]==k){
+                return true;
+            }
+            
+            else if(in[i]+in[j]>k){
+                j--;
+            }
+            else{
+                i++;
+            }
+        }
+    return false;
     }
 };
