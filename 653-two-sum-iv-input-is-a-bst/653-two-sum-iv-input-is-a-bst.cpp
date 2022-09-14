a226c1bdd0e2a483f6d2377df0f027b24b77dfc9
Zheng Yong <angzhengyong@gmail.com>
2022-09-14 16:17:39 +0800

    Time: 52 ms (77.43%), Space: 39.1 MB (18.04%) - LeetHub

--- 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
+++ 653-two-sum-iv-input-is-a-bst/653-two-sum-iv-input-is-a-bst.cpp
@@ -14,6 +14,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     bool findTarget(TreeNode* root, int k) {
