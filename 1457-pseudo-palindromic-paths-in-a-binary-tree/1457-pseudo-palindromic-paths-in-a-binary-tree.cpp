f0923a080159e129f6c4e51e33e9bbd10b97a74a
Zheng Yong <angzhengyong@gmail.com>
2022-09-14 16:38:25 +0800

    Time: 332 ms (99.70%), Space: 179.4 MB (91.11%) - LeetHub

--- 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
+++ 1457-pseudo-palindromic-paths-in-a-binary-tree/1457-pseudo-palindromic-paths-in-a-binary-tree.cpp
@@ -14,6 +14,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int pseudoPalindromicPaths(TreeNode* root) {
