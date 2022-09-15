4a22a26a7b794b8a3a56e72763df1e5c3f0f5d3f
Zheng Yong <angzhengyong@gmail.com>
2022-09-15 10:33:33 +0800

    Time: 321 ms (97.14%), Space: 124.2 MB (93.51%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,27 +1,24 @@
 class Solution {
 public:
     vector<int> findOriginalArray(vector<int>& changed) {
-        ios::sync_with_stdio(false);
+        ios::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
         
-        if(changed.size() & 1) return {};
+        if (changed.size() & 1) return {};
         
         int M = *max_element(changed.begin(), changed.end()), size = 1 + (M << 1);
-        int* v = new int[size]{};
-        vector<int> ans;
+        vector<int> v(size), ans;
         
-        for(int &x : changed) v[x]++;
-        
-        if(v[0] & 1) return {};
+        for (int& x : changed) ++v[x];
+        if (v[0] & 1) return {};
         ans.resize(v[0] >> 1);
-        for(int i = 1; i != size; i++){
-            if(!v[i]) continue;
-            if(v[i << 1] < v[i]) return {};
+        for (int i = 1; i != size; ++i) {
+            if (!v[i]) continue;
+            if (v[i << 1] < v[i]) return {};
             v[i << 1] -= v[i];
             ans.resize(ans.size() + v[i], i);
         }
-        
         return move(ans);
     }
 };
