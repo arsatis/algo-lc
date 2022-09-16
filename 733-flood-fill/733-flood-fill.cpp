09c7d8d637edcd11d4f4d8b5b36fcac205417392
Zheng Yong <angzhengyong@gmail.com>
2022-09-16 20:30:56 +0800

    Time: 21 ms (25.00%), Space: 14.7 MB (5.50%) - LeetHub

--- 733-flood-fill/733-flood-fill.cpp
+++ 733-flood-fill/733-flood-fill.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
     }
     
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
