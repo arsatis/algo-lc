6f470ae9e095a713b0383023cebf715ada39e169
Zheng Yong <angzhengyong@gmail.com>
2022-09-17 17:03:24 +0800

    Time: 348 ms (99.14%), Space: 120.8 MB (62.32%) - LeetHub

--- 336-palindrome-pairs/336-palindrome-pairs.cpp
+++ 336-palindrome-pairs/336-palindrome-pairs.cpp
@@ -1,10 +1,3 @@
-static const auto fastIO = []() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(0);
-    cout.tie(0);
-    return 0;
-}();
-
 class Solution {
 public:
     vector<vector<int>> palindromePairs(vector<string>& words) {
