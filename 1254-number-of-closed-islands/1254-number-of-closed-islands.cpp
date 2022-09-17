1d20fcde1f4796b66539d3b8555a6d419b56f176
Zheng Yong <angzhengyong@gmail.com>
2022-09-17 20:14:13 +0800

    Time: 24 ms (42.17%), Space: 9.5 MB (35.08%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,8 +1,4 @@
 class Solution {
-    int fill(vector<vector<int>>& g, int i, int j) {
-        if (i < 0 || j < 0 || i >= g.size() || j >= g[i].size() || g[i][j]) return 0;
-        return (g[i][j] = 1) + fill(g, i + 1, j) + fill(g, i, j + 1) + fill(g, i - 1, j) + fill(g, i, j - 1);
-    }
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
@@ -10,14 +6,59 @@ public:
         cout.tie(0);
     }
     
-    int closedIsland(vector<vector<int>>& g, int res = 0) {
-        for (int i = 0; i < g.size(); ++i)
-            for (int j = 0; j < g[i].size(); ++j)
-                if (i * j == 0 || i == g.size() - 1 || j == g[i].size() - 1)
-                    fill(g, i, j);
-        for (int i = 0; i < g.size(); ++i)
-            for (int j = 0; j < g[i].size(); ++j)
-                res += fill(g, i, j) > 0;
-        return res;
+    int closedIsland(vector<vector<int>>& grid) {
+        int n = grid.size(),m = grid[0].size();
+        
+        for(int i = 0;i<n;i++){
+            
+            if(grid[i][0] == 0)
+                dfs(i,0,grid,n,m);
+            if(grid[i][m-1] == 0)
+                dfs(i,m-1,grid,n,m);
+        }
+        for(int i = 0;i<m;i++){
+            if(grid[0][i] == 0)
+                dfs(0,i,grid,n,m);
+            if(grid[n-1][i] == 0)
+                dfs(n-1,i,grid,n,m);
+        }
+        
+        int ans = 0;
+        for(int i = 0;i<n;i++){
+            for(int j = 0;j<m;j++){
+                if(grid[i][j] == 0){
+                    ans++;
+                    dfs1(i,j,grid,n,m);
+                }
+            }
+        }
+        return ans;
+    }
+    
+    void dfs1(int i,int j,vector<vector<int>> &grid,int n,int m){
+        grid[i][j] = 1;
+        int dx[] = {0,0,-1,1};
+        int dy[] = {1,-1,0,0};
+        
+        for(int l = 0;l<4;l++){
+            int ni = i + dx[l];
+            int nj = j + dy[l];
+            if(ni>0 and nj>0 and ni<n and nj< m and grid[ni][nj] == 0)
+                dfs(ni,nj,grid,n,m);
+        }
+    }
+    
+    void dfs(int i,int j,vector<vector<int>> &grid,int n,int m){
+        //if(i<0 or j<0 or i>=n or j>= m) return;
+        grid[i][j] = 1;
+        int dx[] = {0,0,-1,1};
+        int dy[] = {1,-1,0,0};
+        
+        for(int l = 0;l<4;l++){
+            int ni = i + dx[l];
+            int nj = j + dy[l];
+            if(ni>0 and nj>0 and ni<n and nj< m and grid[ni][nj] == 0)
+                dfs(ni,nj,grid,n,m);
+        }
     }
 };
