667d934aa167f2235cd999d68a08c79dc72bee17
Zheng Yong <angzhengyong@gmail.com>
2022-09-17 17:05:10 +0800

    Time: 273 ms (21.24%), Space: 93.3 MB (89.14%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
-    }
-    
     int maxProfit(vector<int>& prices) {
         int maxProfit = 0, minPrice = INT_MAX;
         for (auto& p : prices) {
