2bc1a7eac15f50f47c3adc81ddbeed6b4d9a601e
Zheng Yong <angzhengyong@gmail.com>
2022-09-17 20:07:22 +0800

    Time: 105 ms (33.89%), Space: 39.7 MB (24.38%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.cpp
@@ -8,7 +8,7 @@ public:
     
     int maxScoreSightseeingPair(vector<int>& values) {
         int res = 0, imax = 0;
-        for(int i = 0; i < values.size(); ++i) {
+        for (int i = 0; i < values.size(); ++i) {
             res = max(res, imax + values[i] - i);
             imax = max(imax, values[i] + i);
         }
