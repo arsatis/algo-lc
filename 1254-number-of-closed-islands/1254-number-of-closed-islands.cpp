c3632c384e69a8fa38ae2567fd027ddc06187901
Zheng Yong <angzhengyong@gmail.com>
2022-09-17 20:11:48 +0800

    Time: 19 ms (63.32%), Space: 9.4 MB (66.28%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.cpp
@@ -1,40 +1,21 @@
 class Solution {
-    void dfs(vector<vector<int>>& grid, int i, int j) {
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        if (i < 0 || j < 0 || i >= m || j >= n || grid[i][j] == 1) return;
-        grid[i][j] = 1;
-        
-        dfs(grid, i - 1, j);
-        dfs(grid, i + 1, j);
-        dfs(grid, i, j - 1);
-        dfs(grid, i, j + 1);
+    int fill(vector<vector<int>>& g, int i, int j) {
+        if (i < 0 || j < 0 || i >= g.size() || j >= g[i].size() || g[i][j]) return 0;
+        return (g[i][j] = 1) + fill(g, i + 1, j) + fill(g, i, j + 1) + fill(g, i - 1, j) + fill(g, i, j - 1);
     }
 public:
-    int closedIsland(vector<vector<int>>& grid) {
-        int m = grid.size();
-        int n = grid[0].size();
-        
-        if (m <= 2 || n <= 2) return 0;
-        for (int i = 0; i < n; i++) {
-            if (grid[0][i] == 0) dfs(grid, 0, i);
-            if (grid[m - 1][i] == 0) dfs(grid, m - 1, i);
-        }
-        for (int i = 0; i < m; i++) {
-            if (grid[i][0] == 0) dfs(grid, i, 0);
-            if (grid[i][n - 1] == 0) dfs(grid, i, n - 1);
-        }
-        
-        int count = 0;
-        for (int i = 1; i < m-1; i++) {
-            for (int j = 1; j < n-1; j++) {
-                if (grid[i][j] == 0) {
-                    count++;
-                    dfs(grid, i, j);
-                }
-            }
-        }
-        return count;
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
+    int closedIsland(vector<vector<int>>& g, int res = 0) {
+        for (int i = 0; i < g.size(); ++i)
+            for (int j = 0; j < g[i].size(); ++j)
+                if (i * j == 0 || i == g.size() - 1 || j == g[i].size() - 1)
+                    fill(g, i, j);
+        for (int i = 0; i < g.size(); ++i)
+            for (int j = 0; j < g[i].size(); ++j)
+                res += fill(g, i, j) > 0;
+        return res;
     }
 };
