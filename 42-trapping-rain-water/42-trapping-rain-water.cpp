f80c6176be5d5999128c85fe0c5566cebeae5013
Zheng Yong <angzhengyong@gmail.com>
2022-09-18 18:45:57 +0800

    Time: 22 ms (69.54%), Space: 19.9 MB (60.61%) - LeetHub

--- 42-trapping-rain-water/42-trapping-rain-water.cpp
+++ 42-trapping-rain-water/42-trapping-rain-water.cpp
@@ -7,21 +7,17 @@ public:
     }
     
     int trap(vector<int>& height) {
-        if(height.empty()) return 0;
-        int ans = 0;
-        int size = height.size();
-        vector<int> left_max(size), right_max(size);
-        left_max[0] = height[0];
-        for (int i = 1; i < size; i++) {
-            left_max[i] = max(height[i], left_max[i - 1]);
+        int total = 0, leftMax = 0, rightMax = 0;
+        
+        for (int left = 0, right = height.size() - 1; left < right;) {
+            if (height[left] < height[right]) {
+                if (height[left] >= leftMax) leftMax = height[left++];
+                else total += leftMax - height[left++];
+            } else {
+                if (height[right] >= rightMax) rightMax = height[right--];
+                else total += rightMax - height[right--];
+            }
         }
-        right_max[size - 1] = height[size - 1];
-        for (int i = size - 2; i >= 0; i--) {
-            right_max[i] = max(height[i], right_max[i + 1]);
-        }
-        for (int i = 1; i < size - 1; i++) {
-            ans += min(left_max[i], right_max[i]) - height[i];
-        }
-        return ans;
+        return total;
     }
 };
