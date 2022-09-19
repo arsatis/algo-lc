a8e26ea1d234487b7895395e0abc9cb6ba76f86c
Zheng Yong <angzhengyong@gmail.com>
2022-09-19 12:32:28 +0800

    Time: 152 ms (71.37%), Space: 32 MB (98.09%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     vector<vector<string>> findDuplicate(vector<string>& paths) {
