963e5b9799a705278c2df68d19db35670d26a632
Zheng Yong <angzhengyong@gmail.com>
2022-09-19 14:13:38 +0800

    Time: 777 ms (13.50%), Space: 89.4 MB (75.30%) - LeetHub

---
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -0,0 +1,19 @@
+class Solution {
+    int bfs(vector<vector<int>>& grid1, vector<vector<int>>& grid2, int i, int j) {
+        grid2[i][j] = 0;
+        bool isSubIsland = grid1[i][j];
+        if (i > 0 && grid2[i - 1][j]) isSubIsland &= bfs(grid1, grid2, i - 1, j);
+        if (j > 0 && grid2[i][j - 1]) isSubIsland &= bfs(grid1, grid2, i, j - 1);
+        if (i < grid2.size() - 1 && grid2[i + 1][j]) isSubIsland &= bfs(grid1, grid2, i + 1, j);
+        if (j < grid2[0].size() - 1 && grid2[i][j + 1]) isSubIsland &= bfs(grid1, grid2, i, j + 1);
+        return isSubIsland;
+    }
+public:
+    int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
+        int m = grid1.size(), n = grid1[0].size(), num = 0;
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (grid2[i][j]) num += bfs(grid1, grid2, i, j);
+        return num;
+    }
+};
