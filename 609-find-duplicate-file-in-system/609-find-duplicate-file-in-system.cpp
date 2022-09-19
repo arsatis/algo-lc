d6694ba2817552178cc3ce34f6dc366c9f69cb4d
Zheng Yong <angzhengyong@gmail.com>
2022-09-19 12:31:56 +0800

    Time: 174 ms (59.54%), Space: 31.6 MB (99.24%) - LeetHub

---
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<vector<string>> findDuplicate(vector<string>& paths) {
+        unordered_map<string, vector<string>> dict;
+        
+        for (string& s : paths) {
+            auto it = find(s.begin(), s.end(), ' ');
+            string path_to_file(s.begin(), it);
+            
+            while (it != s.end()) {
+                auto content_start = find(++it, s.end(), '('),
+                     content_end = find(content_start, s.end(), ')');
+                string file_name(it, content_start++), key(content_start, content_end);
+                
+                dict[key].emplace_back(path_to_file + '/' + file_name);
+                it = find(content_end, s.end(), ' ');
+            }
+        }
+        
+        vector<vector<string>> dups;
+        for (auto& p : dict) if (p.second.size() > 1) dups.emplace_back(move(p.second));
+        return move(dups);
+    }
+};
