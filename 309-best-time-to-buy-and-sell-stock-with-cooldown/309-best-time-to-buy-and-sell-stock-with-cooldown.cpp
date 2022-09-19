da2ecc00e791c14922d510343f979565a3d8a401
Zheng Yong <angzhengyong@gmail.com>
2022-09-19 17:27:03 +0800

    Time: 4 ms (82.09%), Space: 11.2 MB (78.09%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -2,13 +2,11 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
     }
     
     int maxProfit(vector<int>& prices) {
         int cooldown = 0, sell = 0, buy = -prices[0];
-        for (int& i : prices) {
+        for (int i : prices) {
             int tmp = sell;
             sell = buy + i;
             buy = max(buy, cooldown - i);
