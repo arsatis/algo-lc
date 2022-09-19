9d614db22ec970d33a456507e33a95910922c403
Zheng Yong <angzhengyong@gmail.com>
2022-09-19 13:17:39 +0800

    Time: 140 ms (77.48%), Space: 32.2 MB (98.09%) - LeetHub

--- 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
+++ 609-find-duplicate-file-in-system/609-find-duplicate-file-in-system.cpp
@@ -5,39 +5,29 @@ public:
         cin.tie(0);
         cout.tie(0);
     }
+    
     vector<vector<string>> findDuplicate(vector<string>& paths) {
+        unordered_map<string, vector<string>> dict;
         
-        unordered_map<string, vector<string>> mp;
         for (const string& path : paths) {
             string pathStart;
             int i = 0;
-            while (path[i] != ' ') {
-                pathStart.push_back(path[i]);
-                i++;
-            }
-            i++;
+            while (path[i] != ' ') pathStart.push_back(path[i++]);
+            ++i;
             
             while (i < path.size()) {
                 string fileName, fileData;
-                while (path[i] != '(') {
-                    fileName.push_back(path[i]);
-                    i++;
-                }
-                i++;
-                while (path[i] != ')') {
-                    fileData.push_back(path[i]);
-                    i++;
-                }
+                while (path[i] != '(') fileName.push_back(path[i++]);
+                ++i;
+                
+                while (path[i] != ')') fileData.push_back(path[i++]);
                 i += 2;
-                mp[fileData].push_back(pathStart + "/" + fileName);
+                dict[fileData].push_back(pathStart + "/" + fileName);
             }     
         }
         
-        vector<vector<string>> res;
-        for (const auto& [key, value] : mp)
-            if (value.size() > 1)
-                res.push_back(value);
-        
-        return res;
+        vector<vector<string>> dups;
+        for (auto& [key, value] : dict) if (value.size() > 1) dups.emplace_back(move(value));
+        return move(dups);
     }
 };
