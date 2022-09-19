c3f3b1025618aa06772bc627aebec97b599b5064
Zheng Yong <angzhengyong@gmail.com>
2022-09-19 14:14:06 +0800

    Time: 541 ms (49.07%), Space: 89.4 MB (79.45%) - LeetHub

--- 1905-count-sub-islands/1905-count-sub-islands.cpp
+++ 1905-count-sub-islands/1905-count-sub-islands.cpp
@@ -11,6 +11,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int countSubIslands(vector<vector<int>>& grid1, vector<vector<int>>& grid2) {
