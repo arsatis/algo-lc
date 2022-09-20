1a2055bfa3bc60e15130635e4c5196025ae293f1
Zheng Yong <angzhengyong@gmail.com>
2022-09-20 10:17:54 +0800

    Time: 74 ms (98.60%), Space: 17 MB (65.29%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -1,19 +1,56 @@
 class Solution {
-public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
+    const int mod = 1000000009;
+    const int base = 113;
+    long long qPow(long long x, long long n) {
+        long long ret = 1;
+        while (n) {
+            if (n & 1) {
+                ret = ret * x % mod;
+            }
+            x = x * x % mod;
+            n >>= 1;
+        }
+        return ret;
     }
-    
-    int findLength(vector<int>& nums1, vector<int>& nums2) {
-        int m = nums1.size(), n = nums2.size(), maxLen = 0;
-        vector<vector<int>> dp(m + 1, vector<int>(n + 1));
-        
-        for (int i = m - 1; i >= 0; --i)
-            for (int j = n - 1; j >= 0; --j)
-                if (nums1[i] == nums2[j]) {
-                    dp[i][j] = 1 + dp[i + 1][j + 1];
-                    if (maxLen < dp[i][j]) maxLen = dp[i][j];
-                }
-        return maxLen;
+
+    bool check(vector<int>& A, vector<int>& B, int len) {
+        long long hashA = 0;
+        for (int i = 0; i < len; i++) {
+            hashA = (hashA * base + A[i]) % mod;
+        }
+        unordered_set<long long> bucketA;
+        bucketA.insert(hashA);
+        long long mult = qPow(base, len - 1);
+        for (int i = len; i < A.size(); i++) {
+            hashA = ((hashA - A[i - len] * mult % mod + mod) % mod * base + A[i]) % mod;
+            bucketA.insert(hashA);
+        }
+        long long hashB = 0;
+        for (int i = 0; i < len; i++) {
+            hashB = (hashB * base + B[i]) % mod;
+        }
+        if (bucketA.count(hashB)) {
+            return true;
+        }
+        for (int i = len; i < B.size(); i++) {
+            hashB = ((hashB - B[i - len] * mult % mod + mod) % mod * base + B[i]) % mod;
+            if (bucketA.count(hashB)) {
+                return true;
+            }
+        }
+        return false;
+    }
+public:
+    int findLength(vector<int>& A, vector<int>& B) {
+        int left = 1, right = min(A.size(), B.size()) + 1;
+        while (left < right) {
+            int mid = (left + right) >> 1;
+            if (check(A, B, mid)) {
+                left = mid + 1;
+            } else {
+                right = mid;
+            }
+        }
+        return left - 1;
     }
 };
