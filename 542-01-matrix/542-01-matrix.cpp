5846909404defbd840479c5b594a75ed9cc19ffb
Zheng Yong <angzhengyong@gmail.com>
2022-09-20 10:28:04 +0800

    Time: 126 ms (58.42%), Space: 24.7 MB (99.92%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     vector<vector<int>> updateMatrix(vector<vector<int>>& mat) {
         int m = mat.size(), n = mat[0].size();
                 
