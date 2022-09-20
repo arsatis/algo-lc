7cd8d242eec3cf8c05770c43f8848b0ef9847e35
Zheng Yong <angzhengyong@gmail.com>
2022-09-20 10:37:42 +0800

    Time: 135 ms (83.97%), Space: 59.7 MB (66.24%) - LeetHub

--- 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
+++ 852-peak-index-in-a-mountain-array/852-peak-index-in-a-mountain-array.cpp
@@ -8,18 +8,11 @@ public:
     
     int peakIndexInMountainArray(vector<int>& arr) {
         int s = 0, mid, e=arr.size();
-        while(s<e){
-            mid=s+(e-s)/2;
-            if(arr[mid]>=arr[mid+1]){
-                e=mid;
-                
-            }
-            else{
-                s=mid+1;
-            }
-            
+        while (s < e) {
+            mid = (e + s) / 2;
+            if (arr[mid] >= arr[mid + 1]) e = mid;
+            else s = mid + 1;
         }
-        
         return s;
     }
 };
