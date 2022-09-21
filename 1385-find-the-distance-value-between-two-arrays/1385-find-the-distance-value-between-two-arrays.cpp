05103e6addbde3510cabb5f3b633499118139599
Zheng Yong <angzhengyong@gmail.com>
2022-09-21 10:01:48 +0800

    Time: 25 ms (34.13%), Space: 13.1 MB (69.45%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -9,10 +9,6 @@ class Solution {
         return 1;
     }
 public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
-    }
-    
     int findTheDistanceValue(vector<int>& a, vector<int>& b, int d) {
         sort(b.begin(), b.end());
         int dist = 0;
