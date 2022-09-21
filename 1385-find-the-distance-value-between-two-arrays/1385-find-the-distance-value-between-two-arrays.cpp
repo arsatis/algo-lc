cc89c36fa624b9e425362adebd72741d2dfc2e0b
Zheng Yong <angzhengyong@gmail.com>
2022-09-21 10:04:00 +0800

    Time: 14 ms (78.58%), Space: 13.2 MB (26.84%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,7 +1,8 @@
 class Solution {
     bool ok(vector<int>& a, int target, int d) {
-        for (int l = 0, r = a.size() - 1; l <= r;) {
-            int m = (l + r) >> 1;
+        int l = 0, r = a.size() - 1, m;
+        while (l <= r) {
+            m = (l + r) >> 1;
             if (abs(a[m] - target) <= d) return 0;
             else if (a[m] < target) l = m + 1;
             else r = m - 1;
