b289b771d742b085946e00b3979cd0a30fa1e090
Zheng Yong <angzhengyong@gmail.com>
2022-09-21 09:59:56 +0800

    Time: 20 ms (51.62%), Space: 13.3 MB (26.84%) - LeetHub

--- 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
+++ 1385-find-the-distance-value-between-two-arrays/1385-find-the-distance-value-between-two-arrays.cpp
@@ -1,17 +1,16 @@
 class Solution {
-public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
-    }
     bool ok(vector<int> &a, int target, int d) {
-        int lo=0, hi=a.size()-1, mid, ans;
-        while(lo<=hi) {
-            mid=(lo+hi)>>1;
+        for (int lo=0, hi=a.size()-1; lo <= hi; ) {
+            int mid=(lo+hi)>>1;
             if(abs(a[mid]-target) <= d) return 0;
             else if(a[mid]<target) lo=mid+1;
             else hi=mid-1;
         }
         return 1;
+    }
+public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
     }
     int findTheDistanceValue(vector<int>& a, vector<int>& b, int d) {
         sort(b.begin(), b.end());
