dc9b52366367c12c35a6387f1bbcb9eed4c54b77
Zheng Yong <angzhengyong@gmail.com>
2022-09-21 11:01:53 +0800

    Time: 60 ms (82.59%), Space: 21.4 MB (52.01%) - LeetHub

--- 934-shortest-bridge/934-shortest-bridge.cpp
+++ 934-shortest-bridge/934-shortest-bridge.cpp
@@ -12,6 +12,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int shortestBridge(vector<vector<int>>& grid) {
