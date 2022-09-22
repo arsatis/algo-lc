bb3daac7add411c63ce37bb5124bdf09035538bd
Zheng Yong <angzhengyong@gmail.com>
2022-09-22 11:40:07 +0800

    Time: 18 ms (42.38%), Space: 10.5 MB (58.48%) - LeetHub

---
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    bool canVisitAllRooms(vector<vector<int>>& rooms) {
+        vector<int> visited(rooms.size());
+        queue<int> q;
+        
+        ++visited[0];
+        q.emplace(0);
+        while (!q.empty()) {
+            int next = q.front();
+            q.pop();
+            for (int room : rooms[next])
+                if (!visited[room]) {
+                    q.emplace(room); 
+                    ++visited[room];
+                }
+        }
+        return accumulate(visited.begin(), visited.end(), 0) == rooms.size();
+    }
+};
