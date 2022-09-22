f08602ae7fec6f47209a4d8927372435d46b2d5c
Zheng Yong <angzhengyong@gmail.com>
2022-09-22 09:43:43 +0800

    Time: 25 ms (48.77%), Space: 9.8 MB (40.62%) - LeetHub

--- 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
+++ 557-reverse-words-in-a-string-iii/557-reverse-words-in-a-string-iii.cpp
@@ -1,10 +1,12 @@
 class Solution {
 public:
-    string reverseWords(string s) {
+    Solution() {
         ios_base::sync_with_stdio(0);
         cin.tie(0);
         cout.tie(0);
-
+    }
+    
+    string reverseWords(string s) {
         auto prev = s.begin(), curr = s.begin();
         while ((curr = find(curr, s.end(), ' ')) != s.end()) {
             reverse(prev, curr);
