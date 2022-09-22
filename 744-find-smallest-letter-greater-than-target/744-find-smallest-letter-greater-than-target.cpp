8aac9f521e7fbecfccadd5f733b2f86604eda6e5
Zheng Yong <angzhengyong@gmail.com>
2022-09-22 10:20:52 +0800

    Time: 27 ms (50.83%), Space: 15.8 MB (70.26%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -7,16 +7,7 @@ public:
     }
     
     char nextGreatestLetter(vector<char>& letters, char target) {
-        int l = 0, r = letters.size();
-        char ans = letters[0];
-        while (l <= r) {
-            int i = (l + r) >> 1;
-            if (letters[i] > target){
-                r = i - 1;
-                ans = letters[i];
-            }
-            else l = i + 1;
-        }
-        return ans;
+        auto it = upper_bound(letters.begin(), letters.end(), target);
+        return it == letters.end() ? letters.front() : *it;
     }
 };
