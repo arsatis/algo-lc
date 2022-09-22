10dd3a14edfd658eb96d53b10e61d35328751a47
Zheng Yong <angzhengyong@gmail.com>
2022-09-22 13:55:24 +0800

    Time: 9 ms (11.55%), Space: 7.2 MB (85.96%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.cpp
+++ 413-arithmetic-slices/413-arithmetic-slices.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     Solution() {
         ios::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
     }
     
     int numberOfArithmeticSlices(vector<int>& nums) {
