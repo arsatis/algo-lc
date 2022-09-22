fe99998cdb398deb463b15ebe245d174730423a7
Zheng Yong <angzhengyong@gmail.com>
2022-09-22 10:00:22 +0800

    Time: 0 ms (100.00%), Space: 6.5 MB (31.33%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -7,6 +7,7 @@ public:
     vector<vector<int>> generate(int numRows) {
         vector<vector<int>> output(numRows, vector<int>(1, 1));
         for (int i = 1; i < numRows; ++i) {
+            output[i].reserve(i);
             for (int j = 1; j < i; ++j)
                 output[i].emplace_back(output[i - 1][j - 1] + output[i - 1][j]);
             output[i].emplace_back(1);
