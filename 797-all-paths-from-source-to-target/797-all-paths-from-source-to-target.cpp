4b5b3cdf325a69659042d1cb9dc99e323024dfff
Zheng Yong <angzhengyong@gmail.com>
2022-09-22 10:38:38 +0800

    Time: 23 ms (60.15%), Space: 10.8 MB (67.39%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -12,8 +12,6 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
     }
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
