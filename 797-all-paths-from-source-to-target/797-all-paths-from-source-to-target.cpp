0dc657f9824ceb4b17bb16a7744e4a2a5ba323e9
Zheng Yong <angzhengyong@gmail.com>
2022-09-22 10:41:36 +0800

    Time: 7 ms (99.06%), Space: 10.7 MB (82.40%) - LeetHub

--- 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
+++ 797-all-paths-from-source-to-target/797-all-paths-from-source-to-target.cpp
@@ -1,11 +1,10 @@
 class Solution {
-    int n;
     vector<int> path;
     vector<vector<int>> paths;
     void dfs(vector<vector<int>>& graph) {
         for (int i : graph[path.back()]) {
             path.emplace_back(i);
-            if (i == n) paths.emplace_back(path);
+            if (i == graph.size() - 1) paths.emplace_back(path);
             else dfs(graph);
             path.pop_back();
         }
@@ -16,7 +15,6 @@ public:
     }
     
     vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
-        n = graph.size() - 1;
         path.reserve(graph.size());
         path.emplace_back(0);
         dfs(graph);
