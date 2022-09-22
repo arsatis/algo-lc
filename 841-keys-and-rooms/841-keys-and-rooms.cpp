ea516598e1579e7e6b0c559a32f3e45927191baa
Zheng Yong <angzhengyong@gmail.com>
2022-09-22 11:44:54 +0800

    Time: 25 ms (16.65%), Space: 10.4 MB (80.47%) - LeetHub

--- 841-keys-and-rooms/841-keys-and-rooms.cpp
+++ 841-keys-and-rooms/841-keys-and-rooms.cpp
@@ -7,6 +7,10 @@ class Solution {
             }
     }
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     bool canVisitAllRooms(vector<vector<int>>& rooms) {
         vector<bool> visited (rooms.size(), false);
         
