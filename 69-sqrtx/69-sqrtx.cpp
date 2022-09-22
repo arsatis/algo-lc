e5f52cc389e64f23f4945702e92cf659995af664
Zheng Yong <angzhengyong@gmail.com>
2022-09-22 09:55:07 +0800

    Time: 5 ms (44.67%), Space: 6.1 MB (25.43%) - LeetHub

--- 69-sqrtx/69-sqrtx.cpp
+++ 69-sqrtx/69-sqrtx.cpp
@@ -7,9 +7,9 @@ public:
     }
     
     int mySqrt(int x) {
-        int low = 1, high = x, mid, ans;
-        while (low <= high) {
-            mid = low + ((high - low) >> 1);
+        int ans;
+        for (int low = 1, high = x; low <= high;) {
+            int mid = low + ((high - low) >> 1);
             if ((long long) mid * mid <= x) {
                 ans = mid;
                 low = mid + 1;
