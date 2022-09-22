8e404ef77420db515c155749e7acd2d18092b228
Zheng Yong <angzhengyong@gmail.com>
2022-09-22 10:18:59 +0800

    Time: 27 ms (50.83%), Space: 15.9 MB (24.49%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     char nextGreatestLetter(vector<char>& letters, char target) {
         auto it = upper_bound(letters.begin(), letters.end(), target);
         return it == letters.end() ? letters.front() : *it;
