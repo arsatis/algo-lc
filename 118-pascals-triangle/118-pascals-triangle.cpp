9ac3de2b2fd234bb3e1d726a7b690198603ae749
Zheng Yong <angzhengyong@gmail.com>
2022-09-22 09:59:06 +0800

    Time: 8 ms (5.87%), Space: 6.5 MB (31.33%) - LeetHub

--- 118-pascals-triangle/118-pascals-triangle.cpp
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -4,9 +4,9 @@ public:
         vector<vector<int>> output(numRows, vector<int>(1, 1));
         for (int i = 1; i < numRows; ++i) {
             for (int j = 1; j < i; ++j)
-                output[i].push_back(output[i - 1][j - 1] + output[i - 1][j]);
-            output[i].push_back(1);
+                output[i].emplace_back(output[i - 1][j - 1] + output[i - 1][j]);
+            output[i].emplace_back(1);
         }
-        return output;
+        return move(output);
     }
 };
