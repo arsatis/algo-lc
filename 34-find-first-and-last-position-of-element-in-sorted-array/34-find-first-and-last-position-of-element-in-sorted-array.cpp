cf701dcf268abcbeb24721504c5a283c43478c4d
Zheng Yong <angzhengyong@gmail.com>
2022-09-23 16:41:11 +0800

    Time: 18 ms (27.77%), Space: 13.8 MB (6.97%) - LeetHub

--- 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     vector<int> searchRange(vector<int>& nums, int target) {
         auto lo = lower_bound(nums.begin(), nums.end(), target),
              hi = upper_bound(nums.begin(), nums.end(), target);
