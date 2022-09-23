e92427074f07378d7d09ccea93bc7169fe0d8e65
Zheng Yong <angzhengyong@gmail.com>
2022-09-23 09:43:05 +0800

    Time: 48 ms (80.98%), Space: 6.1 MB (22.83%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     int concatenatedBinary(int n) {
         long ans = 0, mod = 1e9 + 7, len = 0;
         for (int i = 1; i <= n; ++i) {
