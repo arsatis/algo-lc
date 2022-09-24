f89168108476eca3e809c6304706130aac19e2a6
Zheng Yong <angzhengyong@gmail.com>
2022-09-24 14:12:29 +0800

    Time: 22 ms (49.15%), Space: 20 MB (60.91%) - LeetHub

--- 113-path-sum-ii/113-path-sum-ii.cpp
+++ 113-path-sum-ii/113-path-sum-ii.cpp
@@ -24,8 +24,6 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
     }
     
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
