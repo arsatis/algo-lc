a22f10cfd3a86f4467c3c4152889959caa412083
Zheng Yong <angzhengyong@gmail.com>
2022-09-24 21:06:20 +0800

    Time: 4 ms (86.27%), Space: 9.8 MB (17.60%) - LeetHub

--- 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int findKthPositive(vector<int>& arr, int k) {
