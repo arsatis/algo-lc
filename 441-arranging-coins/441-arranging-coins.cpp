fd0170f83fd5c4628910c4b4b05d787b3aa30910
Zheng Yong <angzhengyong@gmail.com>
2022-09-24 14:18:50 +0800

    Time: 0 ms (100.00%), Space: 6 MB (24.53%) - LeetHub

--- 441-arranging-coins/441-arranging-coins.cpp
+++ 441-arranging-coins/441-arranging-coins.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     int arrangeCoins(int n) {
         return sqrt(((long) n << 1) + 0.25) - 0.5;
     }
