d4e4370962c178d7d2654530928142c3de86b855
Zheng Yong <angzhengyong@gmail.com>
2022-09-25 14:43:30 +0800

    Time: 1097 ms (15.70%), Space: 209.9 MB (5.01%) - LeetHub

---
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int minReorder(int n, vector<vector<int>>& connections) {
+        vector<unordered_set<int>> dirEdges(n), undirEdges(n);
+        for (auto& v : connections) {
+            dirEdges[v[0]].insert(v[1]);
+            undirEdges[v[0]].insert(v[1]);
+            undirEdges[v[1]].insert(v[0]);
+        }
+        
+        int num = -1;
+        queue<pair<int, int>> q;
+        q.emplace(0, 0);
+        while (!q.empty()) {
+            auto [prev, curr] = q.front();
+            q.pop();
+            if (dirEdges[curr].find(prev) == dirEdges[curr].end()) ++num;
+            for (int next : undirEdges[curr]) if (next != prev) q.emplace(curr, next);
+        }
+        return num;
+    }
+};
