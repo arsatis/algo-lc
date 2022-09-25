7ecff04f14c013b165323ff0dd1bcd8c8c975431
Zheng Yong <angzhengyong@gmail.com>
2022-09-25 11:43:58 +0800

    Time: 690 ms (31.52%), Space: 127 MB (38.98%) - LeetHub

---
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
+        int maxTime = 0;
+        vector<vector<int>> subord(n);
+        queue<pair<int, int>> q;
+        q.emplace(headID, 0);
+        
+        for (int i = 0; i < n; ++i)
+            if (i != headID) subord[manager[i]].emplace_back(i);
+        while (!q.empty()) {
+            auto [id, time] = q.front();
+            q.pop();
+            if (time > maxTime) maxTime = time;
+            for (int subID : subord[id]) q.emplace(subID, time + informTime[id]);
+        }
+        return maxTime;
+    }
+};
