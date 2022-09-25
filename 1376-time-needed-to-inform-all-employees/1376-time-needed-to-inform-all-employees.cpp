471eb8ec9f97390f7844e69932107f507f53e515
Zheng Yong <angzhengyong@gmail.com>
2022-09-25 11:44:44 +0800

    Time: 560 ms (47.52%), Space: 126.7 MB (42.75%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
     }
     
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
