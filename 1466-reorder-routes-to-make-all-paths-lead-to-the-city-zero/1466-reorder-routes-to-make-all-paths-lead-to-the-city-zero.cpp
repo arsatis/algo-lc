135450188a6b09d31e122f75bbbf285d6249a922
Zheng Yong <angzhengyong@gmail.com>
2022-09-25 14:43:57 +0800

    Time: 1232 ms (10.24%), Space: 209.9 MB (5.01%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int minReorder(int n, vector<vector<int>>& connections) {
