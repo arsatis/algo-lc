388540021434e1ffa4340f4f2f58d50548327bd7
Zheng Yong <angzhengyong@gmail.com>
2022-09-25 11:30:50 +0800

    Time: 257 ms (40.10%), Space: 51.8 MB (45.36%) - LeetHub

--- 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
+++ 1319-number-of-operations-to-make-network-connected/1319-number-of-operations-to-make-network-connected.cpp
@@ -8,6 +8,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int makeConnected(int n, vector<vector<int>>& connections) {
