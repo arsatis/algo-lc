7434c174364e3f4e74aa231637bfe0b50da8b6fe
Zheng Yong <angzhengyong@gmail.com>
2022-09-25 16:38:17 +0800

    Time: 15 ms (97.93%), Space: 9.3 MB (84.60%) - LeetHub

---
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.cpp
@@ -0,0 +1,35 @@
+class Solution {
+    bool seen[1 << 12][12];
+    class Tuple {
+    public:
+        int node, path, cost;
+        Tuple(int node, int path, int cost) : node(node), path(path), cost(cost) {}
+    };
+public:
+    int shortestPathLength(vector<vector<int>>& g) {
+        int n = g.size();
+        queue<Tuple> q;
+        for (int i = 0; i < n; ++i) {
+            int temp = 1 << i;
+            Tuple t(i, temp, 1);
+            q.emplace(t);
+            seen[1 << i][i] = true;
+        }
+        
+        while (!q.empty()) {
+            Tuple cur = q.front();
+            q.pop();
+            
+            if (cur.path == (1 << n) - 1) return cur.cost - 1;
+            for (int x : g[cur.node]){
+                int nextpath = cur.path | (1 << x);
+                Tuple t(x ,nextpath, cur.cost + 1);
+                if (!seen[nextpath][x]) {
+                    q.emplace(t);
+                    seen[nextpath][x] = true;
+                }
+            }
+        }
+        return -1;
+    }
+};
