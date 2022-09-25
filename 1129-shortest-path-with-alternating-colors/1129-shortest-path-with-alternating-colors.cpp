b65ad995062cf0cbe8f25998b9998d9e6d9ce4c4
Zheng Yong <angzhengyong@gmail.com>
2022-09-25 14:25:57 +0800

    Time: 42 ms (33.33%), Space: 15 MB (55.53%) - LeetHub

--- 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
+++ 1129-shortest-path-with-alternating-colors/1129-shortest-path-with-alternating-colors.cpp
@@ -6,19 +6,19 @@ public:
     
     vector<int> shortestAlternatingPaths(int n, vector<vector<int>>& redEdges, vector<vector<int>>& blueEdges) {
         vector<vector<pair<int, int>>> graph(n);
-        for(auto& v : redEdges) graph[v[0]].push_back({v[1], 0});
-        for(auto& v : blueEdges) graph[v[0]].push_back({v[1], 1});
+        for(auto& v : redEdges) graph[v[0]].emplace_back(v[1], 0);
+        for(auto& v : blueEdges) graph[v[0]].emplace_back(v[1], 1);
         vector<vector<int>> vis(n, vector<int>(2, 10001));
         queue<pair<int, int>> q;
-        q.push({0, 0});
-        q.push({0, 1});
+        q.emplace(0, 0);
+        q.emplace(0, 1);
         vis[0][0] = 0, vis[0][1] = 0;
         while(!q.empty()){
             auto [i, c1] = q.front(); q.pop();
             for(auto [j, c2] : graph[i]){
                 if(vis[j][c2] != 10001 || c1 == c2) continue;
                 vis[j][c2] = vis[i][c1] + 1;
-                q.push({j, c2});
+                q.emplace(j, c2);
             }
         }
         vector<int> res;
