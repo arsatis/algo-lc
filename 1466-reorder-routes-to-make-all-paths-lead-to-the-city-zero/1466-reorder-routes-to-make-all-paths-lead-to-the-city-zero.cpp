75a2869efb12097d600a60622e9fa65e870b9d04
Zheng Yong <angzhengyong@gmail.com>
2022-09-25 14:44:29 +0800

    Time: 331 ms (99.86%), Space: 95.3 MB (98.83%) - LeetHub

--- 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
+++ 1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero/1466-reorder-routes-to-make-all-paths-lead-to-the-city-zero.cpp
@@ -6,23 +6,28 @@ public:
         cout.tie(0);
     }
     
-    int minReorder(int n, vector<vector<int>>& connections) {
-        vector<unordered_set<int>> dirEdges(n), undirEdges(n);
-        for (auto& v : connections) {
-            dirEdges[v[0]].insert(v[1]);
-            undirEdges[v[0]].insert(v[1]);
-            undirEdges[v[1]].insert(v[0]);
+    int minReorder(int n, vector<vector<int>>& conn) {
+        vector<pair<int, bool>> adj[n];
+        vector<bool> vis(n);
+        for(auto& it: conn) {
+            adj[it[0]].push_back({it[1], true});
+            adj[it[1]].push_back({it[0], false});
         }
-        
-        int num = -1;
-        queue<pair<int, int>> q;
-        q.emplace(0, 0);
-        while (!q.empty()) {
-            auto [prev, curr] = q.front();
-            q.pop();
-            if (dirEdges[curr].find(prev) == dirEdges[curr].end()) ++num;
-            for (int next : undirEdges[curr]) if (next != prev) q.emplace(curr, next);
+        int ans = 0;
+        queue<int> q;
+        q.push(0);
+        while(!q.empty()) {
+            int m = q.size();
+            for(int i = 0; i < m; i++) {
+                int root = q.front(); q.pop();
+                vis[root] = true;
+                for(auto& it: adj[root]) {
+                    if(vis[it.first]) continue;
+                    if(it.second) ans++;
+                    q.push(it.first);
+                }
+            }
         }
-        return num;
+        return ans;
     }
 };
