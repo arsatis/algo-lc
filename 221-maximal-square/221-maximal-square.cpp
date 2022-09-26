419ed368ceb8cad49b88b2c68c24cc0c93680449
Zheng Yong <angzhengyong@gmail.com>
2022-09-26 09:54:19 +0800

    Time: 146 ms (38.37%), Space: 19 MB (36.32%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int maximalSquare(vector<vector<char>>& matrix) {
@@ -27,7 +29,6 @@ public:
                 }
             }
         }
-        
         return maxSide * maxSide;
     }
 };
