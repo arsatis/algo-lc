d457da5295acceb33051fdcd87b3c66c86cf9337
Zheng Yong <angzhengyong@gmail.com>
2022-09-26 10:17:34 +0800

    Time: 2 ms (67.17%), Space: 6.5 MB (51.27%) - LeetHub

--- 58-length-of-last-word/58-length-of-last-word.cpp
+++ 58-length-of-last-word/58-length-of-last-word.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     int lengthOfLastWord(string s) {
         int len = 0, tail = s.size() - 1;
         while (tail >= 0 && s[tail] == ' ') --tail;
