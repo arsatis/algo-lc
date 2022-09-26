12df09ae0a946e72193a8d6d422a9de8773d3379
Zheng Yong <angzhengyong@gmail.com>
2022-09-26 09:17:06 +0800

    Time: 7 ms (79.10%), Space: 11.3 MB (88.77%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -8,11 +8,9 @@ public:
     bool equationsPossible(vector<string>& equations) {
         for (int i = 0; i < 26; ++i) uf[i] = i;
         for (string& e : equations)
-            if (e[1] == '=')
-                uf[find(e[0] - 'a')] = find(e[3] - 'a');
+            if (e[1] == '=') uf[find(e[0] - 'a')] = find(e[3] - 'a');
         for (string& e : equations)
-            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a'))
-                return false;
+            if (e[1] == '!' && find(e[0] - 'a') == find(e[3] - 'a')) return false;
         return true;
     }
 };
