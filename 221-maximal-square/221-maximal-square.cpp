94b8ff52fc49d581e774f87287e437b0b20b06a6
Zheng Yong <angzhengyong@gmail.com>
2022-09-26 10:00:17 +0800

    Time: 94 ms (73.60%), Space: 20.9 MB (31.68%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -6,24 +6,17 @@ public:
         cout.tie(0);
     }
     
-    int maximalSquare(vector<vector<char>>& mat) {
-        int n = mat.size(), m = mat[0].size(), ans = 0;
-        int matrix[n][m];
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < m; ++j) {
-                if (mat[i][j] == '0') matrix[i][j] = 0;
-                else {
-                    ans = 1;
-                    matrix[i][j] = 1;
+    int maximalSquare(vector<vector<char>>& matrix) {
+        int m = matrix.size(), n = matrix[0].size(), ans = 0;
+        vector<vector<int>>dp(m + 1, vector<int>(n + 1));
+        for (int i = 1; i <= m; ++i) {
+            for (int j = 1; j <= n; ++j) {
+                if (matrix[i - 1][j - 1] == '1') {
+                    dp[i][j] = min(dp[i - 1][j], min(dp[i][j - 1],dp[i - 1][j - 1])) + 1;
+                    ans = max(ans, dp[i][j]);
                 }
             }
-        for (int i = 1; i < n; ++i)
-            for (int j = 1; j < m; ++j) {
-                if (matrix[i][j] == 0) continue;
-                matrix[i][j] += min(matrix[i - 1][j - 1],
-                                min(matrix[i][j - 1], matrix[i - 1][j]));
-                ans = max(ans, matrix[i][j] * matrix[i][j]);
-            }
-        return ans;
+        }
+        return ans * ans;
     }
 };
