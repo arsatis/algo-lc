ce4baca5d5ee656834daafbfe4b64c63442183b7
Zheng Yong <angzhengyong@gmail.com>
2022-09-26 09:16:31 +0800

    Time: 9 ms (61.19%), Space: 11.2 MB (98.15%) - LeetHub

--- 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
+++ 990-satisfiability-of-equality-equations/990-satisfiability-of-equality-equations.cpp
@@ -7,6 +7,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     bool equationsPossible(vector<string>& equations) {
