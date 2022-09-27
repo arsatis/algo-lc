8af8893c34eb2df06c01e9397fd275ffaac7e5fb
Zheng Yong <angzhengyong@gmail.com>
2022-09-27 10:20:19 +0800

    Time: 33 ms (12.52%), Space: 10.5 MB (43.67%) - LeetHub

---
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
+        int m = mat.size(), n = mat[0].size();
+        vector<int> numSoldiers(m);
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                numSoldiers[i] += mat[i][j];
+        
+        vector<vector<int>> srMap(n + 1);
+        for (int i = 0; i < m; ++i) srMap[numSoldiers[i]].emplace_back(i);
+        
+        int idx = 0, srOuterIdx = 0, srInnerIdx = 0;
+        vector<int> weakest(k);
+        while (idx < k) {
+            if (srInnerIdx == srMap[srOuterIdx].size()) srInnerIdx = 0, ++srOuterIdx;
+            while (srMap[srOuterIdx].empty()) ++srOuterIdx;
+            weakest[idx++] = srMap[srOuterIdx][srInnerIdx++];
+        }
+        return move(weakest);
+    }
+};
