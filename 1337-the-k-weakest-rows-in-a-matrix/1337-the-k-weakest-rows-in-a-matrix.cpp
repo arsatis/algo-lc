ce5576fdfd3a118c3eae9646b69e754c1e68cbe9
Zheng Yong <angzhengyong@gmail.com>
2022-09-27 10:30:34 +0800

    Time: 11 ms (95.66%), Space: 10.7 MB (22.76%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -12,6 +12,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
