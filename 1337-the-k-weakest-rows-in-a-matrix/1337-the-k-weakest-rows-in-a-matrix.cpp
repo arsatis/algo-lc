bf7537cfdb11988e0fbdcc719a23a56c50ab8b23
Zheng Yong <angzhengyong@gmail.com>
2022-09-27 10:26:10 +0800

    Time: 20 ms (57.77%), Space: 10.6 MB (22.76%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,36 +1,32 @@
 class Solution {
     int calculateSoldierCount(vector<int>& v) {
-        int l=0; int h=v.size()-1;
-        while(l<=h) {
-            int mid=l+(h-l)/2;
-            if(v[mid]==0) {
-                h=mid-1;
-            } else {
-                l=mid+1;
-            }
+        int l = 0, h = v.size() - 1;
+        while (l <= h) {
+            int mid = (h + l) >> 1;
+            if (v[mid] == 0) h = mid - 1;
+            else l = mid + 1;
         }
         return l;
     }
     
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        
-        priority_queue<pair<int,int>, vector<pair<int,int>>> max_heap;
-        
-        for(int i=0;i<mat.size();++i){
-            max_heap.push({calculateSoldierCount(mat[i]),i});
-            if(max_heap.size()>k){
-                max_heap.pop();
-            }
+        priority_queue<pair<int, int>, vector<pair<int, int>>> max_heap;
+        for (int i = 0; i < mat.size(); ++i) {
+            max_heap.push({ calculateSoldierCount(mat[i]), i });
+            if (max_heap.size() > k) max_heap.pop();
         }
         
         vector<int>ans;
-        while(max_heap.size()){
-            ans.push_back(max_heap.top().second);
+        while (max_heap.size()) {
+            ans.emplace_back(max_heap.top().second);
             max_heap.pop();
         }
-		
         reverse(ans.begin(), ans.end());
-        return ans;
+        return move(ans);
     }
 };
