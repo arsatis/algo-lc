8981023913faf7338fc071672f5a70be219de24c
Zheng Yong <angzhengyong@gmail.com>
2022-09-27 10:21:30 +0800

    Time: 22 ms (49.92%), Space: 10.8 MB (12.54%) - LeetHub

--- 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
+++ 1337-the-k-weakest-rows-in-a-matrix/1337-the-k-weakest-rows-in-a-matrix.cpp
@@ -1,29 +1,29 @@
 class Solution {
+    vector<int> helper(vector<vector<int>>& mat,int k){
+        set<pair<int,int>>st;
+        int n = mat.size();
+        for(int i=0;i<n;i++){
+            int cnt = count(mat[i].begin(),mat[i].end(),1);
+            st.insert({cnt,i});
+        }
+        
+        vector<int>ans;
+        for(auto i:st){
+            if(k==0){
+                break;
+            }
+            ans.push_back(i.second);
+            k--;
+        }
+        
+        return ans;
+    }
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
     }
     
     vector<int> kWeakestRows(vector<vector<int>>& mat, int k) {
-        vector<int> score;
-        vector<int> ans;
-        for(auto i:mat){
-            int s=0;
-            for(auto j:i){
-                if(j) ++s;
-            }
-            score.push_back(s);
-        }
-        vector<bool> used(score.size(),0);
-        for(int i=0;i<k;++i){
-            int min=1000;
-            int min_index=0;
-            for(int j=0;j<score.size();++j){
-                if(score[j]<min and (!used[j])) {min=score[j]; min_index=j;}
-            }
-            used[min_index]=1;
-            ans.push_back(min_index);
-        }
-        return ans;
+        return helper(mat,k);
     }
 };
