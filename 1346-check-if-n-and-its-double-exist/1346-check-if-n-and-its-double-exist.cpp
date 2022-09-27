a89df907723c79a5bf57ffd1f8759022fe1fa4bf
Zheng Yong <angzhengyong@gmail.com>
2022-09-27 10:46:53 +0800

    Time: 10 ms (68.31%), Space: 10.5 MB (24.01%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -2,13 +2,16 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     bool checkIfExist(vector<int>& arr) {
-        unordered_set<int> set;
+        unordered_set<int> seen;
         for (int i = 0; i < arr.size(); ++i) {
-            if (set.count(2 * arr[i]) > 0 || ((arr[i] & 1) == 0 && set.count(arr[i] / 2) > 0)) return true;
-            set.insert(arr[i]);
+            if (seen.find(2 * arr[i]) != seen.end() ||
+                ((arr[i] & 1) == 0 && seen.find(arr[i] / 2) != seen.end())) return true;
+            seen.insert(arr[i]);
         }
         return false;
     }
