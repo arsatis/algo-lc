1b5fdd93b43e5c749e38b0698d41bf104a1936ca
Zheng Yong <angzhengyong@gmail.com>
2022-09-27 10:51:00 +0800

    Time: 8 ms (79.25%), Space: 10.6 MB (24.01%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.cpp
@@ -8,10 +8,9 @@ public:
     
     bool checkIfExist(vector<int>& arr) {
         unordered_set<int> seen;
-        for (int i = 0; i < arr.size(); ++i) {
-            if (seen.find(2 * arr[i]) != seen.end() ||
-                ((arr[i] & 1) == 0 && seen.find(arr[i] / 2) != seen.end())) return true;
-            seen.insert(arr[i]);
+        for (int i : arr) {
+            if (seen.find(i * 2) != seen.end() || (i & 1) == 0 && seen.find(i * 0.5) != seen.end()) return true;
+            seen.insert(i);
         }
         return false;
     }
