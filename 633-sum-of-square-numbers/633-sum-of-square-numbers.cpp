082670b2ceafc744ef06ebd58ff96768c006dd11
Zheng Yong <angzhengyong@gmail.com>
2022-09-28 10:17:19 +0800

    Time: 7 ms (37.90%), Space: 6.1 MB (8.78%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -7,13 +7,22 @@ public:
     }
     
     bool judgeSquareSum(int c) {
-        int a = 0, b = sqrt(c) + 1;
-        while (a <= b) {
-            long sumOfSquares = (long) a * a + (long) b * b;
-            if (sumOfSquares == c) return true;
-            else if (sumOfSquares < c) ++a;
-            else --b;
+        for (long a = 0, sq; (sq = a * a) <= c; ++a) {
+            double b = sqrt(c - sq);
+            if (b == (int) b) return true;
         }
         return false;
     }
 };
+
+// Solution 1: two pointers, O(c)
+// bool judgeSquareSum(int c) {
+//     int a = 0, b = sqrt(c) + 1;
+//     while (a <= b) {
+//         long sumOfSquares = (long) a * a + (long) b * b;
+//         if (sumOfSquares == c) return true;
+//         else if (sumOfSquares < c) ++a;
+//         else --b;
+//     }
+//     return false;
+// }
