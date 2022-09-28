0f69111808e7924291506e2f1ced308c892a781f
Zheng Yong <angzhengyong@gmail.com>
2022-09-28 09:48:45 +0800

    Time: 239 ms (35.48%), Space: 36.2 MB (97.11%) - LeetHub

---
+++ 1272-remove-interval/1272-remove-interval.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<vector<int>> removeInterval(vector<vector<int>>& intervals, vector<int>& toBeRemoved) {
+        vector<vector<int>> output;
+        output.reserve(intervals.size());
+        for (auto& v : intervals) {
+            if (v[0] < toBeRemoved[0]) output.emplace_back(vector<int>{v[0], min(v[1], toBeRemoved[0])});
+            if (v[1] > toBeRemoved[1]) output.emplace_back(vector<int>{max(v[0], toBeRemoved[1]), v[1]});
+        }
+        return move(output);
+    }
+};
