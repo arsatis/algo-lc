18436fac48adef1bcabb10ae85979fb81eb5f9a3
Zheng Yong <angzhengyong@gmail.com>
2022-09-28 10:18:34 +0800

    Time: 2 ms (78.86%), Space: 6 MB (9.14%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -5,9 +5,12 @@ public:
     }
     
     bool judgeSquareSum(int c) {
-        for (long a = 0, sq; (sq = a * a) <= c; ++a) {
-            double b = sqrt(c - sq);
-            if (b == (int) b) return true;
+        int a = 0, b = sqrt(c) + 1;
+        while (a <= b) {
+            long sumOfSquares = (long) a * a + (long) b * b;
+            if (sumOfSquares == c) return true;
+            else if (sumOfSquares < c) ++a;
+            else --b;
         }
         return false;
     }
