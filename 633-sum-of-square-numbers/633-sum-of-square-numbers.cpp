7dc680d88994fa09034fefb77097daba54766d56
Zheng Yong <angzhengyong@gmail.com>
2022-09-28 10:18:01 +0800

    Time: 10 ms (25.90%), Space: 5.9 MB (71.28%) - LeetHub

--- 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
+++ 633-sum-of-square-numbers/633-sum-of-square-numbers.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     bool judgeSquareSum(int c) {
         for (long a = 0, sq; (sq = a * a) <= c; ++a) {
             double b = sqrt(c - sq);
@@ -17,6 +21,15 @@ public:
 //         if (sumOfSquares == c) return true;
 //         else if (sumOfSquares < c) ++a;
 //         else --b;
+//     }
+//     return false;
+// }
+
+// Solution 2: sqrt, O(sqrt(c)log c)
+// bool judgeSquareSum(int c) {
+//     for (long a = 0, sq; (sq = a * a) <= c; ++a) {
+//         double b = sqrt(c - sq);
+//         if (b == (int) b) return true;
 //     }
 //     return false;
 // }
