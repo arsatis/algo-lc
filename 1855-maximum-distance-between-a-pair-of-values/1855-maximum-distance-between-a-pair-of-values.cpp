0fd7d99765de4b496df631506cebec26e8ea2318
Zheng Yong <angzhengyong@gmail.com>
2022-09-29 16:41:17 +0800

    Time: 128 ms (100.00%), Space: 98.6 MB (28.50%) - LeetHub

--- 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
+++ 1855-maximum-distance-between-a-pair-of-values/1855-maximum-distance-between-a-pair-of-values.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int maxDistance(vector<int>& nums1, vector<int>& nums2) {
