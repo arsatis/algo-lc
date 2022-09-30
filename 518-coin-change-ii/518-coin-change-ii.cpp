c6be92810efa76a5e1c98a2008d73a5509981b0a
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 16:13:01 +0800

    Time: 3 ms (99.87%), Space: 7 MB (97.52%) - LeetHub

--- 518-coin-change-ii/518-coin-change-ii.cpp
+++ 518-coin-change-ii/518-coin-change-ii.cpp
@@ -1,23 +1,8 @@
 class Solution {
 public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-    }
-    
-    int change(int amount, vector<int>& coins) {
-        if (amount == 0) return 1;
-        
-        int n = coins.size();
-        vector<vector<int>> dp(n + 2, vector<int>(amount + 2, -1));
-        for (int i = 0; i < n + 1; ++i) dp[i][0] = 1;
-        for (int i = 0; i < amount + 1; ++i) dp[0][i] = 0;
-        for (int i = 1; i < n + 1; ++i)
-            for (int j = 1; j < amount + 1; ++j) {
-                if (coins[i - 1] > j) dp[i][j] = dp[i - 1][j];
-                else dp[i][j] = dp[i - 1][j] + dp[i][j - coins[i - 1]];
-            }
-        return dp[n][amount];
+    int change(int t, vector<int>& coins) {
+        int dp[5001] = { 1 };
+        for (int c : coins) for (int j = c; j <= t; ++j) dp[j] += dp[j - c];
+        return dp[t];
     }
 };
