0101ba5c2bb077591425db1c33148d7e57f5731b
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 19:07:20 +0800

    Time: 3 ms (91.58%), Space: 12.5 MB (29.22%) - LeetHub

--- 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
+++ 1991-find-the-middle-index-in-array/1991-find-the-middle-index-in-array.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     int findMiddleIndex(vector<int>& nums) {
         int prefixSum = 0, sum = accumulate(nums.begin(), nums.end(), 0);
         for (int i = 0; i < nums.size(); ++i) {
