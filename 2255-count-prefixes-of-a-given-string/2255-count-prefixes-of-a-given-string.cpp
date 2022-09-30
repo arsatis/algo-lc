c7da3aa322ab18ed4b0c55bd9fa5c10eefb6ee7e
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 18:55:43 +0800

    Time: 3 ms (99.37%), Space: 12.1 MB (23.33%) - LeetHub

--- 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
+++ 2255-count-prefixes-of-a-given-string/2255-count-prefixes-of-a-given-string.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     int countPrefixes(vector<string>& words, string s) {
         int num = 0;
         for (string& w : words) if (s.find(w) == 0) ++num;
