ad9f624ca7a06d3f4eab17476abff83661368a71
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 21:54:23 +0800

    Time: 67 ms (87.60%), Space: 33.8 MB (49.14%) - LeetHub

--- 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
+++ 448-find-all-numbers-disappeared-in-an-array/448-find-all-numbers-disappeared-in-an-array.cpp
@@ -8,7 +8,10 @@ public:
     
     vector<int> findDisappearedNumbers(vector<int>& nums) {
         vector<int> missing;
-        for (int i : nums) if (nums[abs(i) - 1] > 0) nums[abs(i) - 1] *= -1;
+        for (int i : nums) {
+            int tmp = abs(i) - 1;
+            if (nums[tmp] > 0) nums[tmp] *= -1;
+        }
         for (int i = 0; i < nums.size(); ++i) if (nums[i] > 0) missing.emplace_back(i + 1);
         return move(missing);
     }
