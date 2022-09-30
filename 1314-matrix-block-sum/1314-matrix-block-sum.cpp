05c3ce8b6fdb8fd3a7cb7d6510825dad0e32fed1
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 15:51:20 +0800

    Time: 10 ms (89.12%), Space: 9.3 MB (88.93%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -9,34 +9,29 @@ public:
         int n = mat[0].size();
         vector<vector<int>> result(m, vector<int>(n));
         
-        for (int i = 1; i < m; i++)
-            mat[i][0] += mat[i-1][0];
-        
-        for (int i = 1; i < n; i++)
-            mat[0][i] += mat[0][i-1];
+        for (int i = 1; i < m; i++) mat[i][0] += mat[i-1][0];
+        for (int i = 1; i < n; i++) mat[0][i] += mat[0][i-1];
         
         for (int i = 1; i < m; i++)
-        for (int j = 1; j < n; j++)
-            mat[i][j] += mat[i-1][j] + mat[i][j-1] - mat[i-1][j-1];
+            for (int j = 1; j < n; j++)
+                mat[i][j] += mat[i-1][j] + mat[i][j-1] - mat[i-1][j-1];
         
         for (int i = 0; i < m; i++)
-        for (int j = 0; j < n; j++)
-        {
-            int left = max(i-k, 0);
-            int right = min(i+k, m-1);
-            int up = max(j-k, 0);
-            int down = min(j+k, n-1);
-            
-            if (left == 0 && up == 0)
-                result[i][j] = mat[right][down];
-            else if (left == 0)
-                result[i][j] = mat[right][down] - mat[right][up-1];
-            else if(up == 0)
-                result[i][j] = mat[right][down] - mat[left-1][down];
-            else
-                result[i][j] = mat[right][down] - mat[left-1][down] - mat[right][up-1] + mat[left-1][up-1];
-        }
-        
+            for (int j = 0; j < n; j++) {
+                int left = max(i-k, 0);
+                int right = min(i+k, m-1);
+                int up = max(j-k, 0);
+                int down = min(j+k, n-1);
+
+                if (left == 0 && up == 0)
+                    result[i][j] = mat[right][down];
+                else if (left == 0)
+                    result[i][j] = mat[right][down] - mat[right][up-1];
+                else if(up == 0)
+                    result[i][j] = mat[right][down] - mat[left-1][down];
+                else
+                    result[i][j] = mat[right][down] - mat[left-1][down] - mat[right][up-1] + mat[left-1][up-1];
+            }
         return result; 
     }
 };
