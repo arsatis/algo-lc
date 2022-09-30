c04bfbcf4e93db3dd190fd89efd0363306534911
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 16:01:58 +0800

    Time: 7 ms (38.25%), Space: 7.3 MB (40.70%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     int wiggleMaxLength(vector<int>& nums) {
         int size = nums.size(), peak = 1, valley = 1;
         for (int i=1; i<size; ++i){
