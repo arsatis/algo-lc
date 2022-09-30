7c0797d35aedd19de5e9669b7990e5189139908f
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 22:06:35 +0800

    Time: 81 ms (80.15%), Space: 33.5 MB (79.49%) - LeetHub

--- 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
+++ 442-find-all-duplicates-in-an-array/442-find-all-duplicates-in-an-array.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     vector<int> findDuplicates(vector<int>& nums) {
         vector<int> duplicates;
         for (int i : nums) {
