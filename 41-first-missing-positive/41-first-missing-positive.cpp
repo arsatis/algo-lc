49ad407011f526479a600f135556f22f1827e3ca
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 22:34:54 +0800

    Time: 55 ms (87.27%), Space: 31.1 MB (89.12%) - LeetHub

--- 41-first-missing-positive/41-first-missing-positive.cpp
+++ 41-first-missing-positive/41-first-missing-positive.cpp
@@ -1,7 +1,11 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     int firstMissingPositive(vector<int>& nums) {
-        for (int& i : nums) if (i < 1) i = 10001;
+        for (int& i : nums) if (i < 1) i = nums.size() + 1;
         for (int i : nums) {
             int tmp = abs(i) - 1;
             if (tmp < nums.size() && nums[tmp] > 0) nums[tmp] *= -1;
