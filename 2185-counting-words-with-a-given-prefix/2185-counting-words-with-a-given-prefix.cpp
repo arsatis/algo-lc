97d6e7cfe23d0be7f3606d3fa552a89dbb836846
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 18:57:28 +0800

    Time: 13 ms (83.83%), Space: 9.8 MB (98.05%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     int prefixCount(vector<string>& words, string& pref) {
         int num = 0;
         for (string& w : words) if (w.find(pref) == 0) ++num;
