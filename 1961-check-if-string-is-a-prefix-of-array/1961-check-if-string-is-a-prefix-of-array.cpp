a171fc184e4961b1238a1c6928d491878cdd964e
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 17:59:40 +0800

    Time: 15 ms (23.57%), Space: 14.1 MB (23.10%) - LeetHub

--- 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.cpp
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.cpp
@@ -5,11 +5,12 @@ public:
     }
     
     bool isPrefixString(string s, vector<string>& words) {
-        int innerIdx = 0, outerIdx = 0;
-        for (char c : s) {
-            if (outerIdx == words.size() || c != words[outerIdx][innerIdx++]) return false;
-            if (innerIdx == words[outerIdx].size()) innerIdx = 0, ++outerIdx;
+        string comp =  "";
+        for (string& word : words){
+            comp += word;
+            if(s == comp) return true;
+            if(word.length() > s.length()) return false;
         }
-        return innerIdx == 0;
+        return false;
     }
 };
