9f837c9df6d631e0d43a1c2be3b62c023f541ecc
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 14:04:55 +0800

    Time: 323 ms (11.45%), Space: 67.5 MB (61.07%) - LeetHub

---
+++ 582-kill-process/582-kill-process.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> killProcess(vector<int>& pid, vector<int>& ppid, int kill) {
+        unordered_map<int, vector<int>> children;
+        for (int i = 0; i < ppid.size(); ++i) children[ppid[i]].emplace_back(pid[i]);
+            
+        vector<int> killed;
+        queue<int> q;
+        q.emplace(kill);
+        while (!q.empty()) {
+            int process = q.front();
+            killed.emplace_back(process);
+            for (int i : children[process]) q.emplace(i);
+            q.pop();
+        }
+        return move(killed);
+    }
+};
