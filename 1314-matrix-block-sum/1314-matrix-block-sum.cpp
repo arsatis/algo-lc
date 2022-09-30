47eb95143a5a6764a7bdce61542fb94f6ec1e7e8
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 15:50:47 +0800

    Time: 16 ms (64.99%), Space: 9.4 MB (72.07%) - LeetHub

--- 1314-matrix-block-sum/1314-matrix-block-sum.cpp
+++ 1314-matrix-block-sum/1314-matrix-block-sum.cpp
@@ -5,30 +5,38 @@ public:
     }
     
     vector<vector<int>> matrixBlockSum(vector<vector<int>>& mat, int k) {
-        int n = mat.size(), m = mat[0].size();
-        vector<vector<int>> pref(n, vector<int>(m, 0)), ans(n, vector<int>(m, 0));
+        int m = mat.size();
+        int n = mat[0].size();
+        vector<vector<int>> result(m, vector<int>(n));
         
-        for (int i = 0; i < n; ++i)
-            for (int j = 0; j < m; ++j)
-                pref[i][j] += (j == 0) ? mat[i][j] : pref[i][j - 1] + mat[i][j];
+        for (int i = 1; i < m; i++)
+            mat[i][0] += mat[i-1][0];
         
-        for (int i = 1; i < n; ++i)
-            for (int j = 0; j < m; ++j)
-                pref[i][j] += pref[i - 1][j];
+        for (int i = 1; i < n; i++)
+            mat[0][i] += mat[0][i-1];
         
-        for (int i = 0; i < n; ++i) {
-            for (int j = 0; j < m; ++j) {
-                int minRow = max(0, i - k),
-                    maxRow = min(n - 1, i + k),
-                    minCol = max(0, j - k),
-                    maxCol = min(m - 1, j + k);
-                int val1 = minCol == 0 ? 0 : pref[maxRow][minCol - 1];
-                int val2 = minRow == 0 ? 0 : pref[minRow - 1][maxCol];
-                int val3 = minRow == 0 || minCol == 0
-                    ? 0 : pref[minRow - 1][minCol - 1];
-                ans[i][j] = pref[maxRow][maxCol] - val1 - val2 + val3;
-            }
+        for (int i = 1; i < m; i++)
+        for (int j = 1; j < n; j++)
+            mat[i][j] += mat[i-1][j] + mat[i][j-1] - mat[i-1][j-1];
+        
+        for (int i = 0; i < m; i++)
+        for (int j = 0; j < n; j++)
+        {
+            int left = max(i-k, 0);
+            int right = min(i+k, m-1);
+            int up = max(j-k, 0);
+            int down = min(j+k, n-1);
+            
+            if (left == 0 && up == 0)
+                result[i][j] = mat[right][down];
+            else if (left == 0)
+                result[i][j] = mat[right][down] - mat[right][up-1];
+            else if(up == 0)
+                result[i][j] = mat[right][down] - mat[left-1][down];
+            else
+                result[i][j] = mat[right][down] - mat[left-1][down] - mat[right][up-1] + mat[left-1][up-1];
         }
-        return ans;
+        
+        return result; 
     }
 };
