585cccd769391fe2171f6c0fa1d524dfec0a0c46
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 14:06:34 +0800

    Time: 93 ms (99.62%), Space: 67.5 MB (61.07%) - LeetHub

--- 582-kill-process/582-kill-process.cpp
+++ 582-kill-process/582-kill-process.cpp
@@ -1,4 +1,9 @@
 class Solution {
+    void dfs(vector<int>& killed, unordered_map <int, vector<int>>& adj,int cur){
+        killed.push_back(cur);
+        for(int next: adj[cur])
+            dfs(killed, adj, next);
+    }
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
@@ -7,18 +12,14 @@ public:
     }
     
     vector<int> killProcess(vector<int>& pid, vector<int>& ppid, int kill) {
-        unordered_map<int, vector<int>> children;
-        for (int i = 0; i < ppid.size(); ++i) children[ppid[i]].emplace_back(pid[i]);
-            
+        unordered_map <int, vector<int>> adj;
+        int s = pid.size();
+    
+        for (int i = 0; i < s; ++i)
+            if (ppid[i] > 0)
+                adj[ppid[i]].push_back(pid[i]);
         vector<int> killed;
-        queue<int> q;
-        q.emplace(kill);
-        while (!q.empty()) {
-            int process = q.front();
-            killed.emplace_back(process);
-            for (int i : children[process]) q.emplace(i);
-            q.pop();
-        }
-        return move(killed);
+        dfs(killed, adj, kill);
+        return killed;
     }
 };
