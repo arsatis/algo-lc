abcb2295f11e5958500a5ac00de1f9fef5ba7436
Zheng Yong <angzhengyong@gmail.com>
2022-09-30 14:54:12 +0800

    Time: 58 ms (78.60%), Space: 9.4 MB (91.15%) - LeetHub

---
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int lengthOfLongestSubstringTwoDistinct(string s) {
+        int maxLen = 0, counter[64] = { 0 };
+        for (int l = 0, r = 0, currChars = 0; r < s.size(); ++r) {
+            if (counter[s[r] - 'A'] == 0) {
+                while (currChars == 2) if (--counter[s[l++] - 'A'] == 0) --currChars;
+                ++currChars;
+            }
+            ++counter[s[r] - 'A'];
+            maxLen = max(maxLen, r - l + 1);
+        }
+        return maxLen;
+    }
+};
