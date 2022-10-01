279c9670ba1c1997098b3951e2ee00e1579820c8
Zheng Yong <angzhengyong@gmail.com>
2022-10-01 15:08:51 +0800

    Time: 186 ms (99.92%), Space: 103.8 MB (37.65%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     int minimumRounds(vector<int>& tasks) {
         unordered_map<int, int> taskCount;
         for (int i : tasks) ++taskCount[i];
@@ -7,7 +11,7 @@ public:
         int rounds = 0;
         for (auto& p : taskCount) {
             if (p.second == 1) return -1;
-            rounds += (p.second + 2) / 3;
+            rounds += ceil(p.second / 3.);
         }
         return rounds;
     }
