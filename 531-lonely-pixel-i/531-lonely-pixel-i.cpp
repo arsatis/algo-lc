c0681455cdd796c879982005231072c13ceccde1
Zheng Yong <angzhengyong@gmail.com>
2022-10-01 11:56:30 +0800

    Time: 114 ms (67.89%), Space: 16.8 MB (11.93%) - LeetHub

--- 531-lonely-pixel-i/531-lonely-pixel-i.cpp
+++ 531-lonely-pixel-i/531-lonely-pixel-i.cpp
@@ -7,24 +7,16 @@ public:
     }
     
     int findLonelyPixel(vector<vector<char>>& picture) {
-        const auto& n{int(size(picture))};
-        if(!n)return 0;
-        const auto& m{int(size(picture.front()))};
-        if(!m)return 0;
-        int r[n],c[m];
-        memset(r,0,sizeof(r));
-        memset(c,0,sizeof(c));
-        for(int i=0;i<n;++i){
-            for(int j=0;j<m;++j){
-                if(picture[i][j]=='B')++r[i],++c[j];
-            }
-        }
-        int ret{};
-        for(int i{};i<n;++i){
-            for(int j{};j<m;++j){
-                if(picture[i][j]=='B')ret+=(r[i]==1) and (c[j]==1);
-            }
-        }
-        return ret;
+        int m = picture.size(), n = picture[0].size();
+        vector<int> rows(m), cols(n);
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (picture[i][j] == 'B') ++rows[i], ++cols[j];
+        
+        int num = 0;
+        for (int i = 0; i < m; ++i)
+            for (int j = 0; j < n; ++j)
+                if (picture[i][j] == 'B' && rows[i] == 1 && cols[j] == 1) ++num;
+        return num;
     }
 };
