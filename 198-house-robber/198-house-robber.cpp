96507e39222b895f213a8a073f0415f0ea193d03
Zheng Yong <angzhengyong@gmail.com>
2022-10-01 15:36:49 +0800

    Time: 8 ms (8.19%), Space: 7.8 MB (25.96%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -9,9 +9,8 @@ public:
     int rob(vector<int>& nums) {
         int prev2 = 0, prev1 = 0;
         for (int i : nums) {
-            int curr = max(prev2 + i, prev1);
-            prev2 = prev1;
-            prev1 = curr;
+            prev2 = max(prev2 + i, prev1);
+            swap(prev2, prev1);
         }
         return prev1;
     }
