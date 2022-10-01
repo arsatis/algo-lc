eb01465a91c523d1d391cc61a3910f70ffa71bc4
Zheng Yong <angzhengyong@gmail.com>
2022-10-01 14:22:31 +0800

    Time: 0 ms (100.00%), Space: 5.9 MB (57.07%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.cpp
+++ 70-climbing-stairs/70-climbing-stairs.cpp
@@ -9,7 +9,7 @@ public:
     int climbStairs(int n) {
         if (n <= 2) return n;
         int prev2 = 1, prev1 = 2;
-        for (int i = 3; i <= n; ++i) swap(prev2 += prev1, prev1);
+        while (n-- > 2) swap(prev2 += prev1, prev1);
         return prev1;
     }
 };
