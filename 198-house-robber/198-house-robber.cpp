4ea6158602437ea3bb48186d017d087922e173cc
Zheng Yong <angzhengyong@gmail.com>
2022-10-01 14:26:35 +0800

    Time: 4 ms (38.00%), Space: 7.8 MB (49.68%) - LeetHub

--- 198-house-robber/198-house-robber.cpp
+++ 198-house-robber/198-house-robber.cpp
@@ -1,14 +1,16 @@
 class Solution {
 public:
-    int rob(vector<int>& nums) {
+    Solution() {
         ios_base::sync_with_stdio(0);
-        
-        int i = 0, j = 0;
-        for (int x : nums) {
-            int tmp = j;
-            j = max(i + x, j);
-            i = tmp;
+    }
+    
+    int rob(vector<int>& nums) {
+        int prev2 = 0, prev1 = 0;
+        for (int i : nums) {
+            int tmp = prev1;
+            prev1 = max(prev2 + i, prev1);
+            prev2 = tmp;
         }
-        return j;
+        return prev1;
     }
 };
