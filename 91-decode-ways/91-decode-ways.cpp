417f0189f25c41129280ec500b7bc08ee3725a38
Zheng Yong <angzhengyong@gmail.com>
2022-10-01 11:30:47 +0800

    Time: 0 ms (100.00%), Space: 6.1 MB (75.45%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -1,13 +1,19 @@
 class Solution {
 public:
     int numDecodings(string s) {
-        int p = 1, pp, n = s.size();
+        int prev = 1, prev2, n = s.size();
         for (int i = n - 1; i >= 0; --i) {
-            int cur = s[i] == '0' ? 0 : p;
-            if (i < n - 1 && (s[i] == '1' || s[i] == '2' && s[i + 1] < '7')) cur += pp;
-            pp = p;
-            p = cur;
+            // is the single digit s[i] valid?
+            int curr = s[i] == '0' ? 0 : prev;
+            
+            // are the two digits s[i:i+1] valid?
+            if (i < n - 1 && (s[i] == '1' || s[i] == '2' && s[i + 1] < '7'))
+                curr += prev2;
+            
+            // update curr states
+            prev2 = prev;
+            prev = curr;
         }
-        return s.empty() ? 0 : p;  
+        return s.empty() ? 0 : prev;  
     }
 };
