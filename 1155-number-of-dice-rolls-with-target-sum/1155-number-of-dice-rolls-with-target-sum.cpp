2af1fd8f600c05dcd3583826e85b80bef1ff4cc9
Zheng Yong <angzhengyong@gmail.com>
2022-10-02 10:03:14 +0800

    Time: 3 ms (99.42%), Space: 6.3 MB (73.71%) - LeetHub

--- 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -6,20 +6,21 @@ public:
         cout.tie(0);
     }
     
-    int numRollsToTarget(int n, int k, int target) {
-        int mod = 1e9 + 7;
-        vector<vector<int>> memo(n + 1, vector<int>(target + 1, 0));
-        memo[0][0] = 1;
-        
-        for (int dice = 1; dice <= n; ++dice) {
-            for (int rem = 0; rem <= target; ++rem) {
-                int numWays = 0;
-                for (int i = 1; i <= min(k, rem); ++i)
-                    numWays = (numWays + memo[dice - 1][rem - i]) % mod;
-                memo[dice][rem] = numWays;
+    int numRollsToTarget(int n, int l, int target) {
+        if(target<n || target>n*l) return 0;
+        int mod=1000000007;
+        vector<int>dp(target+1,0);
+        dp[0]=1;
+        for(int i=1;i<=n;i++){
+            int total=0;
+            for(int j=max(0, target-l);j<=target;j++) total=(total+dp[j])%mod;
+            for(int j=target;j>=0;j--){
+                total=(total-dp[j]+mod)%mod;
+                dp[j]=total;
+                if(j-l-1>=0) total=(total+dp[j-l-1])%mod;
             }
         }
-        return memo[n][target];
+        return dp[target];
     }
 };
 
