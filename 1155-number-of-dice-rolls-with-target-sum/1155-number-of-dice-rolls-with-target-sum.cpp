7b2c7f3a54bfd65ad81726fcf5484f5774df6f5f
Zheng Yong <angzhengyong@gmail.com>
2022-10-02 10:01:40 +0800

    Time: 51 ms (49.60%), Space: 8.5 MB (28.55%) - LeetHub

---
+++ 1155-number-of-dice-rolls-with-target-sum/1155-number-of-dice-rolls-with-target-sum.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
+    int numRollsToTarget(int n, int k, int target) {
+        int mod = 1e9 + 7;
+        vector<vector<int>> memo(n + 1, vector<int>(target + 1, 0));
+        memo[0][0] = 1;
+        
+        for (int dice = 1; dice <= n; ++dice) {
+            for (int rem = 0; rem <= target; ++rem) {
+                int numWays = 0;
+                for (int i = 1; i <= min(k, rem); ++i)
+                    numWays = (numWays + memo[dice - 1][rem - i]) % mod;
+                memo[dice][rem] = numWays;
+            }
+        }
+        return memo[n][target];
+    }
+};
