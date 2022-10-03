7ae7e48378b4da5f91daedbc9b6565eb73e13368
Zheng Yong <angzhengyong@gmail.com>
2022-10-03 11:32:44 +0800

    Time: 504 ms (6.65%), Space: 94.4 MB (99.70%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int minCost(string& colors, vector<int>& neededTime) {
+        int totalTime = 0, currMax = 0;
+        for (int i = 0; i < colors.size(); ++i) {
+            if (i > 0 && colors[i] != colors[i - 1]) currMax = 0;
+            totalTime += min(currMax, neededTime[i]);
+            currMax = max(currMax, neededTime[i]);
+        }
+        return totalTime;
+    }
+};
+
+// Solution 1: Two pointers
+// int minCost(string& colors, vector<int>& neededTime) {
+//     int totalTime = 0;
+//     for (int left = 0, right = 0; right < colors.size();) {
+//         int currTime = 0, currMax = 0;
+//         while (right < colors.size() && colors[left] == colors[right]) {
+//             currMax = max(currMax, neededTime[right]);
+//             currTime += neededTime[right++];
+//         }
+//         totalTime += currTime - currMax;
+//         left = right;
+//     }
+//     return totalTime;
+// }
+
+// Solution 2: Greedy
+// int minCost(string& colors, vector<int>& neededTime) {
+//     int totalTime = 0, currMax = 0;
+//     for (int i = 0; i < colors.size(); ++i) {
+//         if (i > 0 && colors[i] != colors[i - 1]) currMax = 0;
+//         totalTime += min(currMax, neededTime[i]);
+//         currMax = max(currMax, neededTime[i]);
+//     }
+//     return totalTime;
+// }
