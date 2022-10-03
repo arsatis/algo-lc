cd6838652088b3aabad84a3dfba7e1d59f06ff26
Zheng Yong <angzhengyong@gmail.com>
2022-10-03 11:43:36 +0800

    Time: 2 ms (67.67%), Space: 6.4 MB (26.01%) - LeetHub

--- 202-happy-number/202-happy-number.cpp
+++ 202-happy-number/202-happy-number.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
-    }
-    
     bool isHappy(int n) {
         unordered_set<int> seen;
         while (seen.find(n) == seen.end()) {
