2575cbbc20bd2c28cd80a2a09cd500822fcda1c6
Zheng Yong <angzhengyong@gmail.com>
2022-10-03 14:10:00 +0800

    Time: 32 ms (75.29%), Space: 14.3 MB (6.12%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -5,21 +5,26 @@ public:
     }
     
     vector<int> findBall(vector<vector<int>>& grid) {
-        vector<int> result(grid[0].size());
+        vector<int> result(grid[0].size(), 0);
+        vector<vector<int>> memo(grid.size() + 1, vector<int>(grid[0].size(), 0));
 
-        for (int i = 0; i < grid[0].size(); ++i) {
-            int currentCol = i;
-            for (int j = 0; j < grid.size(); ++j) {
-                int nextCol = currentCol + grid[j][currentCol];
-                if (nextCol < 0 || nextCol > grid[0].size() - 1 ||
-                    grid[j][currentCol] != grid[j][nextCol]) {
-                    result[i] = -1;
-                    break;
+        for (int row = int(grid.size()); row >= 0; row--) {
+            for (int col = 0; col < grid[0].size(); col++) {
+                if (row == grid.size()) {
+                    memo[row][col] = col;
+                    continue;
                 }
-                result[i] = nextCol;
-                currentCol = nextCol;
+                int nextColumn = col + grid[row][col];
+                if (nextColumn < 0 || nextColumn > grid[0].size() - 1 ||
+                    grid[row][col] != grid[row][nextColumn])
+                    memo[row][col] = -1;
+                else
+                    memo[row][col] = memo[row + 1][nextColumn];
+                if (row == 0) {
+                    result[col] = memo[row][col];
+                }
             }
         }
-        return move(result);
+        return result;
     }
 };
