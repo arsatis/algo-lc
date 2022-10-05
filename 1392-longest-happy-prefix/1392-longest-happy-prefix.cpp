118b9cde985d72fefbbfb2380091285b22e1464d
Zheng Yong <angzhengyong@gmail.com>
2022-10-05 23:41:42 +0800

    Time: 21 ms (99.88%), Space: 13.4 MB (99.07%) - LeetHub

--- 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
+++ 1392-longest-happy-prefix/1392-longest-happy-prefix.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     string longestPrefix(string& s) {
         long long l = 0, r = 0, p = 1, mod = 1e9 + 7;
         int k = 0;
