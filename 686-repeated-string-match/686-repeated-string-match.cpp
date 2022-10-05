69b7966d6c358f8bb10ecd89f8a7f1ff041d3a6b
Zheng Yong <angzhengyong@gmail.com>
2022-10-05 21:42:08 +0800

    Time: 3 ms (97.92%), Space: 7 MB (68.42%) - LeetHub

--- 686-repeated-string-match/686-repeated-string-match.cpp
+++ 686-repeated-string-match/686-repeated-string-match.cpp
@@ -20,6 +20,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int repeatedStringMatch(string& a, string& b) {
