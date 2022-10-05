845b942b5eaf58662f0058cc038dfc76a4af33e6
Zheng Yong <angzhengyong@gmail.com>
2022-10-05 21:21:26 +0800

    Time: 4 ms (33.74%), Space: 6.2 MB (89.84%) - LeetHub

---
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/28-find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int shift = 5, mod = 1e7 + 19, multiplier = 1, n = needle.size();
+        int nHash = 0, hHash = 0;
+        
+        for (int i = 1; i < n; ++i) multiplier = (multiplier << shift) % mod;
+        for (int i = 0; i < n; ++i) {
+            nHash = ((nHash << shift) + needle[i]) % mod;
+            hHash = ((hHash << shift) + haystack[i]) % mod;
+        }
+        
+        if (hHash == nHash) return 0;
+        for (int i = n; i < haystack.size(); ++i) {
+            hHash = (hHash + mod - (multiplier * haystack[i - n] % mod)) % mod; // remove leftmost element
+            hHash = ((hHash << shift) + haystack[i]) % mod; // add rightmost element
+            if (hHash == nHash) return i - n + 1;
+        }
+        return -1;
+    }
+};
