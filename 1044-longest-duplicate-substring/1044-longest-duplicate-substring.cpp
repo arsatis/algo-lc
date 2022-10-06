3d114178a3593a2dd7cb479443e7cf9323373b80
Zheng Yong <angzhengyong@gmail.com>
2022-10-06 23:30:06 +0800

    Time: 95 ms (99.65%), Space: 11.4 MB (99.82%) - LeetHub

--- 1044-longest-duplicate-substring/1044-longest-duplicate-substring.cpp
+++ 1044-longest-duplicate-substring/1044-longest-duplicate-substring.cpp
@@ -36,6 +36,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     string longestDupSubstring(string s) {
