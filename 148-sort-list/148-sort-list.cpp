62cf91c02e1d1e345aa1a4302054f2a472e869b1
Zheng Yong <angzhengyong@gmail.com>
2022-10-06 14:45:21 +0800

    Time: 360 ms (53.77%), Space: 50.8 MB (98.43%) - LeetHub

--- 148-sort-list/148-sort-list.cpp
+++ 148-sort-list/148-sort-list.cpp
@@ -9,21 +9,40 @@
  * };
  */
 class Solution {
+    ListNode* getMid(ListNode* head) {
+        ListNode dummy(0, head), *prev = &dummy;
+        while (head && head->next) {
+            prev = prev->next;
+            head = head->next->next;
+        }
+        ListNode* mid = prev->next;
+        prev->next = nullptr;
+        return mid;
+    }
+    
+    ListNode* merge(ListNode* list1, ListNode* list2) {
+        ListNode dummy(0), *ptr = &dummy;
+        while (list1 && list2) {
+            if (list1->val < list2->val) {
+                ptr->next = list1;
+                list1 = list1->next;
+            } else {
+                ptr->next = list2;
+                list2 = list2->next;
+            }
+            ptr = ptr->next;
+        }
+        ptr->next = list1 ? list1 : list2;
+        return dummy.next;
+    }
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     ListNode* sortList(ListNode* head) {
-        // O(n log n) time, O(n) space
-        vector<int> list;
-        while (head) {
-            list.emplace_back(head->val);
-            head = head->next;
-        }
-        sort(list.begin(), list.end());
-        
-        ListNode* dummy = new ListNode(), *curr = dummy;
-        for (int i : list) {
-            curr->next = new ListNode(i);
-            curr = curr->next;
-        }
-        return dummy->next;
+        if (!head || !head->next) return head;
+        ListNode* mid = getMid(head);
+        return merge(sortList(head), sortList(mid));
     }
 };
