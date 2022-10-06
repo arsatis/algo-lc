c00bf29c1d8115830c0ef7e0d41f956b39980ea0
Zheng Yong <angzhengyong@gmail.com>
2022-10-06 09:59:53 +0800

    Time: 645 ms (44.57%), Space: 128.3 MB (57.82%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.cpp
+++ 981-time-based-key-value-store/981-time-based-key-value-store.cpp
@@ -3,8 +3,6 @@ class TimeMap {
 public:
     TimeMap() {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
     }
     
     void set(string key, string value, int timestamp) {
