792d139978fa0fa421ddb24172456a13e854bfd4
Zheng Yong <angzhengyong@gmail.com>
2022-10-08 10:40:25 +0800

    Time: 876 ms (25.04%), Space: 142.1 MB (77.53%) - LeetHub

---
+++ 716-max-stack/716-max-stack.cpp
@@ -0,0 +1,48 @@
+class MaxStack {
+    set<pair<int, int>> stack;
+    set<pair<int, int>> values;
+    int cnt;
+
+public:
+    MaxStack() {
+        cnt = 0;
+    }
+
+    void push(int x) {
+        stack.insert({cnt, x});
+        values.insert({x, cnt});
+        cnt++;
+    }
+
+    int pop() {
+        pair<int, int> p = *stack.rbegin();
+        stack.erase(p);
+        values.erase({p.second, p.first});
+        return p.second;
+    }
+
+    int top() {
+        return stack.rbegin()->second;
+    }
+
+    int peekMax() {
+        return values.rbegin()->first;
+    }
+
+    int popMax() {
+        pair<int, int> p = *values.rbegin();
+        values.erase(p);
+        stack.erase({p.second, p.first});
+        return p.first;
+    }
+};
+
+/**
+ * Your MaxStack object will be instantiated and called as such:
+ * MaxStack* obj = new MaxStack();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->peekMax();
+ * int param_5 = obj->popMax();
+ */
