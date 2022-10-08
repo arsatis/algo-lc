9d4b7bc24d6fa51f98dccae2502d1e200955fe16
Zheng Yong <angzhengyong@gmail.com>
2022-10-08 10:36:39 +0800

    Time: 32 ms (99.91%), Space: 16.4 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -7,17 +7,45 @@ public:
     }
     
     int threeSumClosest(vector<int>& nums, int target) {
-        int diff = INT_MAX, n = nums.size();
-        sort(begin(nums), end(nums));
-        for (int i = 0; i < n && diff != 0; ++i) {
-            int lo = i + 1, hi = n - 1;
-            while (lo < hi) {
-                int sum = nums[i] + nums[lo] + nums[hi];
-                if (abs(target - sum) < abs(diff)) diff = target - sum;
-                if (sum < target) ++lo;
-                else --hi;
+        sort(nums.begin(), nums.end());
+        int closest = INT_MAX>>2;
+        int N = nums.size();
+        for (int i=0; i<N-2; ++i) {
+            if (nums[i] + nums[N-2] + nums[N-1] < target) {
+                int sum = nums[i] + nums[N-2] + nums[N-1];
+                if (abs(target - sum) <  abs(target - closest)) {
+                    closest = sum;
+                }
+                continue;
+            }
+            if (nums[i] + nums[i+1] + nums[i+2] > target) {
+                int sum = nums[i] + nums[i+1] + nums[i+2];
+                if (abs(target - sum) <  abs(target - closest)) {
+                    closest = sum;
+                }
+                return closest;
+            }
+            if (i>0 && nums[i] == nums[i-1]) continue;
+            int j = i+1;
+            int k = N - 1;
+            while (j < k) {
+                if (j>i+1 && nums[j] == nums[j-1]) {
+                    ++j;
+                    continue;
+                }
+                if (k<N-1 && nums[k] == nums[k+1]) {
+                    --k;
+                    continue;
+                }
+                int sum = nums[i] + nums[j] + nums[k];
+                if (sum == target) return target;
+                if (abs(target - sum) <  abs(target - closest)) {
+                    closest = sum;
+                }
+                if (sum < target) j++;
+                if (sum > target) k--;
             }
         }
-        return target - diff;
+        return closest;
     }
 };
