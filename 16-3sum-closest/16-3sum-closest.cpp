f91b177d39265e888ee0f1567a8e55dc767066bc
Zheng Yong <angzhengyong@gmail.com>
2022-10-08 10:39:07 +0800

    Time: 37 ms (99.77%), Space: 16.5 MB (48.38%) - LeetHub

--- 16-3sum-closest/16-3sum-closest.cpp
+++ 16-3sum-closest/16-3sum-closest.cpp
@@ -7,43 +7,36 @@ public:
     }
     
     int threeSumClosest(vector<int>& nums, int target) {
+        int closest = INT_MAX >> 2, n = nums.size();
         sort(nums.begin(), nums.end());
-        int closest = INT_MAX>>2;
-        int N = nums.size();
-        for (int i=0; i<N-2; ++i) {
-            if (nums[i] + nums[N-2] + nums[N-1] < target) {
-                int sum = nums[i] + nums[N-2] + nums[N-1];
-                if (abs(target - sum) <  abs(target - closest)) {
-                    closest = sum;
-                }
+        for (int i = 0; i < n - 2; ++i) {
+            if (nums[i] + nums[n - 2] + nums[n - 1] < target) {
+                int sum = nums[i] + nums[n - 2] + nums[n - 1];
+                if (abs(target - sum) <  abs(target - closest)) closest = sum;
                 continue;
             }
-            if (nums[i] + nums[i+1] + nums[i+2] > target) {
-                int sum = nums[i] + nums[i+1] + nums[i+2];
-                if (abs(target - sum) <  abs(target - closest)) {
-                    closest = sum;
-                }
+            if (nums[i] + nums[i + 1] + nums[i + 2] > target) {
+                int sum = nums[i] + nums[i + 1] + nums[i + 2];
+                if (abs(target - sum) <  abs(target - closest)) closest = sum;
                 return closest;
             }
-            if (i>0 && nums[i] == nums[i-1]) continue;
-            int j = i+1;
-            int k = N - 1;
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int j = i + 1, k = n - 1;
             while (j < k) {
-                if (j>i+1 && nums[j] == nums[j-1]) {
+                if (j > i + 1 && nums[j] == nums[j - 1]) {
                     ++j;
                     continue;
                 }
-                if (k<N-1 && nums[k] == nums[k+1]) {
+                if (k < n - 1 && nums[k] == nums[k + 1]) {
                     --k;
                     continue;
                 }
                 int sum = nums[i] + nums[j] + nums[k];
                 if (sum == target) return target;
-                if (abs(target - sum) <  abs(target - closest)) {
-                    closest = sum;
-                }
-                if (sum < target) j++;
-                if (sum > target) k--;
+                if (abs(target - sum) <  abs(target - closest)) closest = sum;
+                if (sum < target) ++j;
+                if (sum > target) --k;
             }
         }
         return closest;
