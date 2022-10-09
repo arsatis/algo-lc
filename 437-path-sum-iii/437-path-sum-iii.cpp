b5bd690aa20097a7d4bff081e0224a6c71d0f7a8
Zheng Yong <angzhengyong@gmail.com>
2022-10-09 10:28:35 +0800

    Time: 23 ms (89.79%), Space: 19.3 MB (39.28%) - LeetHub

--- 437-path-sum-iii/437-path-sum-iii.cpp
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -27,6 +27,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int pathSum(TreeNode* root, int targetSum) {
