b553fe17a88bf8c0baf12c9ef174ae6196e46465
Zheng Yong <angzhengyong@gmail.com>
2022-10-09 10:28:24 +0800

    Time: 23 ms (89.79%), Space: 19.3 MB (39.28%) - LeetHub

---
+++ 437-path-sum-iii/437-path-sum-iii.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+    int numPaths = 0, k;
+    unordered_map<long, int> visited;
+    
+    void f(TreeNode* node, long curr) {
+        curr += node->val;
+        if (curr == k) ++numPaths;
+        if (visited.find(curr - k) != visited.end())
+            numPaths += visited[curr - k];
+        
+        ++visited[curr];
+        if (node->left) f(node->left, curr);
+        if (node->right) f(node->right, curr);
+        --visited[curr];
+    }
+public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
+    int pathSum(TreeNode* root, int targetSum) {
+        if (!root) return 0;
+        k = targetSum;
+        f(root, 0);
+        return numPaths;
+    }
+};
