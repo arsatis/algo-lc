e183d58bbad67abf5cea56b0be7dc75bb51f847e
Zheng Yong <angzhengyong@gmail.com>
2022-10-10 10:05:27 +0800

    Time: 163 ms (53.35%), Space: 19.9 MB (85.39%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -6,8 +6,7 @@ public:
         vector<vector<int>> res;
         for (int i = 0; i < n - 2; ++i) {
            if (i > 0 && (nums[i] == nums[i - 1])) continue;
-           int l = i + 1, r = n - 1;
-           while (l < r) {
+           for (int l = i + 1, r = n - 1; l < r;) {
                int sum = nums[i] + nums[l] + nums[r];
                if (sum < 0) ++l;
                else if (sum > 0) --r;
