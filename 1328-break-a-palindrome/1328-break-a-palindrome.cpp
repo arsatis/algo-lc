695e2922cbc71582e81834eaa0a03c4e521726f1
Zheng Yong <angzhengyong@gmail.com>
2022-10-10 09:23:13 +0800

    Time: 8 ms (10.44%), Space: 6.2 MB (45.53%) - LeetHub

---
+++ 1328-break-a-palindrome/1328-break-a-palindrome.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string breakPalindrome(string& palindrome) {
+        if (palindrome.size() == 1) return "";
+        bool isOdd = palindrome.size() & 1;
+        int i = 0, mid = palindrome.size() >> 1;
+        for (; i < palindrome.size(); ++i)
+            if (palindrome[i] != 'a' && (!isOdd || i != mid)) {
+                palindrome[i] = 'a';
+                break;
+            }
+        if (i == palindrome.size()) palindrome.back() = 'b';
+        return palindrome;
+    }
+};
