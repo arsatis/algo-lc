98d00b7e0e19538587cfe08ed83c4743a7aa9ea6
Zheng Yong <angzhengyong@gmail.com>
2022-10-10 09:57:00 +0800

    Time: 0 ms (100.00%), Space: 6.1 MB (51.59%) - LeetHub

--- 365-water-and-jug-problem/365-water-and-jug-problem.cpp
+++ 365-water-and-jug-problem/365-water-and-jug-problem.cpp
@@ -1,6 +1,10 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     bool canMeasureWater(int x, int y, int z) {
-        return z == 0 || z <= (long long)x + y && z % gcd(x, y) == 0;
+        return z == 0 || z <= (long long) x + y && z % gcd(x, y) == 0;
     }
 };
