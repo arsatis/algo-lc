34f91fdc2e26f5693cdc20b45c583816ce8e270b
Zheng Yong <angzhengyong@gmail.com>
2022-10-10 10:02:25 +0800

    Time: 150 ms (59.98%), Space: 19.9 MB (85.39%) - LeetHub

--- 15-3sum/15-3sum.cpp
+++ 15-3sum/15-3sum.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     vector<vector<int>> threeSum(vector<int>& nums) {
         sort(nums.begin(), nums.end());
         int n = nums.size();
