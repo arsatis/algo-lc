e592e3a142e770db633fa7827a1484353b95b4f6
Zheng Yong <angzhengyong@gmail.com>
2022-10-11 11:45:30 +0800

    Time: 130 ms (49.59%), Space: 61.5 MB (87.68%) - LeetHub

--- 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     bool increasingTriplet(vector<int>& nums) {
         int first = INT_MAX, second = INT_MAX;
         for (int x : nums) {
