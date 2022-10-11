605da7eed2f78144100651077ae321a92848729c
Zheng Yong <angzhengyong@gmail.com>
2022-10-11 14:18:27 +0800

    Time: 30 ms (32.21%), Space: 21.6 MB (20.10%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.cpp
@@ -24,8 +24,6 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
     }
     
     TreeNode* sortedArrayToBST(vector<int>& nums) {
