ba2407bdcd47f22cd1adeb8c011d0a32c4c79e1a
Zheng Yong <angzhengyong@gmail.com>
2022-10-11 14:26:14 +0800

    Time: 31 ms (58.19%), Space: 24.1 MB (86.27%) - LeetHub

--- 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
+++ 0230-kth-smallest-element-in-a-bst/0230-kth-smallest-element-in-a-bst.cpp
@@ -19,8 +19,6 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
     }
     
     int kthSmallest(TreeNode* root, int k) {
