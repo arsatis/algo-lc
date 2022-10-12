3cc3cb2fca4cf61684e377477441ac7cad48774b
Zheng Yong <angzhengyong@gmail.com>
2022-10-12 16:44:31 +0800

    Time: 3315 ms (5.56%), Space: 51.5 MB (97.49%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.py
@@ -3,6 +3,11 @@ class Solution:
         in_nodes = [0] * n
         for edge in edges:
             in_nodes[edge[1]] += 1
-        return [i for i in range(n) if in_nodes[i] == 0]
+        
+        output = []
+        for i in range(n):
+            if in_nodes[i] == 0:
+                output.append(i)
+        return output
         
         
