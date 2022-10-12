1dc699406db39bb66acdf03596f5eb1585fbd08f
Zheng Yong <angzhengyong@gmail.com>
2022-10-12 16:56:50 +0800

    Time: 22 ms (14.80%), Space: 13.3 MB (27.72%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -5,8 +5,6 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
     }
     
     int orangesRotting(vector<vector<int>>& grid) {
