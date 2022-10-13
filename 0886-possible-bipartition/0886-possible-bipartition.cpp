9c6a9b9a698a249ee1d2d7d06d38f23fedfa75e1
Zheng Yong <angzhengyong@gmail.com>
2022-10-13 10:11:29 +0800

    Time: 509 ms (19.25%), Space: 316.9 MB (5.13%) - LeetHub

---
+++ 0886-possible-bipartition/0886-possible-bipartition.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+    
+public:
+    bool possibleBipartition(int n, vector<vector<int>>& dislikes) {
+        unordered_map<int, int> color;
+        vector<vector<int>> adjlist(n + 1);
+        for (int i = 0; i <= n; ++i) adjlist[i].reserve(n + 1);
+        for (auto& d : dislikes) {
+            adjlist[d[0]].emplace_back(d[1]);
+            adjlist[d[1]].emplace_back(d[0]);
+        }
+        
+        for (int i = 1; i <= n; ++i) {
+            queue<int> q;
+            q.emplace(i);
+            color.insert({i, 0});
+            
+            while (!q.empty()) {
+                int node = q.front();
+                q.pop();
+                for (int j : adjlist[node]) {
+                    int altColor = color[node] ^ 1;
+                    if (color.find(j) == color.end()) color.insert({j, altColor});
+                    else if (color[j] == altColor) continue;
+                    else return false;
+                    q.emplace(j);
+                }
+            }
+        }
+        return true;
+    }
+};
