04c2d6c7542327dc0535700b837916af06e10911
Zheng Yong <angzhengyong@gmail.com>
2022-10-13 09:46:27 +0800

    Time: 124 ms (80.59%), Space: 29.7 MB (93.20%) - LeetHub

--- 1615-maximal-network-rank/1615-maximal-network-rank.cpp
+++ 1615-maximal-network-rank/1615-maximal-network-rank.cpp
@@ -7,17 +7,19 @@ public:
     }
     
     int maximalNetworkRank(int n, vector<vector<int>>& roads) {
-        vector<unordered_set<int>> neighbours(n);
+        vector<vector<int>> neighbours(n, vector<int>(n));
+        vector<int> numNeighbours(n);
         for (auto& road : roads) {
-            neighbours[road.front()].insert(road.back());
-            neighbours[road.back()].insert(road.front());
+            ++neighbours[road.front()][road.back()];
+            ++neighbours[road.back()][road.front()];
+            ++numNeighbours[road.front()];
+            ++numNeighbours[road.back()];
         }
         
         int maxRank = 0;
         for (int i = 0; i < n; ++i)
             for (int j = i + 1; j < n; ++j) {
-                int currRank = neighbours[i].size() + neighbours[j].size() -
-                    (neighbours[i].find(j) == neighbours[i].end() ? 0 : 1);
+                int currRank = numNeighbours[i] + numNeighbours[j] - neighbours[i][j];
                 maxRank = max(maxRank, currRank);
             }
         return maxRank;
