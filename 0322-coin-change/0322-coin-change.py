a20c21d5af78f4fc8a4a9f0cd004763769ec5a02
Zheng Yong <angzhengyong@gmail.com>
2022-10-14 10:48:59 +0800

    Time: 1666 ms (83.42%), Space: 14.4 MB (39.65%) - LeetHub

--- 0322-coin-change/0322-coin-change.py
+++ 0322-coin-change/0322-coin-change.py
@@ -2,9 +2,9 @@ class Solution:
     def coinChange(self, coins: List[int], amount: int) -> int:
         dp = [0] + [-1] * amount
         for i in range(1, amount + 1):
-            val = math.inf
+            val = 100000
             for coin in coins:
                 if i - coin >= 0 and dp[i - coin] != -1:
                     val = min(val, 1 + dp[i - coin])
-            if val != math.inf: dp[i] = val
+            if val != 100000: dp[i] = val
         return dp[-1]
