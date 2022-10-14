7be15ae2b4dff1a620977d4e78d7140d2cd9227c
Zheng Yong <angzhengyong@gmail.com>
2022-10-14 10:38:26 +0800

    Time: 0 ms (100.00%), Space: 7.8 MB (49.79%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+    
+    int rob(vector<int>& nums) {
+        int prev2 = 0, prev1 = 0;
+        for (int i : nums) {
+            prev2 = max(prev2 + i, prev1);
+            swap(prev2, prev1);
+        }
+        return prev1;
+    }
+};
