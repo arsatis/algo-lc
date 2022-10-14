47fbded6da39a0eaa607538acd06699959594556
Zheng Yong <angzhengyong@gmail.com>
2022-10-14 10:37:11 +0800

    Time: 63 ms (29.81%), Space: 13.8 MB (97.21%) - LeetHub

--- 0198-house-robber/0198-house-robber.py
+++ 0198-house-robber/0198-house-robber.py
@@ -2,7 +2,6 @@ class Solution:
     def rob(self, nums: List[int]) -> int:
         prev1, prev2 = 0, 0
         for x in nums:
-            tmp = max(prev1, prev2 + x)
-            prev2 = prev1
-            prev1 = tmp
+            prev2 = max(prev1, prev2 + x)
+            prev1, prev2 = prev2, prev1
         return prev1
