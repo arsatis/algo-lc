23550fb9b6dd4ef59324151c915583144b3036e1
Zheng Yong <angzhengyong@gmail.com>
2022-10-15 14:19:11 +0800

    Time: 28 ms (99.09%), Space: 9.6 MB (96.63%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
-        if (sum % 2) return false;
+        if (sum & 1) return false;
         bitset<10001> dp(1);
         
         // find all possible subset sums: each idx rep. a subset sum = idx is possible
