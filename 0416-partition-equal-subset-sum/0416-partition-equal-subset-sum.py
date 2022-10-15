96fd61a9be00e4d460e341169b3c06c27e4d3109
Zheng Yong <angzhengyong@gmail.com>
2022-10-15 13:54:53 +0800

    Time: 2804 ms (42.92%), Space: 13.9 MB (97.53%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -3,7 +3,7 @@ class Solution:
         total_sum = sum(nums)
 
         # if total_sum is odd, it cannot be partitioned into equal sum subsets
-        if total_sum % 2 != 0: return False
+        if total_sum % 2: return False
         subset_sum = total_sum >> 1
 
         # construct a dp table of size (subset_sum + 1)
