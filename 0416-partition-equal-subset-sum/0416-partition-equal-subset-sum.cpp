4901cd558416947080bee177c5873b07b2e1f222
Zheng Yong <angzhengyong@gmail.com>
2022-10-15 13:58:28 +0800

    Time: 17 ms (99.69%), Space: 9.7 MB (92.85%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.cpp
@@ -1,13 +1,8 @@
 class Solution {
 public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
-    }
-    
     bool canPartition(vector<int>& nums) {
         int sum = accumulate(nums.begin(), nums.end(), 0);
         if (sum & 1) return false;
-        
         bitset<10001> dp(1);   // 0 is always achievable => dp[0] = 1
         for (int num : nums) dp = dp | dp << num;
         return dp[sum >> 1];
