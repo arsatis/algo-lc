8c7f765e7a87b14c05a5a5e1d4e6fa08dc64eee1
Zheng Yong <angzhengyong@gmail.com>
2022-10-16 15:49:29 +0800

    Time: 12 ms (98.51%), Space: 7.7 MB (68.79%) - LeetHub

--- 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
+++ 1335-minimum-difficulty-of-a-job-schedule/1335-minimum-difficulty-of-a-job-schedule.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int minDifficulty(vector<int>& A, int d) {
