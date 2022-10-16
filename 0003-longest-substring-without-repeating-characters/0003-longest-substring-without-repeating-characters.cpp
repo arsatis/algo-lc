b424ed5e7878993ea676797fa7099dde40fd57b2
Zheng Yong <angzhengyong@gmail.com>
2022-10-16 16:13:14 +0800

    Time: 9 ms (91.85%), Space: 6.7 MB (99.32%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     int lengthOfLongestSubstring(string& s) {
         int maxLen = 0, counter[128] = {};
         for (int left = 0, right = 0; right < s.size(); ++right) {
