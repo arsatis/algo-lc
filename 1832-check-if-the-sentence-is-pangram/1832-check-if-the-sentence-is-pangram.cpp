0c0b72288b428c8403b0005e06cd8d4a0ea7ba9c
Zheng Yong <angzhengyong@gmail.com>
2022-10-17 10:32:56 +0800

    Time: 0 ms (100.00%), Space: 6.4 MB (94.60%) - LeetHub

--- 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
+++ 1832-check-if-the-sentence-is-pangram/1832-check-if-the-sentence-is-pangram.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     bool checkIfPangram(string& sentence) {
         int counter[26] = {};
         for (char c : sentence) ++counter[c - 'a'];
