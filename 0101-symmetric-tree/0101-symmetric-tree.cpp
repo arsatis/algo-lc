a966d4fe877ebe1912cad5924d13b75738f9e947
Zheng Yong <angzhengyong@gmail.com>
2022-10-17 12:03:38 +0800

    Time: 17 ms (11.73%), Space: 16.5 MB (18.12%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -16,12 +16,6 @@ class Solution {
                                        && f(left->right, right->left);
     }
 public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-    }
-    
     bool isSymmetric(TreeNode* root) {
         if (!root) return true;
         return f(root->left, root->right);
