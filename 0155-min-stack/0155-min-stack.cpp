ee10d29c13a35b9b0b71b23c52a56878322811df
Zheng Yong <angzhengyong@gmail.com>
2022-10-18 10:44:05 +0800

    Time: 16 ms (99.05%), Space: 16.6 MB (10.39%) - LeetHub

---
+++ 0155-min-stack/0155-min-stack.cpp
@@ -0,0 +1,37 @@
+class MinStack {
+    stack<int> st;
+    stack<pair<int, int>> minst;
+public:
+    MinStack() {
+        ios_base::sync_with_stdio(0);
+    }
+    
+    void push(int val) {
+        st.emplace(val);
+        if (!minst.empty() && val >= minst.top().first) ++minst.top().second;
+        else minst.emplace(val, 1);
+    }
+    
+    void pop() {
+        st.pop();
+        if (!minst.empty() && minst.top().second > 1) --minst.top().second;
+        else minst.pop();
+    }
+    
+    int top() {
+        return st.top();
+    }
+    
+    int getMin() {
+        return minst.top().first;
+    }
+};
+
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack* obj = new MinStack();
+ * obj->push(val);
+ * obj->pop();
+ * int param_3 = obj->top();
+ * int param_4 = obj->getMin();
+ */
