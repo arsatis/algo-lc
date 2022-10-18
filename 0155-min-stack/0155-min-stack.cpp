248e556183138071d5658e3744a8a31baf15bdc3
Zheng Yong <angzhengyong@gmail.com>
2022-10-18 10:44:25 +0800

    Time: 62 ms (16.99%), Space: 16.4 MB (30.99%) - LeetHub

--- 0155-min-stack/0155-min-stack.cpp
+++ 0155-min-stack/0155-min-stack.cpp
@@ -3,9 +3,7 @@ class MinStack {
     stack<pair<int, int>> minst;
 public:
     MinStack() {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
+        
     }
     
     void push(int val) {
