28ac7e7172eefb4c4feb7955f993b2d802795e5b
Zheng Yong <angzhengyong@gmail.com>
2022-10-18 10:33:05 +0800

    Time: 73 ms (86.14%), Space: 44.9 MB (70.23%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.cpp
@@ -6,9 +6,6 @@ class Trie {
     TrieNode* root;
 public:
     Trie() {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
         root = new TrieNode();
     }
     
