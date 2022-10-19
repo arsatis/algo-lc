8034b287543d10b3e87b6f1b840176666b1c3733
Zheng Yong <angzhengyong@gmail.com>
2022-10-19 09:28:47 +0800

    Time: 27 ms (45.47%), Space: 12.7 MB (39.56%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+        
+        unordered_map<string, int> counts;
+        for (string& word : words) ++counts[word];
+        
+        vector<string> buckets[words.size() + 1];
+        for (auto& pair : counts) buckets[pair.second].push_back(pair.first);
+        
+        vector<string> mostFreq(k);
+        int outerIdx = words.size(), innerIdx = 0;
+        for (int i = 0; i < k; ++i) {
+            while (buckets[outerIdx].size() == 0) --outerIdx;
+            sort(buckets[outerIdx].begin(), buckets[outerIdx].end());
+            mostFreq[i] = buckets[outerIdx][innerIdx];
+            if (buckets[outerIdx].size() <= ++innerIdx) {
+                --outerIdx;
+                innerIdx = 0;
+            }
+        }
+        return move(mostFreq);
+    }
+};
