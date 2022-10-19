799d37ae7e5010252b9b5adf3dfa27635fec64e4
Zheng Yong <angzhengyong@gmail.com>
2022-10-19 15:28:15 +0800

    Time: 85 ms (37.14%), Space: 18.7 MB (98.24%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -11,6 +11,7 @@ public:
             return i[1] < j[1];
         });
         vector<vector<int>> output;
+        output.reserve(intervals.size());
         for (int i = intervals.size() - 1; i >= 0; --i) {
             if (output.size() == 0 || output.back()[0] > intervals[i][1]) output.push_back(intervals[i]);
             else output.back()[0] = min(output.back()[0], intervals[i][0]);
