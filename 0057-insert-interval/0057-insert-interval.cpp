1e9b0be4f21d43084642524e21b8ca21e575c384
Zheng Yong <angzhengyong@gmail.com>
2022-10-20 10:25:15 +0800

    Time: 32 ms (52.46%), Space: 17.1 MB (74.22%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+        vector<vector<int>> output;
+        output.reserve(intervals.size() + 1);
+        
+        // place all smaller intervals into output
+        int i = 0;
+        while (i < intervals.size() && intervals[i].back() < newInterval.front()) output.emplace_back(intervals[i++]);
+        
+        // place new interval into output
+        if (i < intervals.size()) output.push_back({min(intervals[i].front(), newInterval.front()), newInterval.back()});
+        else output.emplace_back(newInterval);
+        
+        // extend the end of the new interval if necessary, and place remaining intervals into output
+        if (i != intervals.size()) {
+            while (i < intervals.size() && intervals[i].front() <= output.back().back())
+                output.back().back() = max(output.back().back(), intervals[i++].back());
+            while (i < intervals.size()) output.emplace_back(intervals[i++]);
+        }
+        return move(output);
+    }
+};
