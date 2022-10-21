151616e556be712bd46c93de6bf8f4f2c283aa3b
Zheng Yong <angzhengyong@gmail.com>
2022-10-21 09:42:55 +0800

    Time: 41 ms (66.21%), Space: 13.6 MB (90.14%) - LeetHub

---
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -0,0 +1,21 @@
+class Solution {
+    int visited[200] = {};
+    void dfs(vector<vector<int>>& mat, int i) {
+        visited[i] = 1;
+        for (int j = 0; j < mat.size(); ++j)
+            if (mat[i][j] && !visited[j]) dfs(mat, j);
+    }
+public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
+    }
+    
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size(), num = 0;
+        for (int i = 0; i < n; ++i)
+            if (!visited[i]) dfs(isConnected, i), ++num;
+        return num;
+    }
+};
