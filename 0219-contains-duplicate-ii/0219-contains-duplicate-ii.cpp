f75554e431f53c8aac64c5d1d849f8e5aab5323a
Zheng Yong <angzhengyong@gmail.com>
2022-10-21 09:33:55 +0800

    Time: 295 ms (68.48%), Space: 72.1 MB (98.02%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         unordered_set<int> seen;
         for (int left = 0, right = 0; right < nums.size(); ++right) {
