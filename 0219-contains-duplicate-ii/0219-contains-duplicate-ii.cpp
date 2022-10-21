8bf6167fd5b822acd567d9a15ecfdda0b22d5c80
Zheng Yong <angzhengyong@gmail.com>
2022-10-21 09:37:41 +0800

    Time: 134 ms (99.82%), Space: 60.1 MB (99.79%) - LeetHub

--- 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.cpp
@@ -7,11 +7,9 @@ public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
         // makes use of locality: O(nk), but faster than O(n) algorithms
         uint_least8_t size = nums.size() - 1;
-        for (uint_fast8_t i = 0; i < size; ++i) {
-            for (uint_fast8_t j = i + 1; j - i <= k && j <= size; ++j) {
+        for (uint_fast8_t i = 0; i < size; ++i)
+            for (uint_fast8_t j = i + 1; j - i <= k && j <= size; ++j)
                 if (nums[i] == nums[j]) return true;
-            }
-        }
         return false;
     }
 };
