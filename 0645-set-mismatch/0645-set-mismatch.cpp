02b0347dfdffd43719e9557185c803e4b0eb67ad
Zheng Yong <angzhengyong@gmail.com>
2022-10-23 10:22:24 +0800

    Time: 73 ms (57.98%), Space: 21.5 MB (66.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -1,9 +1,5 @@
 class Solution {
 public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
-    }
-    
     vector<int> findErrorNums(vector<int>& nums) {
         vector<int> output;
         output.reserve(2);
