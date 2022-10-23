01a220caa02aeb55d183f95f7c9bd323afe8461e
Zheng Yong <angzhengyong@gmail.com>
2022-10-23 10:22:00 +0800

    Time: 37 ms (88.91%), Space: 21.4 MB (66.56%) - LeetHub

--- 0645-set-mismatch/0645-set-mismatch.cpp
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -11,7 +11,6 @@ public:
         output.reserve(2);
         for (int x : nums) nums[abs(x) - 1] *= -1;
         for (int i = 0; i < nums.size(); ++i) if (nums[i] > 0) output.emplace_back(i + 1);
-        cout << output.front() << "\n";
         if (find(nums.begin(), nums.end(), output.front()) == nums.end() &&
             find(nums.begin(), nums.end(), -output.front()) == nums.end()) reverse(output.begin(), output.end());
         return move(output);
