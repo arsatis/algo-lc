c3899916aa83673b7128f22e8bea2e228469bc13
Zheng Yong <angzhengyong@gmail.com>
2022-10-23 10:45:07 +0800

    Time: 21 ms (78.13%), Space: 11.8 MB (17.50%) - LeetHub

--- 0296-best-meeting-point/0296-best-meeting-point.cpp
+++ 0296-best-meeting-point/0296-best-meeting-point.cpp
@@ -7,9 +7,9 @@ public:
     }
     
     int minTotalDistance(vector<vector<int>>& grid) {
-        int row = grid.size();
+        int row = grid.size(), col = grid[0].size();;
         if (0 == row) return 0;
-        const int col = grid[0].size();
+        
         int total = 0;
         vector<int> posR, posC;
         for (int i = 0; i < row; ++i) 
@@ -19,6 +19,7 @@ public:
                     posC.emplace_back(j);
                 }
             }
+        
         int med1 = posR[posR.size() >> 1];
         nth_element(posC.begin(), posC.begin() + (posC.size() >> 1), posC.end());
         int med2 = posC[posC.size() >> 1];
