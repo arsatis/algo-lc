eaba8a2c67186b51652e431899607d821b0c986d
Zheng Yong <angzhengyong@gmail.com>
2022-10-23 10:20:40 +0800

    Time: 90 ms (39.98%), Space: 21.3 MB (86.33%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        vector<int> output;
+        output.reserve(2);
+        for (int x : nums) nums[abs(x) - 1] *= -1;
+        for (int i = 0; i < nums.size(); ++i) if (nums[i] > 0) output.emplace_back(i + 1);
+        cout << output.front() << "\n";
+        if (find(nums.begin(), nums.end(), output.front()) == nums.end() &&
+            find(nums.begin(), nums.end(), -output.front()) == nums.end()) reverse(output.begin(), output.end());
+        return move(output);
+    }
+};
