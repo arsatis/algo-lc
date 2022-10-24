a677997bcde3dde6d6cdce621b3e451fd1691f65
Zheng Yong <angzhengyong@gmail.com>
2022-10-24 11:36:37 +0800

    Time: 5 ms (33.39%), Space: 8.8 MB (61.40%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     vector<int> plusOne(vector<int>& digits) {
         for (int i = digits.size() - 1; i >= 0; --i) {
             if (digits[i] == 9) digits[i] = 0;
