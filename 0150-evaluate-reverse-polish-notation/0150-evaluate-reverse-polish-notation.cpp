9fbfe9c18dd6cdaf3d447d2b60f74636c084a6f8
Zheng Yong <angzhengyong@gmail.com>
2022-10-24 11:46:39 +0800

    Time: 11 ms (89.79%), Space: 12.3 MB (14.39%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.cpp
@@ -7,28 +7,21 @@ public:
     }
     
     int evalRPN(vector<string>& tokens) {
-        long a;
-        long b;
-        vector<long> st;
-        for(string c: tokens){
-            if(c=="+" || c=="-" || c=="*" || c=="/"){
-                b=st.back();
-                st.pop_back();
-                a=st.back();
-                st.pop_back();
-                if(c=="+"){
-                    st.push_back(a+b);
-                }else if (c=="-"){
-                     st.push_back(a-b);
-                }else if (c=="*"){
-                    st.push_back(a*b);
-                }else{
-                    st.push_back(a/b);
-                }
-            }else{
-                st.push_back(stoi(c));
+        unordered_map<string, function<long(long, long)>> op = {
+            { "+" , [](long a, long b) { return a + b; } },
+            { "-" , [](long a, long b) { return a - b; } },
+            { "*" , [](long a, long b) { return a * b; } },
+            { "/" , [](long a, long b) { return a / b; } }
+        };
+        stack<long> st;
+        for (string& s : tokens) {
+            if (op.find(s) == op.end()) st.push(stoi(s));
+            else {
+                long op1 = st.top(); st.pop();
+                long op2 = st.top(); st.pop();
+                st.push(op[s](op2, op1));
             }
         }
-        return (int)st.front();
+        return st.top();
     }
 };
