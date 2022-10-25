1b0e3850e8e13d774b9cc1a6d068728f1d075959
Zheng Yong <angzhengyong@gmail.com>
2022-10-25 09:14:01 +0800

    Time: 6 ms (74.31%), Space: 11.4 MB (77.71%) - LeetHub

--- 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
+++ 1662-check-if-two-string-arrays-are-equivalent/1662-check-if-two-string-arrays-are-equivalent.cpp
@@ -2,6 +2,8 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     bool arrayStringsAreEqual(vector<string>& word1, vector<string>& word2) {
