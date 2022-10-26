5d42fbb26310488d5c59f5c294a61c33f59be92d
Zheng Yong <angzhengyong@gmail.com>
2022-10-26 19:25:36 +0800

    Time: 169 ms (99.78%), Space: 112.3 MB (94.95%) - LeetHub

--- 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     bool checkSubarraySum(vector<int>& nums, int k) {
         unordered_map<int, int> seen{{0, 0}};
         int sum = 0;
