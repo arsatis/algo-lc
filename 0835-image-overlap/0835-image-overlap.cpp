84d6a739929eff88bb3ca4113264b36028085cf1
Zheng Yong <angzhengyong@gmail.com>
2022-10-27 22:12:41 +0800

    Time: 17 ms (97.22%), Space: 9.6 MB (61.81%) - LeetHub

--- 0835-image-overlap/0835-image-overlap.cpp
+++ 0835-image-overlap/0835-image-overlap.cpp
@@ -4,19 +4,35 @@ public:
         ios_base::sync_with_stdio(0);
     }
     
-    int largestOverlap(vector<vector<int>>& A, vector<vector<int>>& B) {
-        vector<int> LA, LB;
-        int N = A.size();
-        unordered_map<int, int> count;
-        for (int i = 0; i < N * N; ++i)
-            if (A[i / N][i % N] == 1)
-                LA.push_back(i / N * 100 + i % N);
-        for (int i = 0; i < N * N; ++i)
-            if (B[i / N][i % N] == 1)
-                LB.push_back(i / N * 100 + i % N);
-        for (int i : LA) for (int j : LB) count[i - j]++;
-        int res = 0;
-        for (auto it : count) res = max(res, it.second);
-        return res;
+    int largestOverlap(vector<vector<int>>& _img1, vector<vector<int>>& _img2) {
+        int rows = _img1.size(), cols = _img1.front().size();
+        
+        //store image rows into bitsets
+        vector<bitset<32>> img1(rows), img2(rows);
+        for(int r = 0; r < rows; ++r) {
+            for(int c = 0; c < cols; ++c) {
+                img1[r][c] = _img1[r][c];
+                img2[r][c] = _img2[r][c];
+            }
+        }
+        
+        int overlap = 0;
+        
+        for(int dr = -(rows-1); dr < rows; ++dr) {
+            for(int dc = -(cols-1); dc < cols; ++dc) {
+                int curoverlap = 0;
+                for(int r1 = 0; r1 < rows; ++r1) {
+                    int r2 = r1 + dr;
+                    if(r2 < 0) continue;
+                    if(r2 >= rows) break;
+                    auto r1r2 = img1[r1] & (dc < 0 ? img2[r2] << -dc : img2[r2] >> dc);
+                    curoverlap += r1r2.count();
+                }
+                if(curoverlap > overlap)
+                    overlap = curoverlap;
+            }
+        }
+        
+        return overlap;
     }
 };
