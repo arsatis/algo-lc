c1a94c144f7e424ae080e06b7e50aa408d045181
Zheng Yong <angzhengyong@gmail.com>
2022-10-28 14:05:27 +0800

    Time: 6 ms (24.46%), Space: 7 MB (92.72%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    void rotate(vector<vector<int>>& matrix) {
+        int n = matrix.size(), a = 0, b = n - 1;
+        while (a < b) {
+            for (int i = 0; i < b - a; ++i) {
+                swap(matrix[a][a + i], matrix[a + i][b]);
+                swap(matrix[a][a + i], matrix[b][b - i]);
+                swap(matrix[a][a + i], matrix[b - i][a]);
+            }
+            ++a, --b;
+        }
+    }
+};
