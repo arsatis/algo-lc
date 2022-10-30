d8c5944867de4a6d93bb6e8cb16d1aba86d8220c
Zheng Yong <angzhengyong@gmail.com>
2022-10-30 10:54:05 +0800

    Time: 127 ms (66.35%), Space: 19 MB (67.09%) - LeetHub

---
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    int shortestPath(vector<vector<int>>& a, int k) {
+        int n = a.size(), m = a[0].size();
+        if (n == 1 && m == 1) return 0;
+        
+        vector<vector<int>> vis(n, vector<int> (m, -1));
+        vector<pair<int, int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
+        auto check = [&](int x, int y) { return x > -1 && x < n && y > -1 && y < m; };
+
+        queue<vector<int>> q;
+        q.push({0, 0, 0, k}); // {row, col, no: of towns visited, coins left}
+        vis[0][0] = k;
+        
+        while (!q.empty()) {
+            auto t = q.front();
+            q.pop();
+            
+            int row = t[0], col = t[1], towns = t[2], coins = t[3];
+            if (row == n - 1 && col == m - 1) return towns;
+            
+            if (a[row][col] == 1) {
+                if (!coins) continue;
+                coins--;
+            }
+            
+            for (auto [dx, dy] : dir) {
+                int x = row + dx;
+                int y = col + dy;
+                if (check(x, y) && (vis[x][y] < coins)) {
+                    vis[x][y] = coins;
+                    q.push({x, y, towns + 1, coins});
+                }
+            }
+        }
+        return -1;
+    }
+};
