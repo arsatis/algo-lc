5bc31b5546d39c3cfba529becd0ab2e322207cf2
Zheng Yong <angzhengyong@gmail.com>
2022-10-30 11:09:59 +0800

    Time: 40 ms (86.37%), Space: 10.3 MB (84.42%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -1,28 +1,22 @@
 class Solution {
 public:
-    int shortestPath(vector<vector<int>>& a, int k) {
-        int n = a.size(), m = a[0].size();
+    int shortestPath(vector<vector<int>>& grid, int k) {
+        int n = grid.size(), m = grid[0].size();
         if (n == 1 && m == 1) return 0;
         
         vector<vector<int>> vis(n, vector<int> (m, -1));
         vector<pair<int, int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
         auto check = [&](int x, int y) { return x > -1 && x < n && y > -1 && y < m; };
 
-        queue<vector<int>> q;
+        queue<tuple<int, int, int, int>> q;
         q.push({0, 0, 0, k}); // {row, col, no: of towns visited, coins left}
         vis[0][0] = k;
         
         while (!q.empty()) {
-            auto t = q.front();
+            auto [row, col, towns, coins] = q.front();
             q.pop();
-            
-            int row = t[0], col = t[1], towns = t[2], coins = t[3];
             if (row == n - 1 && col == m - 1) return towns;
-            
-            if (a[row][col] == 1) {
-                if (!coins) continue;
-                coins--;
-            }
+            coins -= grid[row][col];
             
             for (auto [dx, dy] : dir) {
                 int x = row + dx;
