b63e4d721f8141c00cb54bbe1b95b552afb3a6c2
Zheng Yong <angzhengyong@gmail.com>
2022-10-30 11:18:46 +0800

    Time: 25 ms (92.50%), Space: 9.9 MB (87.22%) - LeetHub

--- 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
+++ 1293-shortest-path-in-a-grid-with-obstacles-elimination/1293-shortest-path-in-a-grid-with-obstacles-elimination.cpp
@@ -2,32 +2,39 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
+        cin.tie(0);
+        cout.tie(0);
     }
     
     int shortestPath(vector<vector<int>>& grid, int k) {
-        int n = grid.size(), m = grid[0].size();
-        if (n == 1 && m == 1) return 0;
+        int steps = 1, m = grid.size(), n = grid[0].size();
+        if (m == 1 && n == 1) return 0;
         
-        vector<vector<int>> vis(n, vector<int> (m, -1));
-        vector<pair<int, int>> dir = {{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-        auto check = [&](int x, int y) { return x > -1 && x < n && y > -1 && y < m; };
-
-        queue<tuple<int, int, int, int>> q;
-        q.emplace(0, 0, 0, k); // {row, col, no: of towns visited, coins left}
-        vis[0][0] = k;
+        vector<pair<int, int>> directions = {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
+        auto check = [&](int x, int y) { return x >= 0 && x < m && y >= 0 && y < n; };
+        
+        vector<vector<int>> visited(m, vector<int>(n, -1));
+        queue<tuple<int, int, int>> q;
+        q.emplace(0, 0, k);
+        visited[0][0] = k;
         
         while (!q.empty()) {
-            auto [row, col, towns, coins] = q.front();
-            q.pop();
-            if (row == n - 1 && col == m - 1) return towns;
-            coins -= grid[row][col];
-            for (auto [dx, dy] : dir) {
-                int x = row + dx, y = col + dy;
-                if (check(x, y) && (vis[x][y] < coins)) {
-                    vis[x][y] = coins;
-                    q.emplace(x, y, towns + 1, coins);
+            int size = q.size();
+            while (size--) {
+                auto [i, j, pwr] = q.front();
+                q.pop();
+                for (auto [di, dj] : directions) {
+                    int _i = i + di, _j = j + dj, _pwr;
+                    if (check(_i, _j) && (_pwr = pwr - grid[_i][_j]) >= 0) {
+                        if (_i == m - 1 && _j == n - 1) return steps;
+                        if (visited[_i][_j] < _pwr) {
+                            visited[_i][_j] = _pwr;
+                            q.emplace(_i, _j, _pwr);
+                        }
+                    }
                 }
             }
+            ++steps;
         }
         return -1;
     }
