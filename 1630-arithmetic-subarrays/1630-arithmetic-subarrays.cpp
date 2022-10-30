54721293e0c54e43105f9355d40d243d697aafc6
Zheng Yong <angzhengyong@gmail.com>
2022-10-30 11:38:55 +0800

    Time: 27 ms (99.91%), Space: 21.3 MB (86.45%) - LeetHub

--- 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
+++ 1630-arithmetic-subarrays/1630-arithmetic-subarrays.cpp
@@ -2,8 +2,6 @@ class Solution {
 public:
     Solution() {
         ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
     }
     
     vector<bool> checkArithmeticSubarrays(vector<int>& nums, vector<int>& l, vector<int>& r) {
