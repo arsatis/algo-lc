e2d6b72024db80ded5eafb4fcab8c511d1a8581a
Zheng Yong <angzhengyong@gmail.com>
2022-10-31 13:58:41 +0800

    Time: 26 ms (68.65%), Space: 17.6 MB (5.71%) - LeetHub

--- 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     bool isToeplitzMatrix(vector<vector<int>>& matrix) {
         for (int i = 1; i < matrix.size(); ++i)
             for (int j = 1; j < matrix[0].size(); ++j)
