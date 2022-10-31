d5b87b6ec3657893e11783715acb4254c80ee1ea
Zheng Yong <angzhengyong@gmail.com>
2022-10-31 14:12:18 +0800

    Time: 58 ms (73.01%), Space: 24 MB (41.04%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -9,7 +9,6 @@ public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         vector<int> nge(nums.size());
         stack<int> st;
-        
         for (int rounds = 0; rounds < 2; ++rounds) {
             for (int i = nums.size() - 1; i >= 0; --i) {
                 while (!st.empty() && st.top() <= nums[i]) st.pop();
