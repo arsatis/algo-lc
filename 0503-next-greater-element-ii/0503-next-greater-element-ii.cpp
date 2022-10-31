96f2ad8c47f03378125019a138c605d1b6f10145
Zheng Yong <angzhengyong@gmail.com>
2022-10-31 14:14:22 +0800

    Time: 25 ms (99.69%), Space: 24.1 MB (41.04%) - LeetHub

--- 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
+++ 0503-next-greater-element-ii/0503-next-greater-element-ii.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     vector<int> nextGreaterElements(vector<int>& nums) {
         vector<int> nge(nums.size(), -1);
         stack<int> st;
