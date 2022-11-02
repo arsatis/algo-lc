85d1231725f0dc4e36c0bd8b98dee00d8d379845
Zheng Yong <angzhengyong@gmail.com>
2022-11-02 10:53:45 +0800

    Time: 125 ms (75.08%), Space: 61.5 MB (19.86%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.cpp
@@ -1,5 +1,9 @@
 class Solution {
 public:
+    Solution() {
+        ios_base::sync_with_stdio(0);
+    }
+    
     int numSubarrayProductLessThanK(vector<int>& nums, int k) {
         if (k == 0) return 0;
         int num = 0, curr = 1;
