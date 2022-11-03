becb73a41c5cb7de9f7c691d6737e009781c87b6
Zheng Yong <angzhengyong@gmail.com>
2022-11-03 09:53:18 +0800

    Time: 26 ms (85.31%), Space: 15.4 MB (70.01%) - LeetHub

---
+++ 0910-smallest-range-ii/0910-smallest-range-ii.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int smallestRangeII(vector<int>& nums, int k) {
+        sort(nums.begin(), nums.end());
+        int mx = nums[nums.size() - 1], mn = nums[0], res = mx - mn;
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            mx = max(mx, nums[i] + (k << 1));
+            mn = min(nums[i + 1], nums[0] + (k << 1));
+            res = min(res, mx - mn);
+        }
+        return res;
+    }
+};
