2916fe96eb7e84bb7e17efb1b40210e992931aeb
Zheng Yong <angzhengyong@gmail.com>
2022-11-06 11:54:43 +0800

    Time: 7 ms (100.00%), Space: 11 MB (55.60%) - LeetHub
    Reduced time complexity from O(nk^2) to O(nk).

--- 265-paint-house-ii/265-paint-house-ii.cpp
+++ 265-paint-house-ii/265-paint-house-ii.cpp
@@ -1,15 +1,16 @@
 class Solution {
 public:
     int minCostII(vector<vector<int>>& costs) {
-        int i = 0, k = costs[0].size();
+        int i = 0, k = costs[0].size(), min1, min2;
         vector<vector<int>> colors(2, vector<int>(k));
         for (auto& v : costs) {
+            min1 = INT_MAX, min2 = INT_MAX;
             for (int j = 0; j < k; ++j) {
-                int tmp = colors[i][j];
-                colors[i][j] = INT_MAX;
-                colors[1 - i][j] = v[j] + *min_element(colors[i].begin(), colors[i].end());
-                colors[i][j] = tmp;
+                if (colors[i][j] < min1) min2 = min1, min1 = colors[i][j];
+                else if (colors[i][j] < min2) min2 = colors[i][j];
             }
+            for (int j = 0; j < k; ++j)
+                colors[1 - i][j] = v[j] + (colors[i][j] == min1 ? min2 : min1);
             i = 1 - i;
         }
         return *min_element(colors[i].begin(), colors[i].end());
