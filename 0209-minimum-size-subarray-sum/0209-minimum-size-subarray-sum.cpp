79b86d20bf0f750fd595cbadd333a8f521937a14
Zheng Yong <angzhengyong@gmail.com>
2022-11-15 23:11:52 +0800

    Time: 26 ms (99.78%), Space: 28.1 MB (99.22%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -1,11 +1,5 @@
 class Solution {
 public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-    }
-
     int minSubArrayLen(int target, vector<int>& nums) {
         int minLen = nums.size(), total = 0;
         for (int l = 0, r = 0; r < nums.size();) {
