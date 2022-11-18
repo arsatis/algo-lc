6716b3cba230a2ab1cea67ee6c6d2c6b76434a4c
Zheng Yong <angzhengyong@gmail.com>
2022-11-18 13:37:13 +0800

    Time: 48 ms (99.89%), Space: 19.1 MB (97.74%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.cpp
@@ -2,12 +2,6 @@ class Solution {
     pair<int, int> dir[8] = {{-1, -1}, {-1, 0}, {-1, 1}, {0, 1},
                              {1, 1}, {1, 0}, {1, -1}, {0, -1}};
 public:
-    Solution() {
-        ios_base::sync_with_stdio(0);
-        cin.tie(0);
-        cout.tie(0);
-    }
-    
     int shortestPathBinaryMatrix(vector<vector<int>>& grid) {
         int n = grid.size() - 1, pathLen = 1;
         queue<pair<int, int>> q;
@@ -18,12 +12,10 @@ public:
         
         while (!q.empty()) {
             int size = q.size();
-            
             while (size--) {
                 auto [i, j] = q.front();
                 if (i == n && j == n) return pathLen;
                 q.pop();
-                
                 for (int k = 0; k < 8; ++k) {
                     int i2 = i + dir[k].first, j2 = j + dir[k].second;
                     if (i2 >= 0 && i2 <= n && j2 >= 0 && j2 <= n && !grid[i2][j2]) {
