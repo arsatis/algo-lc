9e6bf0e6626e47909943004c9bc8ca21c8e4b3cf
Zheng Yong <angzhengyong@gmail.com>
2022-12-01 10:38:55 +0800

    Time: 0 ms (100.00%), Space: 6.4 MB (95.88%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.cpp
@@ -5,6 +5,7 @@ public:
             c = tolower(c);
             return c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u';
         };
+        
         int numVowels = 0, half = s.size() >> 1;
         for (int i = 0; i < half; ++i) if (f(s[i])) ++numVowels;
         for (int i = half; i < s.size(); ++i) if (f(s[i])) --numVowels;
