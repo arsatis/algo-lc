47ba996b8ae4b64d5e6be6eb99f5902702c62b27
Zheng Yong <angzhengyong@gmail.com>
2022-12-10 15:47:33 +0800

    Time: 0 ms (100.00%), Space: 6.6 MB (98.18%) - LeetHub

---
+++ 0415-add-strings/0415-add-strings.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    string addStrings(string& num1, string& num2) {
+        reverse(num1.begin(), num1.end());
+        reverse(num2.begin(), num2.end());
+        
+        string output;
+        int n = max(num1.size(), num2.size()), carry = 0;
+        for (int i = 0; i < n; ++i) {
+            int val1 = i < num1.size() ? num1[i] - '0' : 0,
+                val2 = i < num2.size() ? num2[i] - '0' : 0,
+                sum = val1 + val2 + carry;
+            output += sum % 10 + '0';
+            carry = sum / 10;
+        }
+        if (carry > 0) output += carry + '0';
+        reverse(output.begin(), output.end());
+        return output;
+    }
+};
