a410a6eb2046726302a52a96cddfd3be2153d738
Zheng Yong <angzhengyong@gmail.com>
2022-12-23 14:25:47 +0800

    Time: 152 ms (99.91%), Space: 60.5 MB (98.76%) - LeetHub

--- 0997-find-the-town-judge/0997-find-the-town-judge.cpp
+++ 0997-find-the-town-judge/0997-find-the-town-judge.cpp
@@ -5,10 +5,10 @@ public:
     }
     
     int findJudge(int n, vector<vector<int>>& trust) {
-        if (n == 1) return 1;
-        vector<int> a(n + 1), b(n + 1);
-        for (auto& v : trust) ++a[v.front()], ++b[v.back()];
-        for (int i = 0; i <= n; ++i) if (a[i] == 0 && b[i] == n - 1) return i;
+        int arr[(n << 1) + 1];
+        memset(arr, 0, sizeof(arr));
+        for (auto& v : trust) ++arr[v[0]], ++arr[n + v[1]];
+        for (int i = 1; i <= n; ++i) if (!arr[i] && arr[n + i] == n - 1) return i;
         return -1;
     }
 };
